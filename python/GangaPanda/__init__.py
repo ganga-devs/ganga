66474b44b2faa439d30a8d7eafd1230e74b59833
Daniel van der Ster <dvanders@gmail.com>
2008-07-28 15:57:00 +0000

    Ensure curl is installed when loading Panda

--- python/GangaPanda/__init__.py
+++ python/GangaPanda/__init__.py
@@ -8,7 +8,16 @@ def loadPlugins(c):
     import sys
     from Ganga.Utility.logging import getLogger
 
-    import Lib.Panda
+    try:
+        import Lib.Panda
+    except SystemExit:
+        from Ganga.Core.exceptions import ApplicationConfigurationError
+        import commands
+        (s,o) = commands.getstatusoutput('curl --version')
+        if (s):
+            raise ApplicationConfigurationError(None,"Couldn't load Panda Client: ensure 'curl' is available")
+        else:
+            raise ApplicationConfigurationError(None,"Couldn't load Panda Client")
     import Lib.Athena
 
     return None
