33e430fae2e3837ebfe1501fec2ae25a64e4277c
Hurng-Chun Lee <Hurng-Chun.Lee@cern.ch>
2008-08-21 12:35:21 +0000

    remove python2.3 dependency

--- python/GangaPlotter/PACKAGE.py
+++ python/GangaPlotter/PACKAGE.py
@@ -1,7 +1,7 @@
 ################################################################################
 # Ganga Project. http://cern.ch/ganga
 #
-# $Id: PACKAGE.py,v 1.1 2008-07-17 16:41:31 moscicki Exp $
+# $Id: PACKAGE.py,v 1.2 2008-08-21 12:35:21 hclee Exp $
 ################################################################################
 
 """ Refer to Ganga/PACKAGE.py for details on the purpose of this module.
@@ -9,8 +9,7 @@
 external_packages = {
    'matplotlib' : {'version' : '0.91.2', 'PYTHONPATH':'lib/python2.3/site-packages'},
    'numpy'      : {'version' : '1.0.4', 'PYTHONPATH':'lib/python2.3/site-packages', 'PATH' : 'bin'},
-   'pyqt'       : {'version' : '3.13_python234', 'PYTHONPATH':'lib/python2.3.4/site-packages', 'LD_LIBRARY_PATH' :'lib'},   
-   'Python'     : {'version' : '2.3.4', 'PYTHONPATH':'lib/python2.3', 'LD_LIBRARY_PATH':'lib', 'PATH' : 'bin'}
+   'pyqt'       : {'version' : '3.13_python234', 'PYTHONPATH':'lib/python2.3.4/site-packages', 'LD_LIBRARY_PATH' :'lib'}
    }
 
 from Ganga.Utility.Setup import PackageSetup
