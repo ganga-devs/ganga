ebe681ca36cfbdffdb12b8a615546c39df56575d
Jakub Moscicki <Jakub.Moscicki@cern.ch>
2008-08-22 12:17:30 +0000

    removed python 2.3 dependency (not needed anymore)

--- python/GangaTest/PACKAGE.py
+++ python/GangaTest/PACKAGE.py
@@ -2,26 +2,25 @@
 """
 
 external_packages = {
-	'Python' : {'version' : '2.3.4', 'LD_LIBRARY_PATH':'lib', 'PATH' : 'bin', 'maxHexVersion' : '0x20300f0'},
-	#TestOOB testing tool
-	'PYTF' : {'version' : '1.4', 'noarch':True, 'PYTHONPATH':'.', 'PYTF_TOP_DIR':'.'},
-	#coverage reporting tool
-	'figleaf' : {'version' : '0.6', 'noarch':True, 'PYTHONPATH':'.'},
-	#test externals
-	'test-externals':{'version':'1.0', 'noarch':True, 'TEST_EXTERNAL_PATH':'.'},
-	#subprocess
-	'subprocess':{'version':'2.4.2', 'noarch':True, 'PYTHONPATH':'lib/python2.2/site-packages', 'maxHexVersion':'0x20400f0'}
-	}
+        #TestOOB testing tool
+        'PYTF' : {'version' : '1.4', 'noarch':True, 'PYTHONPATH':'.', 'PYTF_TOP_DIR':'.'},
+        #coverage reporting tool
+        'figleaf' : {'version' : '0.6', 'noarch':True, 'PYTHONPATH':'.'},
+        #test externals
+        'test-externals':{'version':'1.0', 'noarch':True, 'TEST_EXTERNAL_PATH':'.'},
+        #subprocess
+        'subprocess':{'version':'2.4.2', 'noarch':True, 'PYTHONPATH':'lib/python2.2/site-packages', 'maxHexVersion':'0x20400f0'}
+        }
 
 from Ganga.Utility.Setup import PackageSetup
 
 setup = PackageSetup(external_packages)
 
 def standardSetup(setup=setup):
-	for p in setup.packages:
-		setup.prependPath(p,'PYTHONPATH')
-		setup.prependPath(p,'LD_LIBRARY_PATH')
-		setup.prependPath(p,'PATH')
-		setup.prependPath(p,'PYTF_TOP_DIR')
-		setup.setPath(p,'TEST_EXTERNAL_PATH')
+        for p in setup.packages:
+                setup.prependPath(p,'PYTHONPATH')
+                setup.prependPath(p,'LD_LIBRARY_PATH')
+                setup.prependPath(p,'PATH')
+                setup.prependPath(p,'PYTF_TOP_DIR')
+                setup.setPath(p,'TEST_EXTERNAL_PATH')
 
