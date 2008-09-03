4d9d3e8844199b5e3c192d3527158721e01211e9
Daniel van der Ster <dvanders@gmail.com>
2008-09-03 16:59:14 +0000

    Make PandaTools an external dep

--- python/GangaPanda/PACKAGE.py
+++ python/GangaPanda/PACKAGE.py
@@ -1,13 +1,18 @@
 ################################################################################
 # Ganga Project. http://cern.ch/ganga
 #
-# $Id: PACKAGE.py,v 1.2 2008-08-21 14:30:13 dvanders Exp $
+# $Id: PACKAGE.py,v 1.3 2008-09-03 16:59:14 dvanders Exp $
 ################################################################################
 
 """ Refer to Ganga/PACKAGE.py for details on the purpose of this module.
 """
 
 _external_packages = {
+    'PandaTools' : { 'version' : '20080827', 
+                    'PYTHONPATH':'python',
+                    'CONFIGEXTRACTOR_PATH':'share',
+                    'noarch':True
+    } 
 }
 
 from Ganga.Utility.Setup import PackageSetup
@@ -19,4 +24,5 @@ def standardSetup(setup=setup):
         setup.prependPath(p,'PYTHONPATH')
         setup.prependPath(p,'LD_LIBRARY_PATH')
         setup.prependPath(p,'PATH')
+        setup.prependPath(p,'CONFIGEXTRACTOR_PATH')
 
