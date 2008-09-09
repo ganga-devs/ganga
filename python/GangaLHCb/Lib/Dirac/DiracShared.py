a55955f77f5d67b125f3eb644e7c25cb7d732e09
Will Reece <will@reece.org.uk>
2008-09-09 13:07:13 +0000

    adds some sort of locking for writing to results files and adds a method to get the diracMain file

--- python/GangaLHCb/Lib/Dirac/DiracShared.py
+++ python/GangaLHCb/Lib/Dirac/DiracShared.py
@@ -5,9 +5,24 @@ def getPickleFileName():
     import os
     return os.path.join(os.path.expanduser('~'),'.__tmpGangaPickle__%s.p' % os.path.expandvars('$USER'))
   
-def storeResult(result):
-    import pickle, os
+def storeResult(result, retry_count = 0):
+    import pickle, os, time
     fName = getPickleFileName()
+    
+    #if the file exists then hold off a little while
+    if os.path.exists(fName):
+        #thread safe but not AFS safe...
+        time.sleep(2) #sleep for a second
+        if retry_count < 6:
+            #file is locked so wait... and try again
+            storeResult(result, retry_count + 1)
+        else:
+            #file needs to be cleaned up
+            try:
+                os.unlink(fName)
+            except:
+                pass
+    
     out = file(fName,'wb')
     try:
         pickle.dump(result,out)
@@ -49,3 +64,12 @@ def listdirs(path, file_list = []):
             else:
                 file_list.append(fname)
     return file_list
+
+
+def getGenericRunScript():
+    import inspect, os
+    
+    diracShared = inspect.getsourcefile(getGenericRunScript)
+    return os.path.join(os.path.dirname(diracShared),'diracJobMain.py')
+
+
