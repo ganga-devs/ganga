6cefb1dfe1db1f94115ff905f1d27ca9ac189d84
Karl Harrison <harrison@hep.phy.cam.ac.uk>
2008-10-04 17:42:39 +0000

    First import to new Ganga repository

---
+++ python/GangaCamtology/Lib/__init__.py
@@ -0,0 +1,18 @@
+###############################################################################
+# Ganga Project. http://cern.ch/ganga
+#
+# $Id: __init__.py,v 1.1 2008-10-04 17:42:37 karl Exp $
+###############################################################################
+# File: Lib//__init__.py
+# Author: K. Harrison
+# Created: 070123
+                                                                                
+"""Package initialisation file"""
+                                                                                
+__author__  = "K.Harrison <Harrison@hep.phy.cam.ac.uk>"
+__date__    = "30 November 2007"
+__version__ = "1.1"
+
+from Vanseg import Vanseg
+from Classify import Classify
+from CamontDataset import CamontDataset
