f26ba29630692277e89b686c2ec822ea15493876
Katarzyna Pajchel <katarina.pajchel@fys.uio.no>
2008-10-13 19:54:04 +0000

    lfc updates

--- python/GangaNG/PACKAGE.py
+++ python/GangaNG/PACKAGE.py
@@ -1,14 +1,15 @@
 ################################################################################
 # Ganga Project. http://cern.ch/ganga
 #
-# $Id: PACKAGE.py,v 1.3 2008-10-11 15:38:15 pajchel Exp $
+# $Id: PACKAGE.py,v 1.4 2008-10-13 19:54:04 pajchel Exp $
 ################################################################################
 
 """ Refer to Ganga/PACKAGE.py for details on the purpose of this module.
 """
 
 _external_packages = {
-    'nordugrid-arc-standalone' : {'version' : '0.6.4', 'PYTHONPATH':'lib/python2.2/site-packages', 'LD_LIBRARY_PATH':'lib', 'PATH' : 'bin', 'ARC_LOCATION' : '', 'GLOBUS_LOCATION' : ''}
+    'nordugrid-arc-standalone' : {'version' : '0.6.4', 'PYTHONPATH':'lib/python2.3/site-packages', 'LD_LIBRARY_PATH':'lib', 'PATH' : 'bin', 'ARC_LOCATION' : '', 'GLOBUS_LOCATION' : ''},
+    'lfc' : {'version' : '1.6.9', 'PYTHONPATH':'lib/python2.3/site-packages/'}
     }
 
 import os
@@ -20,8 +21,8 @@ setup = PackageSetup(_external_packages)
 
 def standardSetup(setup=setup):
     for p in setup.packages:
-        #setup.prependPath(p,'PYTHONPATH')
-        #setup.prependPath(p,'LD_LIBRARY_PATH')
+        setup.prependPath(p,'PYTHONPATH')
+        setup.prependPath(p,'LD_LIBRARY_PATH')
         #setup.prependPath(p,'PATH')
         setup.prependPath(p,'ARC_LOCATION')
         if not os.environ.has_key('GLOBUS_LOCATION'):
