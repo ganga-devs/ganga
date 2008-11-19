669b5a4beca37d4a32e1bcda04b242c3ceb4d3ac
Marcello Barisonzi <marcello.barisonzi@cern.ch>
2008-11-19 15:12:06 +0000

    try

---
+++ python/GangaSFrame/BOOT.py
@@ -0,0 +1,6 @@
+import sys
+
+print >>sys.stderr
+print >>sys.stderr, 'SFrame Plugin -- Copyleft 2007 Marcello Barisonzi. All Wrongs Reserved.'
+print >>sys.stderr, 'Email: marcello.barisonzi@desy.de'
+print >>sys.stderr
