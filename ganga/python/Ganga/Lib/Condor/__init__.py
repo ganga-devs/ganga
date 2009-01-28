fb2156b5a10296d2b7fd106bda2f9a6be16c0d76
(no author) <anonymous@epldt013.ph.bham.ac.uk>
2009-01-28 18:42:30 +0000

    This commit was manufactured by cvs2svn to create tag 'Ganga-Condor-20-04'.

---
+++ ganga/python/Ganga/Lib/Condor/__init__.py
@@ -0,0 +1,18 @@
+###############################################################################
+# Ganga Project. http://cern.ch/ganga
+#
+# $Id: __init__.py,v 1.1 2008-07-17 16:40:56 moscicki Exp $
+###############################################################################
+# File: Condor/__init__.py
+# Author: K. Harrison
+# Created: 051228
+# Version 1.0: 051228
+                                                                                
+"""Package initialisation file"""
+                                                                                
+__author__  = "K.Harrison <Harrison@hep.phy.cam.ac.uk>"
+__date__    = "28 July 2006"
+__version__ = "1.0"
+
+from Condor import Condor
+from CondorRequirements import CondorRequirements
