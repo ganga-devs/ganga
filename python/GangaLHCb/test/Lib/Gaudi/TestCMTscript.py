4723cb63c2a115d4de7a413017dabe4e95f28e35
Mike Williams <mwill@mit.edu>
2009-02-16 14:13:05 +0000

    *** empty log message ***

--- python/GangaLHCb/test/Lib/Gaudi/TestCMTscript.py
+++ python/GangaLHCb/test/Lib/Gaudi/TestCMTscript.py
@@ -5,11 +5,6 @@ class TestCMTscript(GangaGPITestCase):
     """Tests methods defined in GangaLHCb/Lib/Gaudi/CMTscript.py"""
 
     def test_parse_master_package(self):
-        # this test will pass for current (6/11/2008) version of this method.
-        # this method is currently not used and the behavior is a bit odd
-        # (eg. it returns different types for / vs ' ' sepearated lists).
-        # if this method is ever brought back into usage, this test may need
-        # to be modified.
         val = parse_master_package('a/b/c')
         assert val == ['a','b','c'], 'splitting dir names (3) is broken'
         val = parse_master_package('a/b')
