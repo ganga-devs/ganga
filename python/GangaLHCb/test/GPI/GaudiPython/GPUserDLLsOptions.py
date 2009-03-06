fb7025309c5ac3764f9559220267695a2d2100bf
Mike Williams <mwill@mit.edu>
2009-03-06 16:43:07 +0000

    Updated

--- python/GangaLHCb/test/GPI/GaudiPython/GPUserDLLsOptions.py
+++ python/GangaLHCb/test/GPI/GaudiPython/GPUserDLLsOptions.py
@@ -7,13 +7,15 @@ importOptions('DVTutorial_1.py')
 importOptions('Bs2JpsiPhi2008.py')
 
 import GaudiPython
-
+ 
 appMgr = GaudiPython.AppMgr()
 evt = appMgr.evtsvc()
 
-while True:
+nevt = 0
+while nevt < 1000:
     appMgr.run(1)
     if evt['/Event/Rec/Header'] is None: break
+    nevt = nevt + 1
 
 appMgr.exit()
 
