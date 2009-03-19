9acd9cb6f8eecddba91990658c62def567701933
(no author) <anonymous@epldt013.ph.bham.ac.uk>
2009-03-19 13:13:32 +0000

    This commit was manufactured by cvs2svn to create tag 'Ganga-Base-23-0'.

---
+++ ganga/python/Ganga/Core/JobRepository/Separator.py
@@ -0,0 +1,23 @@
+################################################################################
+# Ganga Project. http://cern.ch/ganga
+#
+# $Id: Separator.py,v 1.1 2008-07-17 16:40:50 moscicki Exp $
+################################################################################
+
+class Parser(object):
+
+    def extractSubJobs(self, attrDict):
+        sjobs = attrDict['data']['subjobs']
+        if sjobs:
+            attrDict['data']['subjobs'] = []
+            res = map(self.extractSubJobs, sjobs)
+        else:
+            res = []
+        return (attrDict, res)
+    extractSubJobs = classmethod(extractSubJobs)
+
+    def insertSubJobs(self, sjob_tree):
+        attrDict = sjob_tree[0]
+        attrDict['data']['subjobs'] = map(self.insertSubJobs, sjob_tree[1])
+        return attrDict
+    insertSubJobs = classmethod(insertSubJobs)
---
+++ ganga/python/Ganga/Core/__init__.py
@@ -0,0 +1,133 @@
+"""
+Core package defines the fundamental subsystems of Ganga Architecture.
+Subsystems are autonomous components (such as a remote services) which may be independetly deployed.
+Subsystems may also be created as local objects in the Ganga Client process.
+"""
+
+from exceptions import *
+
+monitoring_component = None
+
+def set_autostart_policy(interactive_session):
+    """
+    Change the default value of autostart of the monitoring, depending if the session is interactive or batch.
+    The autostart value may be overriden in the config file, so warn if it differs from the default.
+    This function should be called
+    """
+    from Ganga.Core.MonitoringComponent.Local_GangaMC_Service import config
+        
+# internal helper variable for interactive shutdown
+t_last = None
+
+def bootstrap(reg, interactive_session):
+    """
+    Create local subsystems. In the future this procedure should be enhanced to connect to remote subsystems.
+    FIXME: this procedure should be moved to the Runtime package.
+
+    This function will change the default value of autostart of the monitoring, depending if the session is interactive or batch.
+    The autostart value may be overriden in the config file, so warn if it differs from the default.
+    """
+    from Ganga.Core.MonitoringComponent.Local_GangaMC_Service import JobRegistry_Monitor, config
+
+    config.addOption('forced_shutdown_policy','session_type','If there are remaining background activities at exit such as monitoring, output download Ganga will attempt to wait for the activities to complete. You may select if a user is prompted to answer if he wants to force shutdown ("interactive") or if the system waits on a timeout without questions ("timeout"). The default is "session_type" which will do interactive shutdown for CLI and timeout for scripts.')
+
+    config.addOption('forced_shutdown_timeout',60,"Timeout in seconds for forced Ganga shutdown.")
+    config.addOption('forced_shutdown_prompt_time',10,"User will get the prompt every couple of seconds, as specified by this parameter.")
+
+    from Ganga.Utility.logging import getLogger
+
+    logger = getLogger()
+
+    from Ganga.Core.GangaThread import GangaThreadPool
+
+    # create generic Ganga thread pool
+    thread_pool = GangaThreadPool.getInstance()
+
+    #start the internal services coordinator    
+    from Ganga.Core.InternalServices import Coordinator,ShutdownManager
+    Coordinator.bootstrap()
+    #load the shutdown manager    
+    #ShutdownManager.install()
+
+    # backend-specific setup (e.g. Remote: setup any remote ssh pipes)
+    for j in reg:
+        if hasattr(j.backend,'setup'): # protect: EmptyGangaObject does not have setup() method
+            j.backend.setup()
+            
+    #start the monitoring loop
+    global monitoring_component
+    monitoring_component = JobRegistry_Monitor( reg )
+    monitoring_component.start()
+    
+    #register the MC shutdown hook
+    import atexit
+
+    def should_wait_interactive_cb(t_total):
+        global t_last
+        if t_last is None or time.time()-t_last > config['forced_shutdown_prompt_time']:
+            resp = raw_input("Job status update or output download still in progress (shutdown not completed after %d seconds). \n Do you want to force the exit (y/[n])"%t_total) 
+
+            t_last = time.time()
+            return resp.lower() != 'y'
+        else:
+            return True
+
+    def should_wait_batch_cb(t_total):
+        if t_total > config['forced_shutdown_timeout']:
+            logger.warning('Shutdown was forced after waiting for %d seconds for background activities to finish (monitoring, output download, etc). This may result in some jobs being corrupted.',t_total)
+            return False
+        return True
+
+    #register the exit function with the highest priority (==0)    
+    #atexit.register((0,monitoring_component.stop), fail_cb=mc_fail_cb,max_retries=config['max_shutdown_retries'])
+
+
+    #select the shutdown method based on configuration and/or session type
+    forced_shutdown_policy = config['forced_shutdown_policy']
+
+    if forced_shutdown_policy == 'interactive':
+        should_wait_cb = should_wait_interactive_cb
+    else:
+        if forced_shutdown_policy == 'batch':
+            should_wait_cb = should_wait_batch_cb
+        else:
+            if interactive_session:
+                should_wait_cb = should_wait_interactive_cb
+            else:
+                should_wait_cb = should_wait_batch_cb
+
+    atexit.register((0,thread_pool.shutdown), should_wait_cb=should_wait_cb)
+
+    #export to GPI
+    from Ganga.Runtime.GPIexport import exportToGPI
+    exportToGPI('runMonitoring',monitoring_component.runMonitoring,'Functions')     
+
+    autostart_default = interactive_session
+    config.overrideDefaultValue('autostart',bool(autostart_default))
+
+    if config['autostart'] is not autostart_default:
+        msg = 'monitoring loop %s (the default setting for %s session is %s)'
+        val = { True : ('enabled', 'batch', 'disabled'), 
+                False: ('disabled', 'interactive', 'enabled')}
+        logger.warning(msg%val[config['autostart']])
+
+    if config['autostart']:        
+        monitoring_component.enableMonitoring()
+
+
+    # THIS IS FOR DEBUGGING ONLY
+    import time
+    class Stuck(GangaThread.GangaThread):
+        def __init__(self):
+            GangaThread.GangaThread.__init__(self,name='Stuck')
+        def run(self):
+            i = 0
+            while i < 10:
+                time.sleep(3)
+                #print '*'*30,i
+                i += 1
+        def stop(self):
+            print "I was asked to stop..."
+    #DISABLED
+    #s = Stuck()
+    #s.start()
