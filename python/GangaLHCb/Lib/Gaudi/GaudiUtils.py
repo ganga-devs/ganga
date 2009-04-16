b2c892153459c9202f169e2e03c8ed25479da4c1
Mike Williams <mwill@mit.edu>
2009-04-16 12:28:31 +0000

    Revmoved Panoramix

--- python/GangaLHCb/Lib/Gaudi/GaudiUtils.py
+++ python/GangaLHCb/Lib/Gaudi/GaudiUtils.py
@@ -13,7 +13,7 @@ logger = Ganga.Utility.logging.getLogger()
 
 def available_apps():
   return ["Gauss", "Boole", "Brunel", "DaVinci", "Moore", "Vetra",
-          "Panoptes", "Gaudi"]
+          "Panoptes", "Gaudi", 'test']
 
 def available_packs(appname):
   packs={'Gauss'   : 'Sim',
