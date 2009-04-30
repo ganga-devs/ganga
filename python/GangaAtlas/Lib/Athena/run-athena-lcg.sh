7bc49affc9474ec83adc7e482a799e02886a0bf6
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2009-04-30 08:55:28 +0000

    Fix #49823, proper setup for local DB

--- python/GangaAtlas/Lib/Athena/run-athena-lcg.sh
+++ python/GangaAtlas/Lib/Athena/run-athena-lcg.sh
@@ -113,22 +113,6 @@ fi
 
 get_files PDGTABLE.MeV
 
-################################################
-# Make a local copy of requested geomDB if none already available
-if [ ! -e geomDB ]; then
- mkdir geomDB
- cd geomDB
- get_files -data geomDB/larHV_sqlite
- get_files -data geomDB/geomDB_sqlite
- cd ..
-fi
-if [ ! -e sqlite200 ]; then
- mkdir sqlite200
- cd sqlite200
- get_files -data sqlite200/ALLP200.db
- cd ..
-fi
-
 #################################################
 # Remove /lib and /usr/lib from LD_LIBRARY_PATH
 dum=`echo $LD_LIBRARY_PATH | tr ':' '\n' | egrep -v '^/lib' | egrep -v '^/usr/lib' | tr '\n' ':' `
@@ -307,6 +291,22 @@ then
         export DQ2_LOCAL_SITE_ID_BACKUP=$DQ2_LOCAL_SITE_ID
     fi
 
+fi
+
+################################################
+# Make a local copy of requested geomDB if none already available
+if [ ! -e geomDB ]; then
+ mkdir geomDB
+ cd geomDB
+ get_files -data geomDB/larHV_sqlite
+ get_files -data geomDB/geomDB_sqlite
+ cd ..
+fi
+if [ ! -e sqlite200 ]; then
+ mkdir sqlite200
+ cd sqlite200
+ get_files -data sqlite200/ALLP200.db
+ cd ..
 fi
 
 ls -rtla
