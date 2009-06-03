23838f5795b7650eaca4c991e7fd18cd5c4cff89
Johannes Ebke <Johannes.Ebke@physik.uni-muenchen.de>
2009-06-03 11:37:32 +0000

    Changed local dq2 site discovery, relies more on environment Works too if the site still has _USERDISK set and is not in TiersOfATLAS

--- python/GangaAtlas/Lib/Athena/db_dq2localid.py
+++ python/GangaAtlas/Lib/Athena/db_dq2localid.py
@@ -7,27 +7,23 @@ from dq2.info.TiersOfATLAS import _refreshToACache, ToACache
 
 _refreshToACache()
 
-filename = 'dq2localid.txt'
-
-try:
-    open(filename)
-    fsize = os.stat(filename).st_size
-    dq2localid = [ line.strip() for line in file(filename) ]
-    if dq2localid:
-        dq2localid = dq2localid[0]
-except IOError:
-    dq2localid = os.environ['DQ2_LOCAL_SITE_ID']
-
+dq2localid = os.environ['DQ2_LOCAL_SITE_ID']
 dq2localid_alternatename = TiersOfATLAS.getSiteProperty(dq2localid,'alternateName')
-
+if not dq2localid_alternatename:
+    try:
+        dq2localid_alternatename = dq2localid.split("_")[0]
+    except:
+        dq2localid_alternatename = dq2localid
 db_dataset = os.environ['ATLAS_DBRELEASE']
 
 dq2=DQ2()
 db_locations = dq2.listDatasetReplicas(db_dataset).values()[0][1]
 
+print "db_locations = ", db_locations
+
 db_site = dq2localid
 for sitename in TiersOfATLAS.getAllSources():
-    if TiersOfATLAS.getSiteProperty(sitename,'alternateName')==dq2localid_alternatename and sitename in db_locations:    
+    if TiersOfATLAS.getSiteProperty(sitename,'alternateName')==dq2localid_alternatename and sitename in db_locations:
         db_site = sitename
 
 outFile = open('db_dq2localid.txt','w')
