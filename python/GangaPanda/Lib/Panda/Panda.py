d75feb7fd3977e9ddf74cbbb05b746acf2911a95
Daniel van der Ster <dvanders@gmail.com>
2009-06-08 13:02:10 +0000

    force to submitted (because jobs can go from running to activated)

--- python/GangaPanda/Lib/Panda/Panda.py
+++ python/GangaPanda/Lib/Panda/Panda.py
@@ -1,7 +1,7 @@
 ################################################################################
 # Ganga Project. http://cern.ch/ganga
 #
-# $Id: Panda.py,v 1.41 2009-06-08 08:25:24 dvanders Exp $
+# $Id: Panda.py,v 1.42 2009-06-08 13:02:10 dvanders Exp $
 ################################################################################
                                                                                                               
 
@@ -457,7 +457,7 @@ class Panda(IBackend):
                                 job.backend.reason = job.backend.reason + str(job.backend.jobSpec[k])
 
                         if status.jobStatus in ['defined','unknown','assigned','waiting','activated','sent']:
-                            pass
+                            job.updateStatus('submitted')
                         elif status.jobStatus in ['starting','running','holding','transferring']:
                             job.updateStatus('running')
                         elif status.jobStatus == 'finished':
@@ -491,7 +491,7 @@ class Panda(IBackend):
                             pass
 
                         if status.jobStatus in ['defined','unknown','assigned','waiting','activated','sent','finished']:
-                            pass
+                            job.updateStatus('submitted')
                         elif status.jobStatus in ['starting','running','holding','transferring']:
                             job.updateStatus('running')
                         elif status.jobStatus == 'failed':
@@ -574,6 +574,9 @@ class Panda(IBackend):
 #
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.41  2009/06/08 08:25:24  dvanders
+# backend.CE doesn't exist
+#
 # Revision 1.40  2009/05/30 08:31:59  dvanders
 # limit submit to 2000 subjobs
 #
