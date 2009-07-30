9119f140b08ca65c00be20391642395f53bc3b39
Mike Williams <mwill@mit.edu>
2009-07-30 13:49:13 +0000

    Gauss-Job.py

---
+++ python/GangaLHCb/test/Lib/Gaudi/TestGaudi/Gauss-Job.py
@@ -0,0 +1,31 @@
+#
+# Options specific for a given job
+# ie. setting of random number seed and name of output files
+#
+
+from Gauss.Configuration import *
+
+#--Generator phase, set random numbers
+GaussGen = GenInit("GaussGen")
+GaussGen.FirstEventNumber = 1
+GaussGen.RunNumber        = 1082
+
+#--Number of events
+nEvts = 5
+LHCbApp().EvtMax = nEvts
+LHCbApp().DDDBtag = "I don't care"
+LHCbApp().CondDBtag = "I don't care"    
+
+#--Set name of output files for given job (uncomment the lines)
+#  Note that if you do not set it Gauss will make a name based on event type,
+#  number of events and the date
+idFile = 'Gauss'
+HistogramPersistencySvc().OutputFile = idFile+'Histos.root'
+#
+OutputStream("GaussTape").Output = "DATAFILE='PFN:%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"%idFile
+
+GenMonitor = GaudiSequencer( "GenMonitor" )
+#SimMonitor = GaudiSequencer( "SimMonitor" )
+GenMonitor.Members += [ "GaussMonitor::CheckLifeTimeHepMC/HepMCLifeTime" ]
+#SimMonitor.Members += [ "GaussMonitor::CheckLifeTimeMC/MCLifeTime" ]
+
