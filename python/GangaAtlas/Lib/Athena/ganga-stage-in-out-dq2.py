8bf1ae8257a89ca4b5687dc33135944dbe546f8d
Mark Slater <mslater@cern.ch>
2009-09-14 12:51:39 +0000

    Minor fix for TAG usage with multiple datasets in one job

--- python/GangaAtlas/Lib/Athena/ganga-stage-in-out-dq2.py
+++ python/GangaAtlas/Lib/Athena/ganga-stage-in-out-dq2.py
@@ -1422,13 +1422,7 @@ if __name__ == '__main__':
                 rc, out = getstatusoutput(cmd)
                 print out
 
-                bad_dq2_get = False
-                
-                for f in taglfns:
-                    if not os.path.exists(f):
-                        bad_dq2_get = True
-                        
-                if (rc!=0) or bad_dq2_get:
+                if (rc!=0):
                     print taglfns
                     os.system("ls -ltr")
                     print "ERROR: error during dq2-get occured"
@@ -1436,8 +1430,14 @@ if __name__ == '__main__':
                     print out
                     if (rc!=0):
                         print "ERROR: error during retry of dq2-get occured"
-                        sys.exit(EC_DQ2GET)
 
+
+            # check that all files are present
+            for f in taglfns:
+                if not os.path.exists(f):
+                    print "ERROR: failed to download " + f
+                    sys.exit(EC_DQ2GET)
+                                    
             tagddmFileMap = {}
             for i in xrange(0,len(taglfns)):
                 tagddmFileMap[taglfns[i]] = tagguids[i]
