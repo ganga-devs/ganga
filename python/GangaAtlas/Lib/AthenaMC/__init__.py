6c0aa793fa1fc44f9ec0ce64ab0c1132b78cb6c6
Frédéric Brochu <brochu@hep.phy.cam.ac.uk>
2009-10-14 10:53:10 +0000

    Modified __init__.py to prevent loading of Panda RT Handler if there is something wrong with the Panda module (Panda.lib.Panda classes failed imports)

--- python/GangaAtlas/Lib/AthenaMC/__init__.py
+++ python/GangaAtlas/Lib/AthenaMC/__init__.py
@@ -1,5 +1,17 @@
 from AthenaMC import *
 from AthenaMCLCGRTHandler import *
 from AthenaMCLocalRTHandler import *
-from AthenaMCPandaRTHandler import *
 from AthenaMCDatasets import *
+
+loadPandaRTHandler=True
+try:
+    import GangaPanda.Lib.Panda
+except SystemExit:
+    from Ganga.Utility.logging import getLogger
+    logger.error("Couldn't load Panda Client. Disabling AthenaMC-Panda RT Handler")
+    loadPandaRTHandler=False
+    pass
+if loadPandaRTHandler:
+    from AthenaMCPandaRTHandler import *
+
+                                        
