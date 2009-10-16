02bd8c27bffb05e1eb3b03e72d48ed6272883221
Daniel van der Ster <dvanders@gmail.com>
2009-10-16 13:30:13 +0000

    MSGMS for Athena. Still not functional.

---
+++ python/Ganga/Lib/MonitoringServices/MSGMS/AthenaMSGMS.py
@@ -0,0 +1,37 @@
+from Ganga.Lib.MonitoringServices.MSGMS import MSGMS
+import cPickle as pickle
+
+class AthenaMSGMS(MSGMS):
+
+    def __init__(self,job_info):
+        MSGMS.__init__(self, job_info)
+
+    def getSandboxModules(self):
+        import Ganga.Lib.MonitoringServices.MSGMS.AthenaMSGMS
+        return [
+            Ganga.Lib.MonitoringServices.MSGMS.AthenaMSGMS,
+            ] + MSGMS.getSandboxModules(self)
+
+
+    def stop(self, exitcode, **opts):
+        exit_status = "finished"
+#        if exitcode == 0:
+#            exit_status = "finished"
+#        else:
+#            exit_status = "failed"
+
+        message = MSGMS.getMessage(self)
+        message['event'] = exit_status
+        try:
+            f = open('stats.pickle','r')
+            stats = pickle.load(f)
+            f.close()
+            for (k,v) in stats:
+                message['uat09.'+k]=v
+            message['uat09.stats'] = stats
+        except:
+            pass
+
+        sendJobStatusChange( message )
+
+
