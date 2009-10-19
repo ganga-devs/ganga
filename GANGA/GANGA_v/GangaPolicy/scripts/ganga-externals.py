274976b5726e0d6105e90d25b314e5a23819dd50
Ulrik Egede <U.Egede@imperial.ac.uk>
2009-10-19 14:27:34 +0000

    Fix to problems with finding externals

--- GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
+++ GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
@@ -2,15 +2,17 @@
 ## the lhcb-prepare script
 
 # These are the external packages with only Python code.
-externals_noarch = [['ApMon','2.2.11'],
-                    ['figleaf', '0.6'],
-                    ['paramiko', '1.7.3'],
-                    ['PYTF','1.5'],
-                    ['stomputil','1.0']]
+def ganga-externals_noarch():
+    return [['ApMon','2.2.11'],
+            ['figleaf', '0.6'],
+            ['paramiko', '1.7.3'],
+            ['PYTF','1.5'],
+            ['stomputil','1.0']]
 
 
 # These are the packages with architecture dependent code.
-externals_arch = [['matplotlib','0.99.0'],
-                  ['numpy','1.3.0'],
-                  ['pycrypto','2.0.1']]
-archs = ['slc4_amd64_gcc34','slc4_ia32_gcc34','x86_64-slc5-gcc43-opt']
+def ganga-externals_arch():
+    return [['matplotlib','0.99.0'],
+            ['numpy','1.3.0'],
+            ['pycrypto','2.0.1']], \
+            ['slc4_amd64_gcc34','slc4_ia32_gcc34','x86_64-slc5-gcc43-opt']
