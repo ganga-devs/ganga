b0f1223b6bdfffa4bd85f8ede9baccb87a6ab6ae
Ulrik Egede <U.Egede@imperial.ac.uk>
2009-10-20 16:25:06 +0000

    Update to external packages and new Dirac version

--- GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
+++ GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
@@ -6,11 +6,12 @@ externals_noarch = [['ApMon','2.2.11'],
                     ['figleaf', '0.6'],
                     ['paramiko', '1.7.3'],
                     ['PYTF','1.5'],
-                    ['stomputil','1.0']]
+                    ['stomputil','2.0']]
 
 
 # These are the packages with architecture dependent code.
 externals_arch = [['matplotlib','0.99.0'],
                   ['numpy','1.3.0'],
-                  ['pycrypto','2.0.1']]
+                  ['pycrypto','2.0.1'],
+                  ['pyqt','3.18.1_python2.5']]
 archs = ['slc4_amd64_gcc34','slc4_ia32_gcc34','x86_64-slc5-gcc43-opt']
