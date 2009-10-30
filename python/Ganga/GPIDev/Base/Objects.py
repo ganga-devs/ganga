e8aceb4b67162e52d5d2fd104779d03e32082865
Johannes Ebke <Johannes.Ebke@physik.uni-muenchen.de>
2009-10-30 15:01:10 +0000

    Speed improvement for setter

--- python/Ganga/GPIDev/Base/Objects.py
+++ python/Ganga/GPIDev/Base/Objects.py
@@ -50,7 +50,7 @@ class Node(object):
         for n, v in dict['_data'].items():
             if isinstance(v,Node):
                 v._setParent(self)
-            if (isinstance(v,list) or v.__class__.__name__ == "GangaList") and len(v) > 0:
+            if hasattr(v,"__iter__") and not isinstance(v,dict):
                 # set the parent of the list or dictionary (or other iterable) items
                 for i in v:
                     if isinstance(i,Node):
