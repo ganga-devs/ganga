681b32433b996ee49783134c1974f88563316b12
Hurng-Chun Lee <Hurng-Chun.Lee@cern.ch>
2009-11-05 12:16:20 +0000

    remove StagerDataset/StagerJobSplitter from AMAAthena plugin

--- python/GangaAtlas/Lib/AMAAthena/__init__.py
+++ python/GangaAtlas/Lib/AMAAthena/__init__.py
@@ -2,8 +2,7 @@ from AMAAthena import *
 from AMADriverConfig import *
 from AMAAthenaLocalRTHandler import *
 from AMAAthenaLCGRTHandler import *
-from StagerJobSplitter import *
-from StagerDataset import *
+from AMAAthenaPandaRTHandler import *
 from SimpleStagerBroker import *
 
 ## introduce new DQ2 configuration variable
