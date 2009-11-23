691b46f612d87a8675b9c69b83764f050c875918
David Tuckett <david.tuckett@cern.ch>
2009-11-23 10:34:45 +0000

    * Update task meta message

--- python/Ganga/Lib/MonitoringServices/Dashboard/LCGAthenaMS.py
+++ python/Ganga/Lib/MonitoringServices/Dashboard/LCGAthenaMS.py
@@ -6,7 +6,7 @@ N.B. This code is under development and should not generally be used or relied u
 
 #TODO: disable if backend is not LCG/Athena
 #TODO: send athena-specific messages:
-#      task_meta_message, job_meta_message, job_processing_attributes_message
+#      job_meta_message, job_processing_attributes_message
 
 
 from Ganga.Lib.MonitoringServices.Dashboard import CommonUtil
@@ -49,20 +49,20 @@ class LCGAthenaMS(LCGMS):
     def _cl_task_meta_message(self):
         j = self.job_info # called on client, so job_info is Job object
         msg = {
-            'TASKNAME': LCGUtil.cl_task_name(j),
-            'TASKTYPE': cl_task_type(self.config_info), # e.g. analysis, production, hammercloud etc.
-            'OWNERDN': LCGUtil.cl_ownerdn(),
-            'JSTOOL': 'Ganga', # e.g. Ganga, Panda
-            'JSTOOLUI': cl_jstoolui(), # hostname of client
-            'SUBMISSIONTYPE': 'direct',
             'APPLICATION': cl_application(j), # e.g. Athena
             'APPLICATIONVERSION': cl_application_version(j), # e.g. 15.5.1
             'INPUTDATASET': cl_input_dataset(j), # e.g. fdr08_run2.0052283.physics_Muon.merge.AOD.o3_f8_m10
-            'OUTPUTDATASET': cl_output_dataset(j),# e.g. user09.DavidTuckett.ganga.387.20091119
-            'OUTPUTSE': None, # Unknown
-            'TARGET': None, # Unknown
+            'JSTOOL': 'Ganga', # e.g. Ganga, Panda
+            'JSTOOLUI': cl_jstoolui(), # hostname of client
+            'OUTPUTDATASET': None,# Unknown at submission
+            'OUTPUTSE': None, # Unknown at submission
+            'OWNERDN': LCGUtil.cl_ownerdn(),
             'REPORTER': 'ToolUI',
             'REPORTTIME': CommonUtil.utcnow(),
+            'SUBMISSIONTYPE': 'direct',
+            'TARGET': cl_target(j), # e.g. CE_xxx,SITE_xxx,SITE_xxx
+            'TASKNAME': LCGUtil.cl_task_name(j),
+            'TASKTYPE': cl_task_type(self.config_info), # e.g. analysis, production, hammercloud etc.
             '___fqid' : j.fqid,
             }
         return msg
@@ -104,14 +104,6 @@ class LCGAthenaMS(LCGMS):
 #TODO: add error handling code in following methods
 #TODO: move to appropriate XxxUtil modules
 
-def cl_task_type(config):
-    """Build task_type. Only run on client."""
-    return config['task_type']
-
-def cl_jstoolui():
-    """Build jstoolui. Only run on client."""
-    return CommonUtil.hostname()
-
 def cl_application(job):
     """Build application. Only run on client."""
     return CommonUtil.strip_to_none(job.application.atlas_exetype)
@@ -129,12 +121,27 @@ def cl_input_dataset(job):
     datasetcsv = ','.join(job.inputdata.dataset)
     return CommonUtil.strip_to_none(datasetcsv)
 
-def cl_output_dataset(job):
-    """Build output_dataset. Only run on client."""
-    datasets = []
-    for j in job.subjobs:
-        dataset = j.outputdata.datasetname
-        if dataset and dataset not in datasets:
-            datasets.append(dataset)
-    datasetcsv = ','.join(datasets)
-    return CommonUtil.strip_to_none(datasetcsv)
+def cl_jstoolui():
+    """Build jstoolui. Only run on client."""
+    return CommonUtil.hostname()
+
+def cl_target(job):
+    """Build target. Only run on client."""
+    ces = []
+    sites = []
+    targets = []
+    for j in [job] + job.subjobs:
+        ce = j.backend.CE
+        if ce and ce not in ces:
+            ces.append(ce)
+            targets.append('CE_%s' % ce)
+        for site in j.backend.requirements.sites:
+            if site and site not in sites:
+                sites.append(site)
+                targets.append('SITE_%s' % site)
+    targetcsv = ','.join(targets)
+    return CommonUtil.strip_to_none(targetcsv)
+
+def cl_task_type(config):
+    """Build task_type. Only run on client."""
+    return config['task_type']
