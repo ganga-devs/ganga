47eac87b34c991e9965c79a842c08b910258cb26
muenchen <muenchen@example.com>
2010-02-26 08:50:33 +0000

    Yet another JEM lib version bump

--- python/GangaJEM/PACKAGE.py
+++ python/GangaJEM/PACKAGE.py
@@ -10,7 +10,7 @@ Refer to Ganga/PACKAGE.py for details on the purpose of this module.
 
 external_packages = {
                      'JEM' : {
-                              'version' : '0.2.7.2',
+                              'version' : '0.2.7.3',
                               'noarch' : True
                               },
 }
