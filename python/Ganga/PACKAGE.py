b38d3e132a615dd777efac17ab9ca138d5991794
Jakub Moscicki <Jakub.Moscicki@cern.ch>
2010-04-16 14:37:04 +0000

    move to ipython 0.8.4

--- python/Ganga/PACKAGE.py
+++ python/Ganga/PACKAGE.py
@@ -45,7 +45,7 @@ _externalPackages = {
               'noarch':True,
               'syspath'       : 'lib/python2.2/site-packages',
               'maxHexVersion' : '0x20300f0'}, #maximum Python version number for adding Optik to path
-   'ipython': {'version' : '0.6.13',
+   'ipython': {'version' : '0.8.4',
                'noarch':True,
                'syspath' : 'lib/python'},
    'ApMon' : {'version' : '2.2.11', 
