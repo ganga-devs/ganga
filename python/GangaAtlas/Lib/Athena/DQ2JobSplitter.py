6bfb1d2869192e0c8ea16ed23f8e3f122a6fcce1
Daniel van der Ster <dvanders@gmail.com>
2010-05-20 22:40:29 +0000

    allow up to 5000 subjobs

--- python/GangaAtlas/Lib/Athena/DQ2JobSplitter.py
+++ python/GangaAtlas/Lib/Athena/DQ2JobSplitter.py
@@ -522,7 +522,7 @@ class DQ2JobSplitter(ISplitter):
         return subjobs
     
 config = getConfig('Athena')
-config.addOption('MaxJobsDQ2JobSplitter', 1000, 'Maximum number of allowed subjobs of DQ2JobSplitter')
+config.addOption('MaxJobsDQ2JobSplitter', 5000, 'Maximum number of allowed subjobs of DQ2JobSplitter')
 config.addOption('MaxFileSizeNGDQ2JobSplitter', 14336, 'Maximum total sum of filesizes per subjob of DQ2JobSplitter at the NG backend (in MB)')
 config.addOption('MaxFileSizePandaDQ2JobSplitter', 14336, 'Maximum total sum of filesizes per subjob of DQ2JobSplitter at the Panda backend (in MB)')
 config.addOption('AllowedSitesNGDQ2JobSplitter', [ 'NDGF-T1_DATADISK', 'NDGF-T1_MCDISK', 'NDGF-T1_PRODDISK', 'NDGF-T1_SCRATCHDISK' ], 'Allowed space tokens/sites for DQ2JobSplitter on NG backend' )
