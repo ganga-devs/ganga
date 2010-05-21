e5ee964cffb6c6f61787c76a3baaffa14bfd940e
Daniel van der Ster <dvanders@gmail.com>
2010-05-21 07:31:38 +0000

    panda-client 0.2.44 (ignore TZERO and DAQ space tokens)

--- python/GangaPanda/PACKAGE.py
+++ python/GangaPanda/PACKAGE.py
@@ -8,7 +8,7 @@
 """
 
 _external_packages = {
-    'panda-client' : { 'version' : '0.2.43', 
+    'panda-client' : { 'version' : '0.2.44', 
                     'PYTHONPATH':['lib/python2.4/site-packages'],
                     'CONFIGEXTRACTOR_PATH':'etc/panda/share',
                     'PANDA_SYS':'.',
