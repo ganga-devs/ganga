3ca88d67ce1e422360d5563eccdea94658e4b924
David Tuckett <david.tuckett@cern.ch>
2010-05-27 15:04:45 +0000

    Update stomputil to version 2.3.

--- python/Ganga/PACKAGE.py
+++ python/Ganga/PACKAGE.py
@@ -64,7 +64,7 @@ _externalPackages = {
                  'syspath':'lib/python2.3/site-packages'},
    'pycrypto' : {'version' : '2.0.1',
                  'syspath':'lib/python2.3/site-packages'},
-   'stomputil' : {'version' : '2.2',
+   'stomputil' : {'version' : '2.3',
                   'noarch': True,
                   'syspath' : 'python'} 
    }
