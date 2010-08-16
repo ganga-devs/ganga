c958358f0097acbd3bf8862c7fbc8143a5d78622
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2010-08-16 11:16:06 +0000

    Sort pfns

--- python/GangaAtlas/Lib/Athena/make_filestager_joption.py
+++ python/GangaAtlas/Lib/Athena/make_filestager_joption.py
@@ -172,6 +172,9 @@ if (io_type in ['FILE_STAGER']):
     ic_jo_path = os.path.join(os.getcwd(), 'input.py')
     fs_jo_path = os.path.join(os.getcwd(), 'FileStager_jobOption.py')
 
+    # sort pfn_list
+    pfn_list.sort()
+
     dm_util.make_FileStager_jobOption(pfn_list, gridcopy=True, protocol=my_protocol, maxEvent=evtmax, skipEvent=skipevt, fs_cp_cmd=fs_cp_cmd, fs_cp_args=fs_cp_args, fs_of_prefix=fs_of_prefix, ic_jo_path=ic_jo_path, fs_jo_path=fs_jo_path)
 
     dq2tracertime.append(time.time())
