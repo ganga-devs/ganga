52083ceafb2998a94da86941ab9ea616494ffbcd
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2010-09-03 09:41:36 +0000

    added sorting plugin for columns that are html tags, but we want to sort the integer inside the html tag

--- python/Ganga/Core/WebMonitoringGUI/client/media/scripts/components/idfn.datatable.sorting.js
+++ python/Ganga/Core/WebMonitoringGUI/client/media/scripts/components/idfn.datatable.sorting.js
@@ -17,3 +17,33 @@ jQuery.fn.dataTableExt.oSort['numeric-float-desc'] = function(a,b) {
 
 	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
 };
+
+jQuery.fn.dataTableExt.oSort['integer-in-tag-asc']  = function(a,b) {
+	
+	startIndexA = a.indexOf('>') + 1
+	endIndexA = a.indexOf('</')
+	x = a.substring(startIndexA, endIndexA)
+	x = parseInt(x)
+
+	startIndexB = b.indexOf('>') + 1
+	endIndexB = b.indexOf('</')
+	y = b.substring(startIndexB, endIndexB)
+	y = parseInt(y)
+	
+	return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort['integer-in-tag-desc'] = function(a,b) {
+	
+	startIndexA = a.indexOf('>') + 1
+	endIndexA = a.indexOf('</')
+	x = a.substring(startIndexA, endIndexA)
+	x = parseInt(x)
+
+	startIndexB = b.indexOf('>') + 1
+	endIndexB = b.indexOf('</')
+	y = b.substring(startIndexB, endIndexB)
+	y = parseInt(y)
+
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
