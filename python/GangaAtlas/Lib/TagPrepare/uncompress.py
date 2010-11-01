a527d3a0a7bf094fe6875b9c6da77cc5c22b009b
Mark Slater <mslater@cern.ch>
2010-11-01 12:04:55 +0000

    Added the event and run number to inflated TAG file

--- python/GangaAtlas/Lib/TagPrepare/uncompress.py
+++ python/GangaAtlas/Lib/TagPrepare/uncompress.py
@@ -1,8 +1,24 @@
-import os, commands
+import os, commands, sys, re
 from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
 
 rc, out = commands.getstatusoutput('tar -zxf subcoll.tar.gz')
-print out                         
+print out
+
+#rc, out = commands.getstatusoutput('cp ../PoolFileCatalog.xml .')
+#print out                         
+
+print "------     ADdding to PFC"
+if len(sys.argv) > 1:
+    
+    for ln in open('input_tag.txt').readlines():
+        ln = ln.strip()
+        rc, out = commands.getstatusoutput("pool_insertFileToCatalog %s" % ln)
+        print out
+
+            
+rc, out = commands.getstatusoutput('more PoolFileCatalog.xml')
+print out
+
 
 for a in os.listdir('.'):
     print ":::    " + a
@@ -15,45 +31,70 @@ print "Loaded tag_info: "
 print tag_info
 stream_ref = ''
 
-# find which .dat file to inflate
-for ln in open('PoolFileCatalog.xml').readlines():
-    if ln.find('<File ID="') == -1:
-        continue
+os.system('chmod +x CollInflateEventInfo.exe')
 
-    guid = ln[ ln.find('"')+1: ln.rfind('"')]
-    print "Found GUID " + guid
-    infile = ''
-    for tf in tag_info:
-        for r in tag_info[tf]['refs']:
-            if r[2] == guid:
-                if r[1].find('AOD') != -1:
-                    stream_ref = "StreamAOD_ref"
-                infile = tf
+if len(sys.argv) > 1:
+    # not running with Athena - do the appropriates
+    for f in sys.argv[1:]:
+
+        # inflate first
+        print "inflating " + f
+        cmd = "export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ; ./CollInflateEventInfo.exe " + f
+        rc, out = commands.getstatusoutput(cmd)
+        
+        print out
+        
+        rc, out = commands.getstatusoutput("mv outColl.root %s" % f+".root")
+        
+        append_list.append(f)
+        
+        # now update the PFC
+        for ref in tag_info[f]['refs']:
+            dataset = ref[1]
+            fname = ref[0]
+
+            
+else:
+    
+    # find which .dat file to inflate
+    for ln in open('PoolFileCatalog.xml').readlines():
+        if ln.find('<File ID="') == -1:
+            continue
+
+        guid = ln[ ln.find('"')+1: ln.rfind('"')]
+        print "Found GUID " + guid
+        infile = ''
+        for tf in tag_info:
+            for r in tag_info[tf]['refs']:
+                if r[2] == guid:
+                    if r[1].find('AOD') != -1:
+                        stream_ref = "StreamAOD_ref"
+                    infile = tf
+                    break
+            if infile != '':
                 break
-        if infile != '':
-            break
 
-    if infile == '':
-        print "NO MATCHING FILE"
-        continue
+        if infile == '':
+            print "NO MATCHING FILE"
+            continue
 
-    # inflate the dat file
-    print "Found file " + infile
+        # inflate the dat file
+        print "Found file " + infile
 
-    if infile in append_list:
-        print "Already inflated. continuing..."
-        continue
+        if infile in append_list:
+            print "Already inflated. continuing..."
+            continue
     
-    filename = infile
+        filename = infile
 
-    cmd = "export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ; ./CollInflateEventInfo.exe " + filename
-    rc, out = commands.getstatusoutput(cmd)
+        cmd = "export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ; ./CollInflateEventInfo.exe " + filename
+        rc, out = commands.getstatusoutput(cmd)
 
-    print out
+        print out
     
-    rc, out = commands.getstatusoutput("mv outColl.root %s" % filename+".root")
+        rc, out = commands.getstatusoutput("mv outColl.root %s" % filename+".root")
     
-    append_list.append(filename)
+        append_list.append(filename)
 
 
 for a in os.listdir('.'):
@@ -72,6 +113,10 @@ rc, out = commands.getstatusoutput(cmd)
 print cmd
 print out
 
+if len(sys.argv) > 1:
+    os.rename(outname + ".newcoll.root", "outColl.root")
+    sys.exit(0)
+    
 print "OVERWRITING INPUT FILES"
 
 cmd = "CollSplitByGUID.exe -splitref "+stream_ref+" -src PFN:"+outname+".newcoll.root RootCollection" 
@@ -94,50 +139,4 @@ for tmpName in filesInCurDir:
         
                   
 
-
-
-## try:
-##     from EventSelectorAthenaPool.EventSelectorAthenaPoolConf import EventSelectorAthenaPool
-##     orig_ESAP__getattribute2 =  EventSelectorAthenaPool.__getattribute__
-
-##     def _dummy2(self,attr):
-##         if attr == 'InputCollections':
-##             return [outname + ".newcoll.root"]
-##         else:
-##             return orig_ESAP__getattribute2(self,attr)
-
-##     EventSelectorAthenaPool.__getattribute__ = _dummy
-##     print 'Overwrite InputCollections'
-##     print EventSelectorAthenaPool.InputCollections
-## except:
-##     try:
-##         EventSelectorAthenaPool.__getattribute__ = orig_ESAP__getattribute2
-##     except:
-##         pass
-      
-## try:
-##     import AthenaCommon.AthenaCommonFlags
-
-##     def _dummyFilesInput2(*argv):
-##         return [outname + ".newcoll.root"]
-
-##     AthenaCommon.AthenaCommonFlags.FilesInput.__call__ = _dummyFilesInput2
-## except:
-##     pass
-
-## try:
-##     import AthenaCommon.AthenaCommonFlags
-
-##     def _dummyGet_Value2(*argv):
-##         return [outname + ".newcoll.root"]
-
-##     for tmpAttr in dir (AthenaCommon.AthenaCommonFlags):
-##         import re
-##         if re.search('^(Pool|BS).*Input$',tmpAttr) != None:
-##             try:
-##                 getattr(AthenaCommon.AthenaCommonFlags,tmpAttr).get_Value = _dummyGet_Value2
-##             except:
-##                 pass
-## except:
-##     pass
 
