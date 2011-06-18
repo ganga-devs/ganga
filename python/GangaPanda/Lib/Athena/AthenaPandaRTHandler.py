43f924466b6147bbd016483103bede43c85b8554
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2011-06-18 08:46:55 +0000

    Fix Tier3+runGen problem

--- python/GangaPanda/Lib/Athena/AthenaPandaRTHandler.py
+++ python/GangaPanda/Lib/Athena/AthenaPandaRTHandler.py
@@ -883,7 +883,7 @@ class AthenaPandaRTHandler(IRuntimeHandler):
             param += '--accessmode=direct '
         elif job.backend.accessmode == 'COPY2SCRATCH':
             param += '--accessmode=copy '
-        if self.inputdatatype == 'Tier3':
+        if self.inputdatatype == 'Tier3' and not app.atlas_exetype in ['PYARA','ARES','ROOT','EXE']:
             param += '--givenPFN '
  
         jspec.jobParameters = param
