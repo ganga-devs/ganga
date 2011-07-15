22320f604065dca98b4fd0abdee81591d9fcd4bb
Mike Kenyon <mkenyon@cern.ch>
2011-07-15 14:14:39 +0000

    Changes to allow more intuitive and efficient use of FileBuffer.
    From:
    FileBuffer('myfile.py',"import os\n print Blah").create('myfile.py')
     
    To:
    FileBuffer('myfile.py',"import os\n print Blah").create()

--- python/Ganga/GPIDev/Lib/File/FileBuffer.py
+++ python/Ganga/GPIDev/Lib/File/FileBuffer.py
@@ -33,13 +33,15 @@ class FileBuffer:
         else:
             return self._contents.read()
 
-    def create(self,outname):
+    def create(self,outname=None):
         """create a file in a local filesystem as 'outname' """
-        file(outname,'w').write(self.getContents())
+        if outname not None: file(outname,'w').write(self.getContents())
+        else: file(self.name,'w').write(self.getContents())
 
         if self.executable:
             from Ganga.Utility.files import chmod_executable
             chmod_executable(outname)
+        return self
 
 
     def isExecutable(self):
