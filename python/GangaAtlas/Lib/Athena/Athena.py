3f92a94b4db278c5c23c711f84197e8c6fc707f1
Mark Slater <mslater@cern.ch>
2011-08-08 12:52:42 +0000

    Allowed TRF and EXE to not need to specify option_files and fixed a minor bug with create_tarball

--- python/GangaAtlas/Lib/Athena/Athena.py
+++ python/GangaAtlas/Lib/Athena/Athena.py
@@ -135,6 +135,16 @@ def create_tarball( userarea, runDir, currentDir, archiveDir, extFile, excludeFi
     # gather files under work dir
     logger.info("gathering files under %s", userarea)
 
+    # change from glob to re
+    excludeFile2 = []
+    for tmpItem in excludeFile:
+        # change . to \. for regexp
+        tmpItem = tmpItem.replace('.','\.')        
+        # change * to .* for regexp
+        tmpItem = tmpItem.replace('*','.*')
+        # append
+        excludeFile2.append(tmpItem)
+        
     # get files in the working dir
     skippedExt   = ['.o','.a','.so']
     skippedFlag  = False
@@ -154,7 +164,7 @@ def create_tarball( userarea, runDir, currentDir, archiveDir, extFile, excludeFi
                 continue
             # check exclude files
             excludeFileFlag = False
-            for tmpPatt in excludeFile:
+            for tmpPatt in excludeFile2:
                 if re.search(tmpPatt,tmpPath) != None:
                     excludeFileFlag = True
                     break
@@ -928,7 +938,7 @@ class Athena(IApplication):
             else:
                 jobO = ' -c %s ' % self.options
 
-        if not self.option_file:
+        if not self.option_file and not self.atlas_exetype in ['EXE', 'TRF']:
             raise ApplicationConfigurationError(None,'Set option_file before calling prepare()')
         for opt_file in self.option_file:
             if not self.atlas_exetype in ['EXE', 'TRF']: 
