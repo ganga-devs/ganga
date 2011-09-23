b7487e2cc295ed8ce9c87549990e5a5c9c63be23
Alexander Richards <arichard@cern.ch>
2011-09-23 10:17:21 +0000

    Re-writing the LHCb Tasks

---
+++ python/GangaLHCb/Lib/Tasks/LHCbTaskDummySplitter.py
@@ -0,0 +1,25 @@
+from Ganga.GPIDev.Base.Proxy import *
+from Ganga.GPIDev.Adapters.ISplitter import ISplitter
+from Ganga.GPIDev.Schema import *
+class LHCbTaskDummySplitter(ISplitter):
+    _schema = Schema(Version(1,0),{
+        'orig_splitter' : ComponentItem('splitters',defvalue=None,load_default=0,optional=1,doc='original splitter'),
+        })
+    _category= 'splitters'
+    _name = 'LHCbTaskDummySplitter'
+    _exportmethods=[]
+    
+    
+    def __init__(self, splitter):
+        super(LHCbTaskDummySplitter,self).__init__()
+        self.orig_splitter = splitter
+
+    def clone(self):
+        return LHCbTaskDummySplitter(self.orig_splitter)
+    
+    def split(self,job):
+        subjobs = self.orig_splitter.validatedSplit(job)
+        for sj in subjobs:
+            #sj.application.id = -1
+            sj.name += ':%i'%(subjobs.index(sj))
+        return subjobs
--- python/GangaLHCb/Lib/Tasks/TaskApplication.py
+++ python/GangaLHCb/Lib/Tasks/TaskApplication.py
@@ -11,10 +11,20 @@ from GangaLHCb.Lib.Gaudi.Gaudi import *
 #must take care of "taskifying" all possible apps
 from GangaLHCb.Lib.Gaudi.GaudiUtils import available_apps
 from Ganga.GPIDev.Lib.Tasks.TaskApplication import task_map
+## from Ganga.GPIDev.Base.Proxy import *
+## from Ganga.GPIDev.Schema import *
 
 for app in available_apps():
     exec('%sTask = taskify(%s,"%sTask")' %(app,app,app))
+#    exec('smajor = %sTask._schema.version.major'%app)
+#    exec('sminor = %sTask._schema.version.minor'%app)
+#    exec('schema_items = {\'extra\':ComponentItem(\'extras\',defvalue=GaudiExtras(),load_default=1,Hidden=1,protected=0,optional=1,copyable=1,doc=\'Used to pass extra info from Gaudi apps to the RT-handler.\')}.items()')
+#    exec('%sTask._schema = Schema()')
+
+#    exec('%sTask._schema = Schema(Version(smajor,sminor), dict(%sTask._schema.datadict.items() + schema_items ))'%(app,app))
+#    exec('%sTask._name = \'%sTask\''%(app,app))
     exec('task_map["%s"] = %sTask' %(app,app))
+#    exec('print \'ALEX HERE - \',%sTask._schema.getDefaultValue(\'extra\')'%app)
 
 from GangaLHCb.Lib.Gaudi.GaudiPython import GaudiPython
 GaudiPythonTask = taskify(GaudiPython,"GaudiPythonTask")
