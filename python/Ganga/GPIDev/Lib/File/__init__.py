ee4f04247132f1d7d25ab3c16ebdeccabf6bb760
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2011-10-13 09:05:20 +0000

    import the output files

--- python/Ganga/GPIDev/Lib/File/__init__.py
+++ python/Ganga/GPIDev/Lib/File/__init__.py
@@ -1,3 +1,10 @@
 from File import File
 from FileBuffer import FileBuffer
 
+from OutputFile import OutputFile
+from CompressedFile import CompressedFile
+from CastorFile import CastorFile
+from ScratchFile import ScratchFile
+from LHCbDataFile import LHCbDataFile
+
+
