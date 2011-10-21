237de2df7cf54b87cc6c6c590a0ffeeed506a392
Ramon Medrano Llamas <rmedrano@example.com>
2011-10-21 13:01:19 +0000

    Make several improvements on ProdTrans:  * Added the input_type options to the ProdTrans app to control the input_type (for instance, 'Evgen').  * Added the output_type in the symmetric way (for instance, 'Hits').  * Improved the way DBRelease is handled (to not add DBRelease, set it to None).

--- python/GangaPanda/Lib/ProdTrans/ProdTrans.py
+++ python/GangaPanda/Lib/ProdTrans/ProdTrans.py
@@ -31,6 +31,10 @@ class ProdTrans(IApplication):
                                          doc='Generate output file names with random suffix'),
             'max_events': SimpleItem(defvalue=0,
                                      docs='Max events'),
+            'output_type': SimpleItem(defvalue='NTUP_TOP',
+                                      doc='Type of the output file'),
+            'input_type': SimpleItem(defvalue='',
+                                     doc='Type of the input file')
             })
     _category = 'applications'
     _name = 'ProdTrans'
