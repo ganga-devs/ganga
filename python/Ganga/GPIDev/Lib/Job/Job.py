64fb46e700265494d83d5aa59bf2703467103f95
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2011-12-14 09:33:45 +0000

    changes for output stored on lcg se

--- python/Ganga/GPIDev/Lib/Job/Job.py
+++ python/Ganga/GPIDev/Lib/Job/Job.py
@@ -611,16 +611,25 @@ class Job(GangaObject):
 
         from Ganga.GPIDev.Lib.File.FileBuffer import FileBuffer
 
+        from Ganga.Utility.Config import getConfig
+
         content = ''
 
         if len(self.outputfiles) > 0:
             for outputFile in self.outputfiles:
                 if outputFile.__class__.__name__ == 'CompressedFile':
+
                     content += 'zipped %s\n' % outputFile.name  
+
                 elif outputFile.__class__.__name__ == 'MassStorageFile': 
-                    from Ganga.Utility.Config import getConfig 
+
                     massStorageConfig = getConfig('MassStorageOutput')  
                     content += 'massstorage %s %s %s %s %s\n' % (outputFile.name , massStorageConfig['mkdir_cmd'],  massStorageConfig['cp_cmd'], massStorageConfig['ls_cmd'], massStorageConfig['path'])
+
+                elif outputFile.__class__.__name__ == 'LCGStorageElementFile':
+
+                    lcgSEConfig = getConfig('LCGStorageElementOutput')
+                    content += 'lcgse %s %s %s\n' % (outputFile.name , lcgSEConfig['LFC_HOST'],  lcgSEConfig['dest_SRM'])
 
         if content is not '':
             self.getInputWorkspace().writefile(FileBuffer('__postprocessoutput__', content))
