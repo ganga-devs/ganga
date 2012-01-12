acccacc4960ed82b28d7fdf06733d08a998f56e5
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-01-12 17:08:40 +0000

    added methods for setting the location of the uploaded output files

--- python/Ganga/GPIDev/Lib/File/LCGStorageElementFile.py
+++ python/Ganga/GPIDev/Lib/File/LCGStorageElementFile.py
@@ -9,17 +9,32 @@ from Ganga.GPIDev.Schema import *
 from OutputFile import OutputFile
 
 class LCGStorageElementFile(OutputFile):
-    """LCGStorageElementFile represents a class marking a file to be written into LCG SE
+    """LCGStorageElementFile represents a class marking an output file to be written into LCG SE
     """
-    _schema = Schema(Version(1,1), {'name': SimpleItem(defvalue="",doc='name of the file')})
+    _schema = Schema(Version(1,1), {
+        'name'        : SimpleItem(defvalue="",doc='name of the file'),
+        'se'          : SimpleItem(defvalue='', copyable=1, doc='the LCG SE hostname'),
+        'se_type'     : SimpleItem(defvalue='srmv2', copyable=1, doc='the LCG SE type'),
+        'se_rpath'    : SimpleItem(defvalue='generated', copyable=1, doc='the relative path to the VO directory on the SE'),
+        'lfc_host'    : SimpleItem(defvalue='', copyable=1, doc='the LCG LFC hostname'),
+        'srm_token'   : SimpleItem(defvalue='', copyable=1, doc='the SRM space token, meaningful only when se_type is set to srmv2')
+})
     _category = 'outputfiles'
     _name = "LCGStorageElementFile"
+    _location = []
+    _exportmethods = [ "location" , "setLocation" ]
 
     def __init__(self,name='', **kwds):
         """ name is the name of the output file that has to be written into LCG SE
         """
         super(LCGStorageElementFile, self).__init__(name, **kwds)
 
+        print self._schema.name
+
+    def __setattr__(self, attr, value):
+        if attr == 'se_type' and value not in ['','srmv1','srmv2','se']:
+            raise AttributeError('invalid se_type: %s' % value)
+        super(LCGStorageElementFile,self).__setattr__(attr, value)
 
     def __construct__(self,args):
         super(LCGStorageElementFile,self).__construct__(args)
@@ -29,6 +44,20 @@ class LCGStorageElementFile(OutputFile):
         """Get the representation of the file."""
 
         return "LCGStorageElementFile(name='%s')"% self.name
+
+    def setLocation(self, location):
+        """
+        Return list with the locations of the post processed files (if they were configured to upload the output somewhere)
+        """
+        if location not in self._location:
+            self._location.append(location)
+        
+    def location(self):
+        """
+        Return list with the locations of the post processed files (if they were configured to upload the output somewhere)
+        """
+        return self._location
+
 
 
 # add LCGStorageElementFile objects to the configuration scope (i.e. it will be possible to write instatiate LCGStorageElementFile() objects via config file)
