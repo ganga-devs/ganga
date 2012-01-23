ed79cfb43e1183c7f467facc85ecfcaf8e0fcba1
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-01-23 15:31:10 +0000

    raise config error if filename is defined more than once in output config section

--- python/Ganga/GPIDev/Lib/File/OutputFile.py
+++ python/Ganga/GPIDev/Lib/File/OutputFile.py
@@ -83,10 +83,17 @@ def findOutputFileTypeByFileName(filename):
     if dotIndex > -1:
         filename = filename[dotIndex+1:]        
 
+    matchCount = 0
+
     for key in outputfilesConfig.keys():
 
         if filename in outputfilesConfig[key]:
-            return key
+            matchCount += 1
+
+    if matchCount == 1:
+        return key
+    elif matchCount > 1:        
+        raise ConfigError('filename %s defined more than once in [Output] config section' % filename)
  
     return None
 
@@ -107,7 +114,6 @@ def string_file_shortcut(v,item):
             elif key == 'LCGStorageElementFile':
                 return LCGStorageElementFile._proxyClass(v)._impl                                
 
-        
         return OutputFile._proxyClass(v)._impl
 
     return None 
