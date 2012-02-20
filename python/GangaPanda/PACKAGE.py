aa33572f0c272953868e1127f8c9c1ecb28b8747
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2012-02-20 08:45:08 +0000

    Update to panda-client 0.3.85

--- python/GangaPanda/PACKAGE.py
+++ python/GangaPanda/PACKAGE.py
@@ -8,7 +8,7 @@
 """
 
 _external_packages = {
-    'panda-client' : { 'version' : '0.3.80', 
+    'panda-client' : { 'version' : '0.3.85', 
                     'PYTHONPATH':['lib/python2.4/site-packages'],
                     'CONFIGEXTRACTOR_PATH':'etc/panda/share',
                     'PANDA_SYS':'.',
