2cf2f92291e98456de82bbb61b8783eb27d9d53b
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-02-20 17:04:14 +0000

    removed not needed imports

--- python/Ganga/GPIDev/Lib/File/__init__.py
+++ python/Ganga/GPIDev/Lib/File/__init__.py
@@ -2,11 +2,8 @@ from File import File
 from File import ShareDir
 from FileBuffer import FileBuffer
 
-from OutputFile import OutputFile
-from CompressedFile import CompressedFile
+from OutputSandboxFile import OutputSandboxFile
 from MassStorageFile import MassStorageFile
-from ScratchFile import ScratchFile
-from LHCbDataFile import LHCbDataFile
 from LCGStorageElementFile import LCGStorageElementFile
 
 
