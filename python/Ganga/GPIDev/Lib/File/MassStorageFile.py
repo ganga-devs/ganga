2f247c0d676e64388088e51c496cbecf86d49ea0
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-02-21 15:12:27 +0000

    added put method

--- python/Ganga/GPIDev/Lib/File/MassStorageFile.py
+++ python/Ganga/GPIDev/Lib/File/MassStorageFile.py
@@ -68,6 +68,65 @@ class MassStorageFile(OutputSandboxFile):
             targetLocation = os.path.join(dir, os.path.basename(location))      
             os.system('%s %s %s' % (cp_cmd, location, targetLocation))
 
+    def put(self):
+        """
+        Creates and executes commands for file upload to mass storage (Castor), this method will
+        be called on the client for files that are not been uploaded from the WN
+        """     
+        import glob
+        import os
+        import re
+
+        from Ganga.Utility.Config import getConfig
+        massStorageConfig = getConfig('MassStorageOutput') 
+
+        #if Castor mass storage (we understand from the nsls command)
+        if massStorageConfig['ls_cmd'] == 'nsls':
+            host = getConfig('System')['GANGA_HOSTNAME']
+            lxplusHost = re.match('lxplus.*cern\.ch', host)
+            if lxplusHost is None:
+                logger.warning('Output files can be uploaded to Castor only from lxplus')
+                logger.warning('skipping %s for uploading to Castor' % outputFile.name)
+                return 
+
+            mkdir_cmd = massStorageConfig['mkdir_cmd']
+            cp_cmd = massStorageConfig['cp_cmd']
+            ls_cmd = massStorageConfig['ls_cmd']
+            massStoragePath = massStorageConfig['path']
+
+            pathToDirName = os.path.dirname(massStoragePath)
+            dirName = os.path.basename(massStoragePath)
+
+            (exitcode, mystdout, mystderr) = self.execSyscmdSubprocess('nsls %s' % pathToDirName)
+            if exitcode != 0:
+                logger.warning('Error while executing nsls %s command, be aware that Castor commands can be executed only from lxplus, also check if the folder name is correct and existing' % pathToDirName, mystderr)
+                logger.warning('skipping %s for uploading to Castor' % outputFile.name)
+                return
+
+            directoryExists = False 
+            for directory in mystdout.split('\n'):
+                if directory.strip() == dirName:
+                    directoryExists = True
+                    break
+
+            if not directoryExists:
+                (exitcode, mystdout, mystderr) = self.execSyscmdSubprocess('%s %s' % (mkdir_cmd, massStoragePath))
+                if exitcode != 0:
+                    logger.warning('Error while executing %s %s command, check if the ganga user has rights for creating directories in this folder' % (mkdir_cmd, massStoragePath))
+                    logger.warning('skipping %s for uploading to Castor' % outputFile.name)
+                    continue
+            
+            for currentFile in glob.glob(os.path.join(self.joboutputdir, self.name)):
+                (exitcode, mystdout, mystderr) = self.execSyscmdSubprocess('%s %s %s' % (cp_cmd, currentFile, massStoragePath))
+                if exitcode != 0:
+                    logger.warning('Error while executing %s %s %s command, check if the ganga user has rights for uploading files to this mass storage folder' % (cp_cmd, currentFile, massStoragePath))
+                else:
+                    logger.info('%s successfully uploaded to mass storage' % currentFile)              
+                    self.setLocation(os.path.join(massStoragePath, os.path.basename(currentFile)))
+                    #remove file from output
+                    os.system('rm %s' % os.path.join(self.joboutputdir, currentFile))
+
+
 
 # add MassStorageFile objects to the configuration scope (i.e. it will be possible to write instatiate MassStorageFile() objects via config file)
 import Ganga.Utility.Config
