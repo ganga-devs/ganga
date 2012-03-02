9b600a53a45cedf2bacb69188d237c94bcb1f09d
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-03-02 10:12:12 +0000

    added debug info

--- python/Ganga/GPIDev/Lib/File/OutputSandboxFile.py
+++ python/Ganga/GPIDev/Lib/File/OutputSandboxFile.py
@@ -53,7 +53,9 @@ class OutputSandboxFile(GangaObject):
         import os
 
         if self.compressed:
+            print 'compressed'
             for currentFile in glob.glob(os.path.join(self.joboutputdir, self.name)):
+                print 'matched pattern'
                 os.system("gzip %s" % currentFile)
 
     def execSyscmdSubprocess(self, cmd):
