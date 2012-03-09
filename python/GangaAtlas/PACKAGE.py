2e5e13b17d3397d0b475a2e3d78132fd330b47c5
Daniel van der Ster <dvanders@gmail.com>
2012-03-09 07:31:55 +0000

    update to panda-client 0.3.92

--- python/GangaAtlas/PACKAGE.py
+++ python/GangaAtlas/PACKAGE.py
@@ -21,7 +21,7 @@ _external_packages = {
                      'DQ2_ENDUSER_SETUP' : 'True',
                      'noarch':True 
                      },
-    'panda-client' : { 'version' : '0.3.85', 
+    'panda-client' : { 'version' : '0.3.92', 
                        'PYTHONPATH':['lib/python2.4/site-packages'],
                        'CONFIGEXTRACTOR_PATH':'etc/panda/share',
                        'PANDA_SYS':'.',
--- python/GangaPanda/PACKAGE.py
+++ python/GangaPanda/PACKAGE.py
@@ -8,7 +8,7 @@
 """
 
 _external_packages = {
-    'panda-client' : { 'version' : '0.3.85', 
+    'panda-client' : { 'version' : '0.3.92', 
                     'PYTHONPATH':['lib/python2.4/site-packages'],
                     'CONFIGEXTRACTOR_PATH':'etc/panda/share',
                     'PANDA_SYS':'.',
