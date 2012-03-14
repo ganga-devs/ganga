ec2fc66f1b1c1e809a6a47a59d762df5747441c7
Mike Kenyon <mkenyon@cern.ch>
2012-03-14 16:15:43 +0000

    Allow us to attach ShareDirs to the is_prepared attribute

--- python/Ganga/Lib/Executable/Executable.py
+++ python/Ganga/Lib/Executable/Executable.py
@@ -52,7 +52,7 @@ class Executable(IPrepareApp):
         'exe' : SimpleItem(preparable=1,defvalue='echo',typelist=['str','Ganga.GPIDev.Lib.File.File.File'],comparable=1,doc='A path (string) or a File object specifying an executable.'), 
         'args' : SimpleItem(defvalue=["Hello World"],typelist=['str','Ganga.GPIDev.Lib.File.File.File','int'],sequence=1,strict_sequence=0,doc="List of arguments for the executable. Arguments may be strings, numerics or File objects."),
         'env' : SimpleItem(defvalue={},typelist=['str'],doc='Environment'),
-        'is_prepared' : SimpleItem(defvalue=None, strict_sequence=0, visitable=1, copyable=1, typelist=['type(None)','str','bool'],protected=0,comparable=1,doc='Location of shared resources. Presence of this attribute implies the application has been prepared.')
+        'is_prepared' : SimpleItem(defvalue=None, strict_sequence=0, visitable=1, copyable=1, typelist=['type(None)','bool','Ganga.GPIDev.Lib.File.File.ShareDir'],protected=0,comparable=1,doc='Location of shared resources. Presence of this attribute implies the application has been prepared.')
         } )
     _category = 'applications'
     _name = 'Executable'
