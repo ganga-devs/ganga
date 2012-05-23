a45616bc5364e741c086e8f66005c8c4424da371
Alexander Richards <arichard@cern.ch>
2012-05-23 14:37:48 +0000

    supporting additional stuff in job.inputsandbox

--- python/GangaGaudi/Lib/Backends/__init__.py
+++ python/GangaGaudi/Lib/Backends/__init__.py
@@ -1,10 +1,26 @@
+from Ganga.GPIDev.Adapters.StandardJobConfig import StandardJobConfig
 from Ganga.Lib.Localhost.Localhost import Localhost
 from Ganga.Lib.Interactive.Interactive import Interactive
 from Ganga.Lib.Batch.Batch import *
 
 def master_prepare(self, masterjobconfig):
+    def filt(sharedsandbox):
+        if sharedsandbox:
+            def shareboxfilter(item):
+                return item.name.find(self.getJobObject().application.is_prepared.name) is not -1
+            return shareboxfilter
+
+        def nonshareboxfilter(item):
+            return item.name.find(self.getJobObject().application.is_prepared.name) is -1
+        return nonshareboxfilter
+
+    
     if masterjobconfig:
-        return [f.name for f in masterjobconfig.getSandboxFiles()]
+
+        inputsandbox  = [f.name for f in filter(filt(True) , masterjobconfig.getSandboxFiles())]
+        sjc = StandardJobConfig(inputbox=filter(filt(False), masterjobconfig.getSandboxFiles()))
+        inputsandbox += super(type(self),self).master_prepare(sjc)
+        return inputsandbox
     return []
    
 setattr(Localhost,'master_prepare',master_prepare)
