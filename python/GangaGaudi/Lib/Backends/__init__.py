0e6f17ca8093977b65fc9d75db5a8c7f47f8521b
Alexander Richards <arichard@cern.ch>
2012-05-23 13:55:54 +0000

    updating interactive and batch backends

--- python/GangaGaudi/Lib/Backends/__init__.py
+++ python/GangaGaudi/Lib/Backends/__init__.py
@@ -1,4 +1,6 @@
 from Ganga.Lib.Localhost.Localhost import Localhost
+from Ganga.Lib.Interactive.Interactive import Interactive
+from Ganga.Lib.Batch.Batch import *
 
 def master_prepare(self, masterjobconfig):
     if masterjobconfig:
@@ -6,4 +8,7 @@ def master_prepare(self, masterjobconfig):
     return []
    
 setattr(Localhost,'master_prepare',master_prepare)
+setattr(Interactive,'master_prepare',master_prepare)
+setattr(PBS,'master_prepare',master_prepare)
+setattr(SGE,'master_prepare',master_prepare)
 ##localhost.master_prepare=master_prepare
