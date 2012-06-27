04e5fd2a4caa4144d55250cb74c4ac1b98efd0db
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2012-06-27 07:29:16 +0000

    Allow GenericSplitter in Athena application for HammerCloud workflow

--- python/GangaAtlas/Lib/Athena/Athena.py
+++ python/GangaAtlas/Lib/Athena/Athena.py
@@ -1380,7 +1380,7 @@ class Athena(IPrepareApp):
         # check grid/local class match up
         if job.backend._name in ['LCG', 'CREAM' ,'Panda', 'NG']: 
             # check splitter
-            if job.splitter and not job.splitter._name in ['DQ2JobSplitter', 'AnaTaskSplitterJob', 'ATLASTier3Splitter']:
+            if job.splitter and not job.splitter._name in ['DQ2JobSplitter', 'AnaTaskSplitterJob', 'ATLASTier3Splitter', 'GenericSplitter']:
                 raise ApplicationConfigurationError(None,"Cannot use splitter type '%s' with %s backend" % (job.splitter._name, job.backend._name) )
             
             # Check that only DQ2Datasets/AMIDatasets are used on the grid        
