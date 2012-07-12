0a75f95bd017a2b814f8751ca5cf5ce2743a9fa0
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-07-12 09:04:56 +0000

    set type list to strings

--- python/Ganga/GPIDev/Lib/Job/Comment.py
+++ python/Ganga/GPIDev/Lib/Job/Comment.py
@@ -5,7 +5,7 @@ class Comment(GangaObject):
     ''' Job/task comment
     '''
     _schema = Schema(Version(0,1),{
-                                   'comment' : SimpleItem(defvalue='',comparable=0, doc='comment of a job/task')
+                                   'comment' : SimpleItem(defvalue='',comparable=0, typelist=['str'], doc='comment of a job/task')
                                     })
 
     _category = 'comments'
@@ -21,8 +21,6 @@ class Comment(GangaObject):
             self.comment = args[0]
         else:
             super(Comment,self).__construct__(args)
-
-
 
     def lock(self):
         if self._parent._category == 'jobs' and hasattr(self._parent,'commentLocked'):
