0be901c81b4be76725cb4e3f64ec54c1e3c905f1
Ivan Dzhunov <ivan.antoniev.dzhunov@cern.ch>
2012-07-16 09:43:23 +0000

    renaming an output file attribute

--- python/Ganga/GPIDev/Lib/File/MassStorageFile.py
+++ python/Ganga/GPIDev/Lib/File/MassStorageFile.py
@@ -14,7 +14,7 @@ from OutputSandboxFile import OutputSandboxFile
 class MassStorageFile(OutputSandboxFile):
     """MassStorageFile represents a class marking a file to be written into mass storage (like Castor at CERN)
     """
-    _schema = Schema(Version(1,1), {'name': SimpleItem(defvalue="",doc='name of the file'),
+    _schema = Schema(Version(1,1), {'namePattern': SimpleItem(defvalue="",doc='pattern of the file name'),
                                     'joboutputdir': SimpleItem(defvalue="",doc='outputdir of the job with which the outputsandbox file object is associated'),
                                     'locations' : SimpleItem(defvalue=[],typelist=['str'],sequence=1,doc="list of locations where the outputfiles are uploaded"),
                                     'compressed' : SimpleItem(defvalue=False, typelist=['bool'],protected=0,doc='wheather the output file should be compressed before sending somewhere')
@@ -24,10 +24,10 @@ class MassStorageFile(OutputSandboxFile):
     _name = "MassStorageFile"
     _exportmethods = [ "location" , "get", "setLocation" ]
         
-    def __init__(self,name='', **kwds):
-        """ name is the name of the output file that has to be written into mass storage
+    def __init__(self,namePattern='', **kwds):
+        """ namePattern is the pattern of the output file that has to be written into mass storage
         """
-        super(MassStorageFile, self).__init__(name, **kwds)
+        super(MassStorageFile, self).__init__(namePattern, **kwds)
         self.locations = []
 
     def __construct__(self,args):
@@ -37,7 +37,7 @@ class MassStorageFile(OutputSandboxFile):
     def __repr__(self):
         """Get the representation of the file."""
 
-        return "MassStorageFile(name='%s')"% self.name
+        return "MassStorageFile(namePattern='%s')"% self.namePattern
     
 
     def setLocation(self):
@@ -62,7 +62,7 @@ class MassStorageFile(OutputSandboxFile):
 
             if line.startswith('massstorage'):
 
-                if outputPattern == self.name:
+                if outputPattern == self.namePattern:
                     massStorageLocation = outputPath.strip('\n')        
                     if massStorageLocation not in self.locations:
                         self.locations.append(massStorageLocation)
@@ -108,7 +108,7 @@ class MassStorageFile(OutputSandboxFile):
             lxplusHost = re.match('lxplus.*cern\.ch', host)
             if lxplusHost is None:
                 logger.warning('Output files can be uploaded to Castor only from lxplus')
-                logger.warning('skipping %s for uploading to Castor' % outputFile.name)
+                logger.warning('skipping %s for uploading to Castor' % self.namePattern)
                 return 
 
             mkdir_cmd = massStorageConfig['mkdir_cmd']
@@ -122,7 +122,7 @@ class MassStorageFile(OutputSandboxFile):
             (exitcode, mystdout, mystderr) = self.execSyscmdSubprocess('nsls %s' % pathToDirName)
             if exitcode != 0:
                 logger.warning('Error while executing nsls %s command, be aware that Castor commands can be executed only from lxplus, also check if the folder name is correct and existing' % pathToDirName, mystderr)
-                logger.warning('skipping %s for uploading to Castor' % outputFile.name)
+                logger.warning('skipping %s for uploading to Castor' % self.namePattern)
                 return
 
             directoryExists = False 
@@ -135,12 +135,12 @@ class MassStorageFile(OutputSandboxFile):
                 (exitcode, mystdout, mystderr) = self.execSyscmdSubprocess('%s %s' % (mkdir_cmd, massStoragePath))
                 if exitcode != 0:
                     logger.warning('Error while executing %s %s command, check if the ganga user has rights for creating directories in this folder' % (mkdir_cmd, massStoragePath))
-                    logger.warning('skipping %s for uploading to Castor' % outputFile.name)
+                    logger.warning('skipping %s for uploading to Castor' % self.namePattern)
                     return
             
-            fileName = self.name
+            fileName = self.namePattern
             if self.compressed:
-                fileName = '%s.gz' % self.name 
+                fileName = '%s.gz' % self.namePattern 
 
             for currentFile in glob.glob(os.path.join(self.joboutputdir, fileName)):
                 (exitcode, mystdout, mystderr) = self.execSyscmdSubprocess('%s %s %s' % (cp_cmd, currentFile, massStoragePath))
@@ -165,7 +165,7 @@ class MassStorageFile(OutputSandboxFile):
         massStorageConfig = getConfig('Output')['MassStorageFile']['uploadOptions']  
 
         for outputFile in outputFiles:
-            massStorageCommands.append('massstorage %s %s %s %s %s' % (outputFile.name , massStorageConfig['mkdir_cmd'],  massStorageConfig['cp_cmd'], massStorageConfig['ls_cmd'], massStorageConfig['path'])) 
+            massStorageCommands.append('massstorage %s %s %s %s %s' % (outputFile.namePattern , massStorageConfig['mkdir_cmd'],  massStorageConfig['cp_cmd'], massStorageConfig['ls_cmd'], massStorageConfig['path'])) 
 
                 
         script = """\n
