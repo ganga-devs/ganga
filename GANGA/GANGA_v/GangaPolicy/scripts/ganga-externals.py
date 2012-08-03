8be63c8b86d3c6b2007efff63217b7e5b02b1511
Ulrik Egede <U.Egede@imperial.ac.uk>
2012-08-03 13:47:11 +0000

    Updated dependencies

--- GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
+++ GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
@@ -11,11 +11,11 @@ externals_noarch = [['ApMon','2.2.11'],
 
 
 # These are the packages with architecture dependent code.
-externals_arch = [['matplotlib','0.99.0'],
-                  ['numpy','1.3.0'],
-                  ['pycrypto','2.0.1'],
-                  ['pyqt','3.18.1_python2.5']]
-archs = ['x86_64-slc5-gcc46-opt','x86_64-slc5-gcc43-opt','i686-slc5-gcc43-opt']
+externals_arch = [['matplotlib','1.1.1'],
+                  ['numpy','1.6.2'],
+                  ['pycrypto','2.0.1']]
+#                  ['pyqt','3.18.1_python2.5']]
+archs = ['x86_64-slc5-gcc46-opt','x86_64-slc5-gcc43-opt']
 
 # when comparing to top level Ganga PACKAGE.py need to compare just
 # the numeric versions so list here the bits that bloat the version
