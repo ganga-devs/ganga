7c2d1e50c953359beeb81ea500c80dd0532a7664
Alexander Richards <arichard@cern.ch>
2012-12-18 14:47:32 +0000

    Making the metadata non copyable

--- python/Ganga/GPIDev/Lib/Job/Job.py
+++ python/Ganga/GPIDev/Lib/Job/Job.py
@@ -174,7 +174,7 @@ class Job(GangaObject):
                                     'postprocessors':ComponentItem('postprocessor',defvalue=None,doc='list of postprocessors to run after job has finished'),
                                     'merger':ComponentItem('mergers',defvalue=None,hidden=1,copyable=0,load_default=0,optional=1,doc='optional output merger'),
                                     'do_auto_resubmit':SimpleItem(defvalue = False, doc='Automatically resubmit failed subjobs'),
-                                    'metadata':ComponentItem('metadata',defvalue = MetadataDict(), doc='the metadata', protected =1),
+                                    'metadata':ComponentItem('metadata',defvalue = MetadataDict(), doc='the metadata', protected =1, copyable=0),
                                     'fqid':SimpleItem(getter="getStringFQID",transient=1,protected=1,load_default=0,defvalue=None,optional=1,copyable=0,comparable=0,typelist=['str'],doc='fully qualified job identifier',visitable=0)
                                     })
 
