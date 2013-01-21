6e94f3ad66498087fc042526d35e0370ee493ada
Alexander Richards <arichard@cern.ch>
2013-01-21 13:44:32 +0000

    Fixes for change from OutputSandboxFile ->SandboxFile

--- python/GangaLHCb/Lib/Applications/PythonOptionsParser.py
+++ python/GangaLHCb/Lib/Applications/PythonOptionsParser.py
@@ -202,8 +202,8 @@ class PythonOptionsParser:
 ##        outputdata = []
 ##        if job.outputdata: outputdata = job.outputdata.files
 ##        outsandbox = [f for f in job.outputsandbox]
-        outputdata = [f.namePattern for f in job.outputfiles if stripProxy(f)._name != 'OutputSandboxFile']
-        outsandbox = [f.namePattern for f in job.outputfiles if stripProxy(f)._name == 'OutputSandboxFile']
+        outputdata = [f.namePattern for f in job.outputfiles if stripProxy(f)._name != 'SandboxFile']
+        outsandbox = [f.namePattern for f in job.outputfiles if stripProxy(f)._name == 'SandboxFile']
 
         # if user put any files in both, remove them from the sandbox
         for f in outsandbox:
