d9eabb6d4044662fe4ce0089d578f7b1bf57a492
Ramon Medrano Llamas <rmedrano@example.com>
2013-01-24 13:35:39 +0000

    Add the submit_host to be able to submit through glidein.

--- python/GangaCMS/Lib/ConfParams/CRAB.py
+++ python/GangaCMS/Lib/ConfParams/CRAB.py
@@ -14,6 +14,7 @@ class CRAB(GangaObject):
     _comments.append('http://belforte.home.cern.ch/belforte/misc/test/crab-v2.7.2.html#jobtype__')
     _comments.append('http://belforte.home.cern.ch/belforte/misc/test/crab-v2.7.2.html#server_name') 
     _comments.append('http://belforte.home.cern.ch/belforte/misc/test/crab-v2.7.2.html#use_server')
+    _comments.append('http://belforte.home.cern.ch/belforte/misc/test/crab-v2.7.2.html#scheduler')
     _comments.append('--- TO DO ---')
 
     schemadic = {}
@@ -21,6 +22,7 @@ class CRAB(GangaObject):
     schemadic['server_name'] = SimpleItem(defvalue=None, typelist=['type(None)','str'], doc=_comments[1])
     schemadic['use_server']  = SimpleItem(defvalue=None, typelist=['type(None)','int'], doc=_comments[2])
     schemadic['scheduler']   = SimpleItem(defvalue=None, typelist=['type(None)','str'], doc=_comments[3])
+    schemadic['submit_host'] = SimpleItem(defvalue=None, typelist=['type(None)','str'], doc=_comments[4])
 
 
     _schema =  Schema(Version(0,0), {})
