a9dec4320f6253a644eebe48efbe627b18592ccc
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2013-01-29 14:25:29 +0000

    Update panda-client 0.4.16

--- python/GangaAtlas/PACKAGE.py
+++ python/GangaAtlas/PACKAGE.py
@@ -22,7 +22,7 @@ _external_packages = {
                      'noarch':True ,
                      'RUCIO_APPID' : 'ganga',
                      },
-    'panda-client' : { 'version' : '0.4.11', 
+    'panda-client' : { 'version' : '0.4.16', 
                        'PYTHONPATH':['lib/python2.4/site-packages'],
                        'CONFIGEXTRACTOR_PATH':'etc/panda/share',
                        'PANDA_SYS':'.',
