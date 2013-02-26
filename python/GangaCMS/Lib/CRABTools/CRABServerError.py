722881dcea8b9328209a06e4287b2edd9ac4768d
Ramon Medrano Llamas <rmedrano@example.com>
2013-02-26 15:17:52 +0000

    Formatting.

--- python/GangaCMS/Lib/CRABTools/CRABServerError.py
+++ python/GangaCMS/Lib/CRABTools/CRABServerError.py
@@ -1,11 +1,12 @@
 from Ganga.Core import GangaException
 
+
 class CRABServerError(GangaException):
-    def __init__(self,message):
-        GangaException.__init__(self,message)
+    """Exception for errors regarding CRAB commands."""
+
+    def __init__(self, message):
+        GangaException.__init__(self, message)
         self.message = message
 
     def __str__(self):
-        return "CRABServerError: %s"%(self.message)
-
-
+        return "CRABServerError: %s" % (self.message)
