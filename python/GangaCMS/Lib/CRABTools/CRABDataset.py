869a2f04530f40bf4dc8353fd8678a7151179acb
Ramon Medrano Llamas <rmedrano@example.com>
2013-02-28 13:25:04 +0000

    Formatting and import optimization

--- python/GangaCMS/Lib/CRABTools/CRABDataset.py
+++ python/GangaCMS/Lib/CRABTools/CRABDataset.py
@@ -1,29 +1,21 @@
-#
-# CRAB Dataset containing all possible parameters
-# grupped into four categories (CMSSW,CRAB,GRID,USER)
-#
-# 08/06/10 @ ubeda
-#
-
 from Ganga.GPIDev.Lib.Dataset import Dataset
-from Ganga.GPIDev.Schema import *
+from Ganga.GPIDev.Schema import Schema, SimpleItem, Version
+from GangaCMS.Lib.ConfParams import CMSSW, CRAB, GRID, USER
 
-from GangaCMS.Lib.ConfParams import *
 
 class CRABDataset(Dataset):
-
-    schemadic={}
+    """Dataset definition for CRAB jobsets."""
+    schemadic = {}
     schemadic.update(CMSSW().schemadic)
     schemadic.update(CRAB().schemadic)
     schemadic.update(GRID().schemadic)
     schemadic.update(USER().schemadic)
-    schemadic['target_site'] = SimpleItem(defvalue=None, typelist=['type(None)','str'], doc='Target site name for the job. Intented to use only on HammerCloud.')
-
-    _schema   = Schema(Version(1,0), schemadic) 
+    schemadic['target_site'] = SimpleItem(defvalue=None,
+                                          typelist=['type(None)', 'str'],
+                                          doc='Target site name for the job.')
+    _schema = Schema(Version(1, 0), schemadic)
     _category = 'datasets'
-    _name     = 'CRABDataset'
-                    
+    _name = 'CRABDataset'
+
     def __init__(self):
-        super(CRABDataset,self).__init__()
-                
-        
+        super(CRABDataset, self).__init__()
