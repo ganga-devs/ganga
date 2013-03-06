974730a39e646c2079196de6446c2bd4c7349b1c
Mark Slater <mslater@cern.ch>
2013-03-06 13:13:53 +0000

    Fixed not passing the second arg through to the MC sript

--- python/GangaNA62/Lib/Applications/runNA62MC.sh
+++ python/GangaNA62/Lib/Applications/runNA62MC.sh
@@ -3,7 +3,7 @@
 # wrapper to run the na62 mc generation script and pipe the output to the separate stdout/err
 echo "Running: ${NA62SCRIPT} $1 $2 > ${NA62STDOUT} 2> ${NA62STDERR}"
 chmod +x ${NA62SCRIPT}
-./${NA62SCRIPT} $1 > ${NA62STDOUT} 2> ${NA62STDERR}
+./${NA62SCRIPT} $1 $2 > ${NA62STDOUT} 2> ${NA62STDERR}
 retcode=$?
 echo "-------------------------------------------------"
 echo $retcode
