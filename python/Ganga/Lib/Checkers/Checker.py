2fed7bf987f9b705e4bf0fdf62f131f9327b190f
Patrick Owen <patrickowen22@gmail.com>
2013-05-21 12:31:18 +0000

    add RootFileChecker

--- python/Ganga/Lib/Checkers/Checker.py
+++ python/Ganga/Lib/Checkers/Checker.py
@@ -5,7 +5,7 @@
 
 from Ganga.GPIDev.Base import GangaObject
 from Ganga.GPIDev.Adapters.IPostProcessor import PostProcessException
-from Ganga.GPIDev.Adapters.IChecker import IChecker
+from Ganga.GPIDev.Adapters.IChecker import IChecker, IFileChecker
 from Ganga.GPIDev.Base.Proxy import GPIProxyObject
 from Ganga.GPIDev.Schema import ComponentItem, FileItem, Schema, SimpleItem, Version
 from Ganga.Utility.Config import makeConfig, ConfigError, getConfig
@@ -19,7 +19,30 @@ import string
 
 logger = getLogger()
 
+def SortedValues(adict):
+    import ROOT
+    items = adict.items()
+    items.sort()
+    return [value for key, value in items]
 
+def GetKeyNames(f,dir = ""):
+    import ROOT
+    f.cd(dir)
+    return [key.GetName() for key in ROOT.gDirectory.GetListOfKeys()]
+    
+def GetTreeObjects(f, dir = ""):
+    import ROOT
+    tree_dict = {}
+    for tdir in GetKeyNames(f,dir):
+        if tdir == "":
+            continue
+        absdir = os.path.join(dir,tdir)
+        if isinstance(f.Get(tdir),ROOT.TDirectory):
+            for absdir,tree in GetTreeObjects(f,absdir).iteritems():
+                tree_dict[absdir] = tree
+        if isinstance(f.Get(absdir),ROOT.TTree):
+            tree_dict[absdir]=f.Get(absdir)
+    return tree_dict        
 
 
 
@@ -57,51 +80,158 @@ class MetaDataChecker(IChecker):
             logger.info('MetaDataChecker has failed job(%s) because the expression "%s" is False'%(job.fqid,self.expression))
         return self.result
 
-class FileChecker(IChecker):
+class FileChecker(IFileChecker):
     """
     Checks if string is in file.
-    searchStrings are the files you would like to check for.
-    files are the files you would like to check.
-    failIfFound (default = True) decides whether to fail the job if the string is found. If you set this to false the job will fail if the string *isnt* found. 
+    self.searchStrings are the files you would like to check for.
+    self.files are the files you would like to check.
+    self.failIfFound (default = True) decides whether to fail the job if the string is found. If you set this to false the job will fail if the string *isnt* found.
+    self.fileMustExist toggles whether to fail the job if the specified file doesn't exist (default is True).
     """
-    _schema = IChecker._schema.inherit_copy()
+    _schema = IFileChecker._schema.inherit_copy()
     _schema.datadict['searchStrings'] = SimpleItem(defvalue = [], doc='String to search for')
-    _schema.datadict['files'] = SimpleItem(defvalue = [], doc='File to search in')
     _schema.datadict['failIfFound'] = SimpleItem(True, doc='Toggle whether job fails if string is found or not found.')
     _category = 'postprocessor'
     _name = 'FileChecker'
-    _exportmethods = ['check']  
+    _exportmethods = ['check']
 
 
     def check(self,job):
         """
         Check that a string is in a file, takes the job object as input.
         """
-        if not len(self.files):
-            raise PostProcessException('No files specified, FileChecker will do nothing!')
 
         if not len(self.searchStrings):
-            raise PostProcessException('No serachStrings specified, FileChecker will do nothing!')
-        filepaths = []
-        for f in self.files:
-            filepath = os.path.join(job.outputdir,f)
-            if os.path.exists(filepath):
-                filepaths.append(filepath)
-            else:
-                logger.warning('File %s does not exist',filepath)
+            raise PostProcessException('No searchStrings specified, FileChecker will do nothing!')
+        filepaths = self.findFiles(job)
         if not len(filepaths):
             raise PostProcessException('None of the files to check exist, FileChecker will do nothing!') 
- 
-        for f in filepaths:
+        for filepath in filepaths:
             for searchString in self.searchStrings:
-                grepoutput = commands.getoutput('grep %s %s' % (searchString,filepath))
+                grepoutput = commands.getoutput('grep "%s" %s' % (searchString,filepath))
                 if len(grepoutput) and self.failIfFound is True:            
                     logger.info('The string %s has been found in file %s, FileChecker will fail job(%s)',searchString,filepath,job.fqid)
                     return self.failure
                 if not len(grepoutput) and self.failIfFound is False:            
                     logger.info('The string %s has not been found in file %s, FileChecker will fail job(%s)',searchString,filepath,job.fqid)
                     return self.failure
-        return self.success 
+        return self.result 
+
+
+class RootFileChecker(IFileChecker):
+    """
+    Checks ROOT files to see if they are zombies.
+    For master job, also checks to see if merging performed correctly.
+    self.files are the files you would like to check.
+    self.fileMustExist toggles whether to fail the job if the specified file doesn't exist (default is True).
+    """
+    _schema = IFileChecker._schema.inherit_copy()
+    _schema.datadict['checkMerge'] = SimpleItem(defvalue = True, doc='Toggle whether to check the merging proceedure')
+    _category = 'postprocessor'
+    _name = 'RootFileChecker'
+    _exportmethods = ['check']
+    
+    def checkBranches(self,mastertrees,subtrees):
+        import ROOT
+        for masterpath,mastertree in mastertrees.iteritems():
+            for subpath,subtree in subtrees.iteritems():
+                if (subpath == masterpath):
+                    subbranches = [branch.GetName() for branch in subtree.GetListOfBranches()]
+                    masterbranches = [branch.GetName() for branch in mastertree.GetListOfBranches()]
+                    if (subbranches != masterbranches):
+                        return self.failure
+        return self.success
+
+
+    def addEntries(self,mastertrees,subtrees,entries_dict):
+        import ROOT
+        for masterpath,mastertree in mastertrees.iteritems():
+            for subpath,subtree in subtrees.iteritems():
+                if (subpath == masterpath):                                             
+                    if (subpath in entries_dict):
+                        entries_dict[subpath]+=subtree.GetEntries()
+                    else:
+                        entries_dict[subpath]=subtree.GetEntries()
+        return entries_dict
+                
+    def checkMergeable(self,f):
+            import ROOT            
+            tf = ROOT.TFile.Open(f)
+            if tf.IsZombie():
+                logger.info('ROOT file %s is a zombie, failing job',f)
+                tf.Close()
+                return self.failure
+            if not len(GetKeyNames(tf)):
+                logger.info('ROOT file %s has no keys, failing job',f)
+                tf.Close()
+                return self.failure
+            tf.Close()
+            if (os.path.getsize(f) < 330):
+                logger.info('ROOT file %s has no size, failing job',f)
+                return self.failure
+            return self.success
+
+                        
+    def check(self,job):
+        """
+        Check that ROOT files are not zombies and were closed properly, also (for master job only) checks that the merging performed correctly.
+        """
+        import ROOT
+        self.result = True
+        filepaths = self.findFiles(job)
+        if not len(filepaths):
+            raise PostProcessException('None of the files to check exist, RootFileChecker will do nothing!') 
+        for f in filepaths:
+            if f.find('.root') < 0:
+                raise PostProcessException('The file "%s" is not a ROOT file, RootFileChecker will do nothing!'%os.path.basename(f))
+            if not self.checkMergeable(f):
+                return self.failure
+            if (len(job.subjobs) and self.checkMerge):
+                haddoutput = f+'.hadd_output'
+                if not os.path.exists(haddoutput):
+                    logger.warning('Hadd output file %s does not exist, cannot perform check on merging.',haddoutput)
+                    return self.success
+
+                for failString in ['Could not find branch','One of the export branches','Skipped file']:
+                    grepoutput = commands.getoutput('grep "%s" %s' % (failString,haddoutput))
+                    if len(grepoutput):
+                        logger.info('There was a problem with hadd, the string "%s" was found. Will fail job',failString)
+                        return self.failure
+
+                tf = ROOT.TFile.Open(f)
+                mastertrees = GetTreeObjects(tf)
+                entries_dict = {}
+                for sj in job.subjobs:
+                    if (sj.status == 'completed'):
+                        for subfile in self.findFiles(sj):
+                            if (os.path.basename(subfile) == os.path.basename(f)):
+                                subtf = ROOT.TFile.Open(subfile)
+                                subtrees = GetTreeObjects(subtf)
+    
+                                substructure = subtrees.keys()
+                                substructure.sort()
+                                masterstructure = mastertrees.keys()
+                                masterstructure.sort()
+                                if (substructure != masterstructure):
+                                    logger.info('File structure of subjob %s is not the same as master job, failing job',sj.fqid)
+                                    return self.failure
+                                    
+                                if not self.checkBranches(mastertrees,subtrees):
+                                    logger.info('The tree structure of subjob %s is not the same as merged tree, failing job',sj.fqid)
+                                    return self.failure                                    
+                                entries_dict = self.addEntries(mastertrees,subtrees,entries_dict)
+                                subtf.Close()
+                                
+                master_entries_dict = dict( (n, mastertrees[n].GetEntries() ) for n in set(mastertrees) )
+                if (SortedValues(entries_dict) != SortedValues(master_entries_dict)):
+                    logger.info('Sum of subjob tree entries is not the same as merged tree entries for file %s, failing job (check hadd output)',os.path.basename(f))
+                    return self.failure
+                tf.Close() 
+        return self.result 
+
+
+
+
 
 class CustomChecker(IChecker):
     """User tool for writing custom check with Python.
--- python/GangaGaudi/Lib/Checkers/LHCbMetadataChecker.py
+++ python/GangaGaudi/Lib/Checkers/LHCbMetadataChecker.py
@@ -26,7 +26,18 @@ logger = getLogger()
 class LHCbMetaDataChecker(MetaDataChecker):
     """
     Checks the meta data of a job is within some range,
-    Currently accepts 'lumi', 'inputevents', 'outputevents'.
+    Currently accepts 'lumi', 'inputevents', 'outputevents', 'nskipped' and 'nfiles'.
+    
+    For example do:
+    
+    mc = LHCbMetaDataChecker()
+    
+    mc.expression = 'nskipped == 0'
+    
+    j.postprocessors.append(mc)
+    
+    to fail jobs which skip some input files.
+    
     """
     _schema = MetaDataChecker._schema.inherit_copy()
     _category = 'postprocessor'
@@ -41,6 +52,8 @@ class LHCbMetaDataChecker(MetaDataChecker):
         inputevents = None
         outputevents = None
         lumi = None
+        nskipped = None
+        nfiles = None
         if self.expression.find('inputevents') > -1:
             try:
                 inputevents = j.metadata['events']['input']
@@ -55,7 +68,17 @@ class LHCbMetaDataChecker(MetaDataChecker):
             try:
                 lumi = float(j.metadata['lumi'][1:j.metadata['lumi'].find(' ')])
             except: 
-                raise PostProcessException("The metadata value j.lumi was not defined")            
+                raise PostProcessException("The metadata value j.lumi was not defined")
+        if self.expression.find('nskipped') > -1:
+            try:
+                nskipped = len(j.metadata['xmlskippedfiles'])
+            except: 
+                raise PostProcessException("The metadata value j.xmlskippedfiles was not defined")
+        if self.expression.find('nfiles') > -1:
+            try:
+                nfiles = float(j.metadata['xmldatanumbers']['full'])
+            except: 
+                raise PostProcessException("The metadata value j.xmldatanumbers was not defined")               
         return eval(self.expression)
 
 
