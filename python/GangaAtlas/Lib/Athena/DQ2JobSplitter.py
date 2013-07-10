0d841b454d7d82f3dfafd7a46367f28d4caa8b70
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2013-07-10 08:06:51 +0000

    Fix a bug in job splitting for ProdTrans application

--- python/GangaAtlas/Lib/Athena/DQ2JobSplitter.py
+++ python/GangaAtlas/Lib/Athena/DQ2JobSplitter.py
@@ -876,8 +876,11 @@ class DQ2JobSplitter(ISplitter):
                     # FIXME - set correct parameter values
                     inBS = False
                     inTRF = False
-                    if job.application.atlas_exetype == 'TRF':
-                        inTRF = True
+                    try:
+                        if job.application.atlas_exetype == 'TRF':
+                            inTRF = True
+                    except:
+                        pass
                     if job.backend.accessmode == 'DIRECT':
                         inTRF=False
                     inARA = False
