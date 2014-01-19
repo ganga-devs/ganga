bf95c8d701f623aeea48363f36e53f8c249ea4c8
Johannes Elmsheuser <Johannes.Elmsheuser@physik.uni-muenchen.de>
2014-01-19 13:25:26 +0000

    Initial import for Jedi backend

---
+++ GangaPanda/Lib/ProdTrans/ProdTransPandaRTHandler.py
@@ -0,0 +1,212 @@
+import commands, exceptions, random, re, sys, time
+
+from Ganga.Core.exceptions import ApplicationConfigurationError
+from Ganga.GPIDev.Adapters.IRuntimeHandler import IRuntimeHandler
+from Ganga.Core import BackendError
+
+import Ganga.Utility.logging
+logger = Ganga.Utility.logging.getLogger()
+
+from Ganga.Utility.Config import getConfig
+configPanda = getConfig('Panda')
+
+def getLatestDBReleaseCaching():
+    import tempfile
+    import cPickle as pickle
+    from pandatools import Client
+    from GangaAtlas.Lib.Credentials.ProxyHelper import getNickname
+
+    TMPDIR = tempfile.gettempdir()
+    nickname = getNickname(allowMissingNickname=False)
+    DBRELCACHE = '%s/ganga.latestdbrel.%s'%(TMPDIR,nickname)
+
+    try:
+        fh = open(DBRELCACHE)
+        dbrelCache = pickle.load(fh)
+        fh.close()
+        if dbrelCache['mtime'] > time.time() - 3600:
+            logger.debug('Loading LATEST DBRelease from local cache')
+            return dbrelCache['atlas_dbrelease']
+        else:
+            raise Exception()
+    except:
+        logger.debug('Updating local LATEST DBRelease cache')
+        atlas_dbrelease = Client.getLatestDBRelease(False)
+        dbrelCache = {}
+        dbrelCache['mtime'] = time.time()
+        dbrelCache['atlas_dbrelease'] = atlas_dbrelease
+        fh = open(DBRELCACHE,'w')
+        pickle.dump(dbrelCache,fh)
+        fh.close()
+        return atlas_dbrelease
+
+
+class ProdTransPandaRTHandler(IRuntimeHandler):
+    """Runtime handler for the ProdTrans application."""
+
+    def master_prepare(self, app, appmasterconfig):
+        """Prepare the master aspect of job submission.
+           Returns: jobmasterconfig understood by Panda backend."""
+
+        job = app._getParent()
+        logger.debug('ProdTransPandaRTHandler master_prepare() for %s',
+                    job.getFQID('.'))
+
+        return None
+
+    def prepare(self, app, appsubconfig, appmasterconfig, jobmasterconfig):
+        """Prepare the specific aspec of each subjob.
+           Returns: subjobconfig list of objects understood by backends."""
+
+        from pandatools import Client
+        from pandatools import AthenaUtils
+        from taskbuffer.JobSpec import JobSpec
+        from taskbuffer.FileSpec import FileSpec
+        from GangaAtlas.Lib.ATLASDataset.DQ2Dataset import dq2_set_dataset_lifetime
+        from GangaPanda.Lib.Panda.Panda import refreshPandaSpecs
+        
+        # make sure we have the correct siteType
+        refreshPandaSpecs()
+
+        job = app._getParent()
+        masterjob = job._getRoot()
+
+        logger.debug('ProdTransPandaRTHandler prepare called for %s',
+                     job.getFQID('.'))
+
+        job.backend.actualCE = job.backend.site
+        job.backend.requirements.cloud = Client.PandaSites[job.backend.site]['cloud']
+
+        # check that the site is in a submit-able status
+        if not job.splitter or job.splitter._name != 'DQ2JobSplitter':
+            allowed_sites = job.backend.list_ddm_sites()
+
+        try:
+            outDsLocation = Client.PandaSites[job.backend.site]['ddm']
+            tmpDsExist = False
+            if (configPanda['processingType'].startswith('gangarobot') or configPanda['processingType'].startswith('hammercloud')) and Client.getDatasets(job.outputdata.datasetname):
+                tmpDsExist = True
+                logger.info('Re-using output dataset %s'%job.outputdata.datasetname)
+            Client.addDataset(job.outputdata.datasetname,False,location=outDsLocation,allowProdDisk=True,dsExist=tmpDsExist)
+            logger.info('Output dataset %s registered at %s'%(job.outputdata.datasetname,outDsLocation))
+            dq2_set_dataset_lifetime(job.outputdata.datasetname, outDsLocation)
+        except exceptions.SystemExit:
+            raise BackendError('Panda','Exception in adding dataset %s: %s %s'%(job.outputdata.datasetname,sys.exc_info()[0],sys.exc_info()[1]))
+        
+        # JobSpec.
+        jspec = JobSpec()
+        jspec.currentPriority = app.priority
+        jspec.jobDefinitionID = masterjob.id
+        jspec.jobName = commands.getoutput('uuidgen')
+        jspec.coreCount = app.core_count
+        jspec.AtlasRelease = 'Atlas-%s' % app.atlas_release
+        jspec.homepackage = app.home_package
+        jspec.transformation = app.transformation
+        jspec.destinationDBlock = job.outputdata.datasetname
+        if job.outputdata.location:
+            jspec.destinationSE = job.outputdata.location
+        else:
+            jspec.destinationSE = job.backend.site
+        if job.inputdata:
+            jspec.prodDBlock = job.inputdata.dataset[0]
+        else:
+            jspec.prodDBlock = 'NULL'
+        if app.prod_source_label:
+            jspec.prodSourceLabel = app.prod_source_label
+        else:
+            jspec.prodSourceLabel = configPanda['prodSourceLabelRun']
+        jspec.processingType = configPanda['processingType']
+        jspec.computingSite = job.backend.site
+        jspec.cloud = job.backend.requirements.cloud
+        jspec.cmtConfig = app.atlas_cmtconfig
+        if app.dbrelease == 'LATEST':
+            try:
+                latest_dbrelease = getLatestDBReleaseCaching()
+            except:
+                from pandatools import Client
+                latest_dbrelease = Client.getLatestDBRelease()
+            m = re.search('(.*):DBRelease-(.*)\.tar\.gz', latest_dbrelease)
+            if m:
+                self.dbrelease_dataset = m.group(1)
+                self.dbrelease = m.group(2)
+            else:
+                raise ApplicationConfigurationError(None, "Error retrieving LATEST DBRelease. Try setting application.dbrelease manually.")
+        else:
+            self.dbrelease_dataset = app.dbrelease_dataset
+            self.dbrelease = app.dbrelease
+        jspec.jobParameters = app.job_parameters
+
+        if self.dbrelease:
+            jspec.jobParameters += ' DBRelease=DBRelease-%s.tar.gz' % (self.dbrelease,)
+            dbspec = FileSpec()
+            dbspec.lfn = 'DBRelease-%s.tar.gz' % self.dbrelease
+            dbspec.dataset = self.dbrelease_dataset
+            dbspec.prodDBlock = jspec.prodDBlock
+            dbspec.type = 'input'
+            jspec.addFile(dbspec)
+
+        if job.inputdata:
+            m = re.search('(.*)\.(.*)\.(.*)\.(.*)\.(.*)\.(.*)',
+                          job.inputdata.dataset[0])
+            if not m:
+                raise ApplicationConfigurationError(None, "Error retrieving run number from dataset name")
+            if jspec.transformation.endswith("_tf.py") or jspec.transformation.endswith("_tf"):
+                jspec.jobParameters += ' --runNumber %d' % int(m.group(2))
+            else:
+                jspec.jobParameters += ' RunNumber=%d' % int(m.group(2))
+        
+        # Output files.
+        randomized_lfns = []
+        for lfn in app.output_files:
+            ofspec = FileSpec()
+            if app.randomize_lfns:
+                randomized_lfn = lfn + ('.%s.%d.%s' % (job.backend.site, int(time.time()), commands.getoutput('uuidgen')[:4] ) )
+            else:
+                randomized_lfn = lfn
+            ofspec.lfn = randomized_lfn
+            randomized_lfns.append(randomized_lfn)
+            ofspec.destinationDBlock = jspec.destinationDBlock
+            ofspec.destinationSE = jspec.destinationSE
+            ofspec.dataset = jspec.destinationDBlock
+            ofspec.type = 'output'
+            jspec.addFile(ofspec)
+        if jspec.transformation.endswith("_tf.py") or jspec.transformation.endswith("_tf"):
+            jspec.jobParameters += ' --output%sFile %s' % (app.output_type, ','.join(randomized_lfns))
+        else:
+            jspec.jobParameters += ' output%sFile=%s' % (app.output_type, ','.join(randomized_lfns))
+
+        # Input files.
+        if job.inputdata:
+            for guid, lfn, size, checksum, scope in zip(job.inputdata.guids, job.inputdata.names, job.inputdata.sizes, job.inputdata.checksums, job.inputdata.scopes):
+                ifspec = FileSpec()
+                ifspec.lfn = lfn
+                ifspec.GUID = guid
+                ifspec.fsize = size
+                ifspec.md5sum = checksum
+                ifspec.scope = scope
+                ifspec.dataset = jspec.prodDBlock
+                ifspec.prodDBlock = jspec.prodDBlock
+                ifspec.type = 'input'
+                jspec.addFile(ifspec)
+            if app.input_type:
+                itype = app.input_type
+            else:
+                itype = m.group(5)
+            if jspec.transformation.endswith("_tf.py") or jspec.transformation.endswith("_tf"):
+                jspec.jobParameters += ' --input%sFile %s' % (itype, ','.join(job.inputdata.names))
+            else:
+                jspec.jobParameters += ' input%sFile=%s' % (itype, ','.join(job.inputdata.names))
+
+        # Log files.
+        lfspec = FileSpec()
+        lfspec.lfn = '%s.job.log.tgz' % jspec.jobName
+        lfspec.destinationDBlock = jspec.destinationDBlock
+        lfspec.destinationSE  = jspec.destinationSE
+        lfspec.dataset = jspec.destinationDBlock
+        lfspec.type = 'log'
+        jspec.addFile(lfspec)
+        
+        return jspec
+
+from Ganga.GPIDev.Adapters.ApplicationRuntimeHandlers import allHandlers
+allHandlers.add('ProdTrans', 'Panda', ProdTransPandaRTHandler)
---
+++ GangaPanda/__init__.py
@@ -0,0 +1,62 @@
+## Config options
+from Ganga.Utility.Config import makeConfig, ConfigError
+
+# -------------------------------------------------
+# Panda Options
+config = makeConfig('Panda','Panda backend configuration parameters')
+config.addOption( 'prodSourceLabelBuild', 'panda', 'prodSourceLabelBuild')
+config.addOption( 'prodSourceLabelRun', 'user', 'prodSourceLabelRun')
+config.addOption( 'assignedPriorityBuild', 2000, 'assignedPriorityBuild' )
+config.addOption( 'assignedPriorityRun', 1000, 'assignedPriorityRun' )
+config.addOption( 'processingType', 'ganga', 'processingType' )
+config.addOption( 'enableDownloadLogs', False , 'enableDownloadLogs' )  
+config.addOption( 'trustIS', True , 'Trust the Information System' )  
+config.addOption( 'serverMaxJobs', 5000 , 'Maximum number of subjobs to send to the Panda server' )  
+config.addOption( 'chirpconfig', '' , 'Configuration string for chirp data output, e.g. "chirp^etpgrid01.garching.physik.uni-muenchen.de^/tanyasandoval^-d chirp" ' )  
+config.addOption( 'chirpserver', '' , 'Configuration string for the chirp server, e.g. "voatlas92.cern.ch". If this variable is set config.Panda.chirpconfig is filled and chirp output will be enabled.' )  
+config.addOption( 'siteType', 'analysis' , 'Expert only.' )  
+
+# -------------------------------------------------
+# Jedi Options
+config = makeConfig('Jedi','Jedi backend configuration parameters')
+config.addOption( 'prodSourceLabelBuild', 'panda', 'prodSourceLabelBuild')
+config.addOption( 'prodSourceLabelRun', 'user', 'prodSourceLabelRun')
+config.addOption( 'assignedPriorityBuild', 2000, 'assignedPriorityBuild' )
+config.addOption( 'assignedPriorityRun', 1000, 'assignedPriorityRun' )
+config.addOption( 'processingType', 'ganga', 'processingType' )
+config.addOption( 'enableDownloadLogs', False , 'enableDownloadLogs' )  
+config.addOption( 'trustIS', True , 'Trust the Information System' )  
+config.addOption( 'serverMaxJobs', 5000 , 'Maximum number of subjobs to send to the Panda server' )  
+config.addOption( 'chirpconfig', '' , 'Configuration string for chirp data output, e.g. "chirp^etpgrid01.garching.physik.uni-muenchen.de^/tanyasandoval^-d chirp" ' )  
+config.addOption( 'chirpserver', '' , 'Configuration string for the chirp server, e.g. "voatlas92.cern.ch". If this variable is set config.Panda.chirpconfig is filled and chirp output will be enabled.' )  
+config.addOption( 'siteType', 'analysis' , 'Expert only.' )  
+
+
+
+def getEnvironment(c):
+    import PACKAGE
+    PACKAGE.standardSetup()
+    return {}
+    
+def loadPlugins(c):
+
+    import sys
+    from Ganga.Utility.logging import getLogger
+
+    try:
+        import Lib.Panda
+        import Lib.Jedi
+    except SystemExit:
+        from Ganga.Core.exceptions import ApplicationConfigurationError
+        import commands
+        (s,o) = commands.getstatusoutput('curl --version')
+        if (s):
+            raise ApplicationConfigurationError(None,"Couldn't load Panda Client: ensure 'curl' is available")
+        else:
+            raise ApplicationConfigurationError(None,"Couldn't load Panda Client")
+    import Lib.Athena
+    import Lib.Executable
+    import Lib.ProdTrans
+
+    return None
+
