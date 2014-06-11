e6f0c540d91a4dcf31813072851824192745365e
Ganga General <gangage@cern.ch>
2014-06-11 09:06:40 +0000

    global tag done from 6.0.31-pre

--- python/GangaDirac/Lib/Files/DiracFile.py
+++ python/GangaDirac/Lib/Files/DiracFile.py
@@ -76,7 +76,7 @@ class DiracFile(IOutputFile):
     def _attribute_filter__set__(self,name, value):
         if name == 'lfn':
             self.namePattern = os.path.basename(value)
-        if name == 'localDir' and type(value) != type(None):
+        if name == 'localDir':
             return expandfilename(value)
         return value
 
