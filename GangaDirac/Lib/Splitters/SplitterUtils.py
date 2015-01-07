9a129a7037764d47a49d69e75e3d542b43a6acd9
Rob Currie <r.currie@imperial.ac.uk>
2015-01-07 14:36:12 +0000

    Updating Splitter utils to have more debug info

--- GangaDirac/Lib/Splitters/SplitterUtils.py
+++ GangaDirac/Lib/Splitters/SplitterUtils.py
@@ -23,21 +23,44 @@ def DiracSplitter(inputs, filesPerJob, maxFiles, ignoremissing):
     """
     split_files = []
     i=inputs.__class__()
-    for files in igroup(inputs.files[:maxFiles],
-                        getConfig('DIRAC')['splitFilesChunks'],
-                        leftovers=True):
-        i.files = files
-        result = execute('splitInputData(%s,%d)'\
-                         % (i.getLFNs(),filesPerJob)
-                         )
 
-        if not result_ok(result):
-            logger.error('Error splitting files: %s' % str(result))
-            raise SplittingError('Error splitting files.')
-        split_files += result.get('Value',[])
+    all_files = igroup( inputs.files[:maxFiles], getConfig('DIRAC')['splitFilesChunks'],
+                        leftovers=True )
+
+    #import traceback
+    #traceback.print_stack()
+
+    for files in all_files:
+
+        i.files = files
+
+        LFNsToSplit = i.getLFNs()
+
+        #print "LFNsToSplit: " + str(LFNsToSplit)
+
+        if( len( LFNsToSplit ) ) > 1:
+
+            logger.debug( "Splitting inputData" )
+
+            result = execute('splitInputData(%s,%d)'\
+                             % ( i.getLFNs(), filesPerJob ) )
+
+            if not result_ok(result):
+                logger.error('DIRAC:: Error splitting files: %s' % str(result))
+                raise SplittingError('Error splitting files.')
+
+            split_files += result.get( 'Value', [] )
+
+        else:
+
+            logger.debug( "Don't try to split a single file" )
+
+            split_files = [ LFNsToSplit ]
+
     if len(split_files) == 0:
         raise SplittingError('An unknown error occured.')
 
+    # FIXME
     # check that all files were available on the grid
     big_list = []
     for l in split_files: big_list.extend(l)
@@ -47,7 +70,10 @@ def DiracSplitter(inputs, filesPerJob, maxFiles, ignoremissing):
         if not ignoremissing:
             raise SplittingError('Some files not found!')
     ###
-   
+
+    logger.debug( "Split Files: %s" % str(split_files) )
+    #print split_files
+
     for dataset in split_files:
         yield dataset
-        
+
