24e99f2a7c6b3fe0f5a342381d67e25186981a5d
Patrick Owen <patrickowen22@gmail.com>
2015-05-29 08:50:16 +0000

    moving config/DIRAC/ROOT version bits from the LHCb environment into the Ganga script

--- GANGA/GANGA_v/GangaPolicy/scripts/StripPath.sh
+++ GANGA/GANGA_v/GangaPolicy/scripts/StripPath.sh
@@ -1,6 +1,6 @@
 scriptdir=$GANGAPOLICYROOT/scripts
 
-tmpfile=`python $scriptdir/PathStripper.py --shell=sh --mktemp -e PATH -e LD_LIBRARY_PATH -e PYTHONPATH -e JOBOPTSEARCHPATH -e HPATH --dirac --ganga --root`
+tmpfile=`python $scriptdir/PathStripper.py --shell=sh --mktemp -e PATH -e LD_LIBRARY_PATH -e PYTHONPATH -e JOBOPTSEARCHPATH -e HPATH`
 . $tmpfile
 rm -f $tmpfile
 unset tmpfile
