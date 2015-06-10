b7c79341a74bf6e030a8922004d304f94cd38283
Matt Williams <Matt.Williams@cern.ch>
2015-06-10 12:35:34 +0000

    Replace use of the apply() function with argument unpacking

--- python/Ganga/Utility/external/logging/__init__.py
+++ python/Ganga/Utility/external/logging/__init__.py
@@ -898,7 +898,7 @@ class Logger(Filterer):
         if self.manager.disable >= DEBUG:
             return
         if DEBUG >= self.getEffectiveLevel():
-            apply(self._log, (DEBUG, msg, args), kwargs)
+            self._log(*(DEBUG, msg, args), **kwargs)
 
     def info(self, msg, *args, **kwargs):
         """
@@ -912,7 +912,7 @@ class Logger(Filterer):
         if self.manager.disable >= INFO:
             return
         if INFO >= self.getEffectiveLevel():
-            apply(self._log, (INFO, msg, args), kwargs)
+            self._log(*(INFO, msg, args), **kwargs)
 
     def warning(self, msg, *args, **kwargs):
         """
@@ -926,7 +926,7 @@ class Logger(Filterer):
         if self.manager.disable >= WARNING:
             return
         if self.isEnabledFor(WARNING):
-            apply(self._log, (WARNING, msg, args), kwargs)
+            self._log(*(WARNING, msg, args), **kwargs)
 
     warn = warning
 
@@ -942,13 +942,13 @@ class Logger(Filterer):
         if self.manager.disable >= ERROR:
             return
         if self.isEnabledFor(ERROR):
-            apply(self._log, (ERROR, msg, args), kwargs)
+            self._log(*(ERROR, msg, args), **kwargs)
 
     def exception(self, msg, *args):
         """
         Convenience method for logging an ERROR with exception information.
         """
-        apply(self.error, (msg,) + args, {'exc_info': 1})
+        self.error(*(msg,) + args, **{'exc_info': 1})
 
     def critical(self, msg, *args, **kwargs):
         """
@@ -962,7 +962,7 @@ class Logger(Filterer):
         if self.manager.disable >= CRITICAL:
             return
         if CRITICAL >= self.getEffectiveLevel():
-            apply(self._log, (CRITICAL, msg, args), kwargs)
+            self._log(*(CRITICAL, msg, args), **kwargs)
 
     fatal = critical
 
@@ -983,7 +983,7 @@ class Logger(Filterer):
         if self.manager.disable >= level:
             return
         if self.isEnabledFor(level):
-            apply(self._log, (level, msg, args), kwargs)
+            self._log(*(level, msg, args), **kwargs)
 
     def findCaller(self):
         """
@@ -1198,7 +1198,7 @@ def critical(msg, *args, **kwargs):
     """
     if len(root.handlers) == 0:
         basicConfig()
-    apply(root.critical, (msg,)+args, kwargs)
+    root.critical(*(msg,)+args, **kwargs)
 
 fatal = critical
 
@@ -1208,14 +1208,14 @@ def error(msg, *args, **kwargs):
     """
     if len(root.handlers) == 0:
         basicConfig()
-    apply(root.error, (msg,)+args, kwargs)
+    root.error(*(msg,)+args, **kwargs)
 
 def exception(msg, *args):
     """
     Log a message with severity 'ERROR' on the root logger,
     with exception information.
     """
-    apply(error, (msg,)+args, {'exc_info': 1})
+    error(*(msg,)+args, **{'exc_info': 1})
 
 def warning(msg, *args, **kwargs):
     """
@@ -1223,7 +1223,7 @@ def warning(msg, *args, **kwargs):
     """
     if len(root.handlers) == 0:
         basicConfig()
-    apply(root.warning, (msg,)+args, kwargs)
+    root.warning(*(msg,)+args, **kwargs)
 
 warn = warning
 
@@ -1233,7 +1233,7 @@ def info(msg, *args, **kwargs):
     """
     if len(root.handlers) == 0:
         basicConfig()
-    apply(root.info, (msg,)+args, kwargs)
+    root.info(*(msg,)+args, **kwargs)
 
 def debug(msg, *args, **kwargs):
     """
@@ -1241,7 +1241,7 @@ def debug(msg, *args, **kwargs):
     """
     if len(root.handlers) == 0:
         basicConfig()
-    apply(root.debug, (msg,)+args, kwargs)
+    root.debug(*(msg,)+args, **kwargs)
 
 def log(level, msg, *args, **kwargs):
     """
@@ -1249,7 +1249,7 @@ def log(level, msg, *args, **kwargs):
     """
     if len(root.handlers) == 0:
         basicConfig()
-    apply(root.log, (level, msg)+args, kwargs)
+    root.log(*(level, msg)+args, **kwargs)
 
 def disable(level):
     """
