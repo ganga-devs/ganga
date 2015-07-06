b17d47ec3924d680ce626f6782c9d7b493f5c322
Matt Williams <Matt.Williams@cern.ch>
2015-07-06 10:33:15 +0000

    Add a simple test which doesn't use the Ganga runtime. This is just testing the API as if it's plain Python.

---
+++ python/Ganga/new_tests/TestProxy.py
@@ -0,0 +1,76 @@
+import unittest
+
+from Ganga.GPIDev.Base import GangaObject
+from Ganga.GPIDev.Schema import Schema, Version, SimpleItem
+
+class TestGangaObject(GangaObject):
+    _schema = Schema(Version(1,0), {'a' : SimpleItem(42, typelist=['int'])})
+    _category = 'TestGangaObject'
+    _name = 'TestGangaObject'
+
+import Ganga.GPIDev.Base.Proxy
+import Ganga.Core.exceptions
+
+class TestProxy(unittest.TestCase):
+    """
+    Test the Proxy functions
+    """
+    
+    def setUp(self):
+        """
+        Create an instance of the GangaObject subclass and save the proxy
+        version of it
+        """
+        new_object = TestGangaObject()
+        self.p = Ganga.GPIDev.Base.Proxy.addProxy(new_object)
+
+    def test_default(self):
+        """
+        Check that the default value of a parameter is saved correctly
+        """
+        self.assertEqual(self.p.a, 42)
+
+    def test_assign(self):
+        """
+        Make sure that valid values can be assigned to an attribute of the proxy
+        """
+        new_value = 7
+        self.p.a = new_value
+        self.assertEqual(self.p.a, new_value)
+
+    def test_set_nonexistant(self):
+        """
+        Ensure that assigning to non-existant attributes raises an exception
+        """
+        def _assign():
+            self.p.b = 'fail'
+            
+        self.assertRaises(Ganga.Core.exceptions.GangaAttributeError, _assign)
+        
+        #TODO In Python >= 2.7 use
+        # with self.assertRaises(Ganga.Core.exceptions.GangaAttributeError):
+        #     self.p.b = 'fail'
+
+    def test_set_wrong_type(self):
+        """
+        Ensure that assigning an incorrect type to an attribute raises an exception
+        """
+        def _assign():
+            self.p.a = 'fail'
+            
+        self.assertRaises(Ganga.Core.exceptions.TypeMismatchError, _assign)
+    
+    def test_stripProxy(self):
+        """
+        Check that stripping the proxy returns the original class
+        """
+        stripped = Ganga.GPIDev.Base.Proxy.stripProxy(self.p)
+        #TODO In Python >= 2.7 use
+        # self.assertIsInstance(stripped, TestGangaObject)
+        self.assertEqual(type(stripped), TestGangaObject)
+
+    def test_isType(self):
+        self.assertTrue(Ganga.GPIDev.Base.Proxy.isType(self.p, TestGangaObject))
+
+    def test_isProxy(self):
+        self.assertTrue(Ganga.GPIDev.Base.Proxy.isProxy(self.p))
