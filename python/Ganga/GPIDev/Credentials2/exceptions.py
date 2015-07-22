b4fc4437a853a82ca0a981397a4e8f2bbfc62f7e
Matt Williams <Matt.Williams@cern.ch>
2015-07-22 13:13:28 +0000

    Add new exceptions for handling invalid credentials

--- python/Ganga/GPIDev/Credentials2/exceptions.py
+++ python/Ganga/GPIDev/Credentials2/exceptions.py
@@ -15,3 +15,19 @@ class CredentialRenewalError(CredentialsError):
     """
     def __init__(self, *args):
         super(self.__class__, self).__init__(self, *args)
+
+
+class InvalidCredentialError(CredentialsError):
+    """
+    The credential is invalid for some reason
+    """
+    def __init__(self, *args):
+        super(self.__class__, self).__init__(self, *args)
+
+
+class ExpiredCredentialError(InvalidCredentialError):
+    """
+    The credential has expired
+    """
+    def __init__(self, *args):
+        super(self.__class__, self).__init__(self, *args)
