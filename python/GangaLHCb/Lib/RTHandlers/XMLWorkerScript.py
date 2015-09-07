aa89db32d6c71228570674dacd5e85639c3bb2e8
Rob Currie <r.currie@imperial.ac.uk>
2015-09-07 16:40:01 +0000

    Fixing some typos caught by test jobs

--- python/GangaLHCb/Lib/RTHandlers/XMLWorkerScript.py
+++ python/GangaLHCb/Lib/RTHandlers/XMLWorkerScript.py
@@ -1,7 +1,7 @@
 # XMLSummary parsing
 import os, sys
 if 'XMLSUMMARYBASEROOT' not in os.environ:
-    sys.stderr.write(\"\'XMLSUMMARYBASEROOT\' env var not defined so summary.xml not parsed\")
+    sys.stderr.write("'XMLSUMMARYBASEROOT' env var not defined so summary.xml not parsed")
 else:
     schemapath  = os.path.join(os.environ['XMLSUMMARYBASEROOT'],'xml/XMLSummary.xsd')
     summarypath = os.path.join(os.environ['XMLSUMMARYBASEROOT'],'python/XMLSummaryBase')
@@ -10,22 +10,22 @@ else:
 
     outputxml = os.path.join(os.getcwd(), 'summary.xml')
     if not os.path.exists(outputxml):
-        sys.stderr.write(\"XMLSummary not passed as \'summary.xml\' not present in working dir\")
+        sys.stderr.write("XMLSummary not passed as 'summary.xml' not present in working dir")
     else:
         try:
             XMLSummarydata = summary.Summary(schemapath,construct_default=False)
             XMLSummarydata.parse(outputxml)
         except:
-            sys.stderr.write(\"Failure when parsing XMLSummary file \'summary.xml\'\")
+            sys.stderr.write("Failure when parsing XMLSummary file 'summary.xml'")
 
         try:
             fn = open('__parsedxmlsummary__','w')
             for name, method in activeSummaryItems().iteritems():
                 try:
-                    fn.write( '%s = %s\\n' % ( name, str(method(XMLSummarydata)) ) )
+                    fn.write( '%s = %s\n' % ( name, str(method(XMLSummarydata)) ) )
                 except Exception, e:
-                    fn.write( '%s = None\\n' % name )
-                    sys.stderr.write('XMLSummary warning: Method \"%s\" not available for this job\\n' % name)
+                    fn.write( '%s = None\n' % name )
+                    sys.stderr.write('XMLSummary warning: Method "%s" not available for this job\n' % name)
         except:
             sys.stderr.write('XMLSummary error: Failed to create __parsedxmlsummary__ file')
         finally:
