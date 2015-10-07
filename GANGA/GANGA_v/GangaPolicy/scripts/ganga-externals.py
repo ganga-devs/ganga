91364f3bf94fc19747fe7a9ffdc6ba0a8f205199
rcurrie <rcurrie@cern.ch>
2015-10-07 15:16:45 +0200

    Updating deps to drop lots of unused externals, updating lhcb-prepare to current best state

--- GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
+++ GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
@@ -2,11 +2,9 @@
 ## the lhcb-prepare script
 
 # These are the external packages with only Python code.
-externals_noarch = [['ApMon','2.2.11'],
-                    ['httplib2', '0.8'],
+externals_noarch = [['httplib2', '0.8'],
                     ['python-gflags', '2.0'],
                     ['google-api-python-client', '1.1'],
-                    ['figleaf', '0.6'],
                     ['paramiko', '1.7.3'],
                     ['PYTF','1.6'],
                     ['stomputil','2.3'],
@@ -14,15 +12,12 @@ externals_noarch = [['ApMon','2.2.11'],
 
 
 # These are the packages with architecture dependent code.
-externals_arch = [['matplotlib','1.1.1'],
-                  ['numpy','1.6.2'],
-                  ['pycrypto','2.0.1']]
-archs = ['x86_64-slc6-gcc48-opt','x86_64-slc6-gcc46-opt','x86_64-slc5-gcc46-opt']
+externals_arch = [['pycrypto','2.0.1']]
+archs = ['x86_64-slc6-gcc48-opt']
 
 # when comparing to top level Ganga PACKAGE.py need to compare just
 # the numeric versions so list here the bits that bloat the version
 # numbers and what to replace them with when checking. i.e. '.'
 # seperated digits are compared
-version_check_bloats = { 'ipython':[('_ganga_patch','.')],
-                         'pyqt':[('_python2.5','')] }
+version_check_bloats = { 'ipython':[('_ganga_patch','.')] }
 
