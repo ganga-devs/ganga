03f0282e7829cd7bbf701975248a50c8c5b06dc9
rcurrie <rcurrie@cern.ch>
2015-10-12 19:10:40 +0200

    Minor changes to shutdown manager code

--- python/Ganga/Core/InternalServices/ShutdownManager.py
+++ python/Ganga/Core/InternalServices/ShutdownManager.py
@@ -67,15 +67,19 @@ def _ganga_run_exitfuncs():
         logger.debug("This should only happen if Ganga filed to initialize correctly")
 
     ## Stop the Mon loop from iterating further!
-    #from Ganga.Core import monitoring_component
-    #if monitoring_component is not None:
-    #    monitoring_component.disableMonitoring()
+    from Ganga.Core import monitoring_component
+    if monitoring_component is not None:
+        from Ganga.Core.MonitoringComponent.Local_GangaMC_Service import getStackTrace
+        getStackTrace()
+        monitoring_component.disableMonitoring()
+        #monitoring_component.stop()
 
     ## This will stop the Registries flat but we may still have threads processing data!
-    from Ganga.Core.InternalServices import Coordinator
-    if Coordinator.servicesEnabled:
-        Coordinator.disableInternalServices( shutdown = True )
+    #from Ganga.Core.InternalServices import Coordinator
+    #if Coordinator.servicesEnabled:
+    #    Coordinator.disableInternalServices( shutdown = True )
 
+    ## Stop the tasks system from running it's GangaThread before we get to the GangaThread shutdown section!
     from Ganga.GPIDev.Lib.Tasks import stopTasks
     stopTasks()
 
