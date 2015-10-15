6058cdbf46441f7be7ff2fcd4a6900246e9f2611
rcurrie <rcurrie@cern.ch>
2015-10-15 10:36:08 +0200

    Minor code cleanup and notes

--- python/GangaGaudi/Lib/Applications/cmakeUtils.py
+++ python/GangaGaudi/Lib/Applications/cmakeUtils.py
@@ -4,19 +4,23 @@ import os
 
 
 def get_user_platform(env=os.environ):
-    ''' 
+    '''
+    Doesn't appear to be used within Ganga!
+    This will likely still extract the code as in the CMT package using env['CMTCONFIG']
     '''
     raise NotImplementedError
 
 
 def update_project_path(user_release_area, env=os.environ):
     '''
+    Doesn't appear to be used within Ganga!
     '''
     return NotImplementedError
 
 
 def get_user_dlls(appname, version, user_release_area, platform, env):
     '''
+
     '''
     return NotImplementedError
 
