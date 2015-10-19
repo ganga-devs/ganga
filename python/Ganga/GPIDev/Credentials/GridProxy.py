f740a7ff16bd2e592e8055ebdead7d5d33399816
rcurrie <rcurrie@cern.ch>
2015-10-19 15:47:33 +0200

    Adding missing isType

--- python/Ganga/GPIDev/Credentials/GridProxy.py
+++ python/Ganga/GPIDev/Credentials/GridProxy.py
@@ -96,6 +96,7 @@ import os
 import re
 import time
 
+from Ganga.GPIDev.Base.Proxy import isType
 from Ganga.GPIDev.Credentials.ICredential import ICommandSet, ICredential, registerCommandSet
 from Ganga.GPIDev.Schema import SimpleItem
 from Ganga.Utility.logging import getLogger
