6342cfa5aa8b7b346a806ba54baeccd1acad69d0
rcurrie <rcurrie@cern.ch>
2015-10-20 17:17:24 +0200

    Adding missing isType import

--- python/Ganga/GPIDev/Adapters/ISplitter.py
+++ python/Ganga/GPIDev/Adapters/ISplitter.py
@@ -5,10 +5,9 @@
 ##########################################################################
 
 from Ganga.GPIDev.Base import GangaObject
-from Ganga.GPIDev.Base.Proxy import TypeMismatchError
+from Ganga.GPIDev.Base.Proxy import TypeMismatchError, isType
 from Ganga.GPIDev.Schema import Schema, Version
 from Ganga.Utility.util import containsGangaObjects, isNestedList
-
 
 class SplittingError(Exception):
 
