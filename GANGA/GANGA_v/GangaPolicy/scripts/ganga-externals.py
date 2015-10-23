7eae0ac7477e76f96bc0be3e9ecafac25fced051
rcurrie <rcurrie@cern.ch>
2015-10-23 17:42:31 +0200

    Reverting to IPython 0.6

--- GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
+++ GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
@@ -9,7 +9,7 @@ externals_noarch = [['httplib2', '0.8'],
                     ['stomputil','2.3'],
                     ['figleaf', '0.6'],
                     ['PYTF','1.6'],
-                    ['ipython', '3.2.1']]
+                    ['ipython', '0.6.13_ganga_patch1']]
 
 
 # These are the packages with architecture dependent code.
@@ -20,5 +20,5 @@ archs = ['x86_64-slc6-gcc48-opt']
 # the numeric versions so list here the bits that bloat the version
 # numbers and what to replace them with when checking. i.e. '.'
 # seperated digits are compared
-version_check_bloats = { 'ipython':[('.')] }
+version_check_bloats = { 'ipython':[('_ganga_patch','.')] }
 
