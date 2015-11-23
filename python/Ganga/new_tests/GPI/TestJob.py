b706eb4c0741bf8e35dd3686ab9535b6ae6f56a8
Matt Williams <Matt.Williams@cern.ch>
2015-11-23 15:05:33 +0000

    Add second integration test

--- python/Ganga/new_tests/GPI/TestJob.py
+++ python/Ganga/new_tests/GPI/TestJob.py
@@ -5,3 +5,8 @@ class TestJob(GangaUnitTest):
     def testJobCreate(self):
         from Ganga.GPI import Job
         j = Job()
+
+    def testJobSubmit(self):
+        from Ganga.GPI import Job
+        j = Job()
+        j.submit()
