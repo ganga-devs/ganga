2b6c63226c6718e0f7cd499b9b5bf3d9f351dadb
rcurrie <rcurrie@cern.ch>
2015-11-30 13:31:55 +0100

    Fixing #1

--- GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
+++ GANGA/GANGA_v/GangaPolicy/scripts/ganga-externals.py
@@ -6,17 +6,12 @@ externals_noarch = [['httplib2', '0.8'],
                     ['python-gflags', '2.0'],
                     ['google-api-python-client', '1.1'],
                     ['paramiko', '1.7.3'],
-                    ['stomputil','2.3'],
-                    ['ipython', '0.6.13_ganga_patch1']]
+                    ['stomputil','2.4'],
+                    ['ipython', '1.2.1']]
 
 
 # These are the packages with architecture dependent code.
 externals_arch = [['pycrypto','2.0.1']]
 archs = ['x86_64-slc6-gcc48-opt']
 
-# when comparing to top level Ganga PACKAGE.py need to compare just
-# the numeric versions so list here the bits that bloat the version
-# numbers and what to replace them with when checking. i.e. '.'
-# seperated digits are compared
-version_check_bloats = { 'ipython':[('_ganga_patch','.')] }
 
