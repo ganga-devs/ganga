bc420b71f5b245f2848153933e6a0f76e4253340
Matt Williams <Matt.Williams@cern.ch>
2016-01-12 12:11:37 +0000

    Don't remove test file twice

--- python/Ganga/new_tests/GPI/Bugs/Savannah13979.py
+++ python/Ganga/new_tests/GPI/Bugs/Savannah13979.py
@@ -7,20 +7,19 @@ class Savannah13979(GangaUnitTest):
     def Savannah13979(self):
         from Ganga.GPI import Job, Executable, export, load
 
-        import os
         self.fname = 'test_savannah_13979.ganga'
         j = Job(application=Executable())
+
         # One-line parameter
         j.application.args = ['a']
         export(j, self.fname)
         self.assertTrue(load(self.fname))
+
         # Two-line parameter
         j.application.args = ['''a
         b''']
         export(j, self.fname)
         self.assertTrue(load(self.fname))
-
-        os.remove(self.fname)
 
     def tearDown(self):
         import os
