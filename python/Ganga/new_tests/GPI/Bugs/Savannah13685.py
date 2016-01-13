9702459b39ba923b935b7afcd77ad5f42f36b024
Matt Williams <Matt.Williams@cern.ch>
2016-01-13 15:11:14 +0000

    Migrate some more tests to the new framework

---
+++ python/Ganga/new_tests/GPI/Bugs/Savannah13685.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from ..GangaUnitTest import GangaUnitTest
+from Ganga.GPIDev.Lib.Job.Job import JobError
+
+
+class Savannah13685(GangaUnitTest):
+    def Savannah13685(self):
+        from Ganga.GPI import Job
+
+        j = Job()
+        j.application.exe = 'sleep 6'
+
+        self.assertRaises(JobError, j.submit)
+
+        j.application.exe = 'bin/hostname'
+
+        self.assertRaises(JobError, j.submit)
