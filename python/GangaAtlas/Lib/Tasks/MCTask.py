cc15442320e53ef4eda2793cbe03b951d0a38c18
Mark Slater <mslater@cern.ch>
2016-01-13 09:28:27 +0000

    Fixed some imports of fx/Effects after config move

--- python/GangaAtlas/Lib/Tasks/MCTask.py
+++ python/GangaAtlas/Lib/Tasks/MCTask.py
@@ -3,8 +3,10 @@ from Ganga.GPIDev.Lib.Tasks import Task
 from MCTransforms import EvgenTransform, SimulTransform, ReconTransform
 from GangaAtlas.Lib.AthenaMC.AthenaMCDatasets import AthenaMCInputDatasets
 from Ganga.GPIDev.Schema import *
-from Ganga.Utility.ColourText import ANSIMarkup
+from Ganga.Utility.ColourText import ANSIMarkup, fgcol, Effects
 markup = ANSIMarkup()
+fx = Effects()
+
 
 from Ganga.GPIDev.Base.Objects import Node
 
