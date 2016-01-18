f9b70dd3c85828d8294125a7c1c9d876a82782ec
Matt Williams <Matt.Williams@cern.ch>
2016-01-18 15:50:13 +0000

    Add the ability to run tests using setup.py
    Use:
    
        python setup.py tests -t unit
        python setup.py tests -t integration
        python setup.py tests -t all
    
    or
    
        python setup.py tests
    
    to default to running the unit tests

--- setup.py
+++ setup.py
@@ -1,10 +1,43 @@
 from setuptools import setup
 from setuptools import find_packages
+from setuptools import Command
+import subprocess
+import os
 
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+
+class RunTestsCommand(Command):
+    """
+    A custom setuptools command to run the Ganga test suite.
+    """
+    description = 'run the Ganga test suite'
+    all_types = ['unit', 'integration', 'all']
+    user_options = [
+        ('type=', 't', 'the type of tests: [{0}]'.format(', '.join(all_types))),
+    ]
+
+    def initialize_options(self):
+        self.type = 'unit'
+
+    def finalize_options(self):
+        if not self.type in self.all_types:
+            raise Exception('Test type must be [{0}]'.format(', '.join(self.all_types)))
+
+    def run(self):
+        os.environ['GANGASYSROOT'] = os.path.dirname(os.path.realpath(__file__))
+        if self.type == 'unit':
+            print('Running unit tests')
+            subprocess.check_call('nosetests Ganga/new_tests/*.py', cwd='python', shell=True)
+        elif self.type == 'integration':
+            print('Running integration tests')
+            subprocess.check_call('nosetests Ganga/new_tests/GPI --testmatch="(?:\\b|_)([Tt]est|Savannah|JIRA)"', cwd='python', shell=True)
+        elif self.type == 'all':
+            print('Running all tests')
+            subprocess.check_call('nosetests Ganga/new_tests/*.py Ganga/new_tests/GPI --testmatch="(?:\\b|_)([Tt]est|Savannah|JIRA)"', cwd='python', shell=True)
 
 
 setup(name='ganga',
@@ -31,4 +64,7 @@ setup(name='ganga',
           'Programming Language :: Python :: 2.7',
       ],
       package_data={'Ganga': ['Runtime/HEAD_CONFIG.INI']},
+      cmdclass={
+          'tests': RunTestsCommand,
+      },
       )
