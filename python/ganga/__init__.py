9faf8ee2caad47d1189c09681c7e56931e9a0d5a
Mark Slater <mslater@cern.ch>
2016-01-20 14:51:35 +0000

    Added registries to GPI and started them after importing GPI

--- python/ganga/__init__.py
+++ python/ganga/__init__.py
@@ -6,9 +6,6 @@ import Ganga.GPIDev.Lib.Registry
 from Ganga.Utility.Plugin import allPlugins
 import Ganga.GPI
 
-# start up the registries
-bootstrap()
-
 # make all plugins visible in both the GPI and ganga.*
 for k in allPlugins.allCategories():
     for n in allPlugins.allClasses(k):
@@ -16,3 +13,8 @@ for k in allPlugins.allCategories():
         if not cls._declared_property('hidden'):
             setattr(ganga, n, cls._proxyClass)
             setattr(Ganga.GPI, n, cls._proxyClass)
+
+# start up the registries
+for n, k, d in bootstrap():
+    setattr(ganga, n, k)
+    setattr(Ganga.GPI, n, k)
