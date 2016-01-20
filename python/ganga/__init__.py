4d48f9781c103fcedabe0afbf192b8a53bc240a5
Mark Slater <mslater@cern.ch>
2016-01-20 14:40:53 +0000

    Made sure Registries are started up correctly

--- python/ganga/__init__.py
+++ python/ganga/__init__.py
@@ -1,12 +1,15 @@
 import ganga
 
 from Ganga.Runtime import plugins
-
-from Ganga.Runtime.GPIexport import exportToGPI
+from Ganga.Runtime.Repository_runtime import bootstrap
+import Ganga.GPIDev.Lib.Registry
 from Ganga.Utility.Plugin import allPlugins
 import Ganga.GPI
 
-# make all plugins visible in GPI
+# start up the registries
+bootstrap()
+
+# make all plugins visible in both the GPI and ganga.*
 for k in allPlugins.allCategories():
     for n in allPlugins.allClasses(k):
         cls = allPlugins.find(k, n)
