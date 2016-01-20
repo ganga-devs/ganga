07e74629da87bd88726ddfc0fad1be15e50b6de1
Matt Mottram <mjmottram@gmail.com>
2016-01-20 14:29:04 +0000

    Fix to expend environment variables in RUNTIME_PATH.

--- python/Ganga/Runtime/bootstrap.py
+++ python/Ganga/Runtime/bootstrap.py
@@ -762,14 +762,14 @@ under certain conditions; type license() for details.
 
             
             import inspect
-            GangaRootPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), '../..'))
-            def transform(x):
-                return os.path.normpath(Ganga.Utility.files.expandfilename(os.path.join(GangaRootPath,x)))
-
             from os.path import expandvars, expanduser
 
-            paths = map(transform, filter(lambda x: expandvars(expanduser(x)), config['RUNTIME_PATH'].split(':')))
+            GangaRootPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), '../..'))
+            def transform(x):
+                return os.path.normpath(Ganga.Utility.files.expandfilename(
+                        os.path.join(GangaRootPath, expandvars(x))))
 
+            paths = map(transform, filter(lambda x: expandvars(expanduser(x)), config['RUNTIME_PATH'].split(':')))
 
             for path in paths:
                 r = RuntimePackage(path)
