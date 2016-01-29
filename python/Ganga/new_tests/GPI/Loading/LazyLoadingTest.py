9384eb64816d6185ff2fd0966bac571bea008462
rcurrie <rcurrie@cern.ch>
2016-01-29 18:49:22 +0100

    Small changes to asserts

--- python/Ganga/new_tests/GPI/Loading/LazyLoadingTest.py
+++ python/Ganga/new_tests/GPI/Loading/LazyLoadingTest.py
@@ -17,13 +17,13 @@ class LazyLoadingTest(GangaUnitTest):
 
         from Ganga.GPI import Job, jobs
         j=Job()
-        self.assertTrue(len(jobs) == 1)
+        self.assertEqual(len(jobs), 1) # Don't really gain anything from assertEqual...
 
     def test_b_JobNotLoaded(self):
         """ Second get the job and check that getting it via jobs doesn't cause it to be loaded"""
         from Ganga.GPI import jobs
 
-        self.assertTrue(len(jobs) == 1)
+        self.assertEqual(len(jobs), 1)
 
         print("len: %s" % str(len(jobs)))
 
@@ -34,13 +34,13 @@ class LazyLoadingTest(GangaUnitTest):
 
         has_loaded_job = raw_j._getRegistry().has_loaded(raw_j)
 
-        self.assertTrue(has_loaded_job == False)
+        self.assertFalse(has_loaded_job)
 
     def test_c_JobLoaded(self):
         """ Third do something to trigger a loading of a Job and then test if it's loaded"""
         from Ganga.GPI import jobs
 
-        self.assertTrue(len(jobs) == 1)
+        self.assertEqual(len(jobs), 1)
 
         j = jobs(0)
 
@@ -52,17 +52,17 @@ class LazyLoadingTest(GangaUnitTest):
 
         has_loaded_job = raw_j._getRegistry().has_loaded(raw_j)
 
-        self.assertTrue(has_loaded_job == True)
+        self.assertTrue(has_loaded_job)
 
     def test_d_JobRemoval(self):
         """ Fourth make sure that we get rid of the jobs safely"""
         from Ganga.GPI import jobs
 
-        self.assertTrue(len(jobs) == 1)
+        self.assertEqual(len(jobs), 1)
 
         jobs(0).remove()
 
-        self.assertTrue(len(jobs) == 0)
+        self.assertEqual(len(jobs), 0)
 
         from Ganga.Utility.Config import setConfigOption
         setConfigOption('TestingFramework', 'AutoCleanup', 'True')
