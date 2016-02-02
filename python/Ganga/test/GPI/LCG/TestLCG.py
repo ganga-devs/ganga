36a31f3704ddfe3ce5adbaae8420c04a108fd702
Matt Williams <matt@milliams.com>
2016-02-02 11:33:28 +0000

    Add a test for LCG jobs and selecting the correct proxy

---
+++ python/Ganga/test/GPI/LCG/TestLCG.py
@@ -0,0 +1,22 @@
+from Ganga.testlib.mark import external
+from Ganga.testlib.monitoring import run_until_completed
+
+
+#@external
+def test_job_submit(gpi):
+    from Ganga.GPI import Job, LCG
+
+    j = Job()
+    j.backend = LCG()
+    j.submit()
+    assert run_until_completed(j, timeout=600, sleep_period=10), 'Timeout on job submission: job is still not finished'
+
+
+#@external
+def test_job_kill(gpi):
+    from Ganga.GPI import Job, LCG
+
+    j = Job()
+    j.backend = LCG()
+    j.submit()
+    j.kill()
