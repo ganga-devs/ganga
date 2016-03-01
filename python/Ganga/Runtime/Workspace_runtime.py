0934a784289655bf1d061d156bfbef4c221246f7
Matt Williams <Matt.Williams@cern.ch>
2016-03-01 12:53:04 +0000

    Remove requiresGridProxy()
    These have returned nothing but False in years.

--- python/Ganga/Runtime/Workspace_runtime.py
+++ python/Ganga/Runtime/Workspace_runtime.py
@@ -2,10 +2,6 @@ def requiresAfsToken():
     return getLocalRoot().find('/afs') == 0
 
 
-def requiresGridProxy():
-    return False
-
-
 def getLocalRoot():
     from Ganga.Utility.files import fullpath
     import Ganga.Core.FileWorkspace
