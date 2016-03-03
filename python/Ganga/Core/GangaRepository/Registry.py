250ab7b72b1b8dbcced13643a3bc762ee3483a06
Matt Williams <matt@milliams.com>
2016-03-03 12:08:26 +0000

    Set the dirty flag to False after a flush

--- python/Ganga/Core/GangaRepository/Registry.py
+++ python/Ganga/Core/GangaRepository/Registry.py
@@ -640,6 +640,7 @@ class Registry(object):
                 if not obj._dirty:
                     continue
                 self.repository.flush([obj_id])
+                obj._dirty = False
                 self.repository.unlock([obj_id])
 
     @synchronised
