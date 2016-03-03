1d4b378b00cd0f4c37fbedb8f92f85c485888dfc
Robert Currie <rob-c@users.noreply.github.com>
2016-03-03 13:36:29 +0000

    Update TestSelect.py
    Removing brackets

--- python/Ganga/new_tests/GPI/Parser/TestSelect.py
+++ python/Ganga/new_tests/GPI/Parser/TestSelect.py
@@ -11,38 +11,38 @@ class TestSelect(GangaUnitTest):
 
         job_slice =  jobs.select(status='new')
 
-        assert(len(job_slice) == 1)
+        assert len(job_slice) == 1
 
         j.submit()
 
         from GangaTest.Framework.utils import sleep_until_completed
         sleep_until_completed(j)
 
-        assert(j.status == 'completed')
+        assert j.status == 'completed'
 
         job_slice2 =  jobs.select(status='completed')
 
-        assert(len(job_slice2) == 1)
+        assert len(job_slice2) == 1
 
     def test_SelectSubJob(self):
 
         from Ganga.GPI import Job, jobs, ArgSplitter
         j=Job(splitter=ArgSplitter(args=[[1],[2],[3]]))
 
-        assert(len(j.subjobs) == 0)
+        assert len(j.subjobs) == 0
 
         j.submit()
 
         from GangaTest.Framework.utils import sleep_until_completed
         sleep_until_completed(j)
 
-        assert(j.status == 'completed')
+        assert j.status == 'completed'
 
-        assert(len(j.subjobs) == 3)
+        assert len(j.subjobs) == 3
 
         job_slice =  j.subjobs.select(status='completed')
 
 
-        assert(len(job_slice) == len(j.subjobs))
+        assert len(job_slice) == len(j.subjobs)
 
 
