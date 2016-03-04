7f7735c788e19f8cfe1c5ac337831ae4cab629c2
Mark Slater <mslater@cern.ch>
2016-03-04 11:29:19 +0000

    Prints out stdout/err for this test that overy occasionally fails for no reason

--- python/Ganga/new_tests/GPI/Bugs/TestSavannah19123.py
+++ python/Ganga/new_tests/GPI/Bugs/TestSavannah19123.py
@@ -24,14 +24,24 @@ class TestSavannah19123(GangaUnitTest):
 
         j = Job()
         j.application.exe = 'bash'
-        j.application.args = ['-c', 'for i in `seq 1 30`; do echo $i; sleep 1; done']
+        j.application.args = ['-c', 'for i in `seq 1 2`; do echo $i; sleep 1; done']
         j.backend = Local()
 
         j.submit()
 
         check(False)
 
-        sleep_until_state(j, 5, 'running')
+        if not sleep_until_state(j, 5, 'running'):
+            # problem with the test - print out stdout/stderr and assert
+            for fn in ['stdout','stderr']:
+                fn = os.path.join(j.outputdir,fn)
+                print " ----  Contents of " + fn
+                if os.path.exists(fn):
+                    print open(fn).read()
+                else:
+                    print "NO FILE AVAILABLE"
+
+            self.assertEqual(j.status, 'running')
 
         j.kill()
 
