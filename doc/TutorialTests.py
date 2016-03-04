59342cc8335f5b4bf388c4814f2f7aa6655baa08
Mark Slater <mslater@cern.ch>
2016-03-04 09:38:42 +0000

    Started shifting the Wiki docs to the repo and adding examples as tests

---
+++ doc/TutorialTests.py
@@ -0,0 +1,38 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from ganga import *
+
+class TutorialTests(unittest.TestCase):
+
+    # A set of tests that are explicitly quoted in the tutorial docs
+    # DON'T CHANGE THE COMMENTS as these are used to pick out the code!
+    # Should also add more checks for completed jobs, etc.
+    def testInstallAndBasicUsage(self):
+
+        # -- INSTALLANDBASICUSAGE HELP START
+        help(Job)
+        # -- INSTALLANDBASICUSAGE HELP STOP
+
+        # -- INSTALLANDBASICUSAGE SUBMIT START
+        j = Job()
+        j.submit()
+        # -- INSTALLANDBASICUSAGE SUBMIT STOP
+
+        # -- INSTALLANDBASICUSAGE JOBS START
+        jobs(0)
+        # -- INSTALLANDBASICUSAGE JOBS STOP
+
+        # -- INSTALLANDBASICUSAGE JOBSAPP START
+        jobs(0).application
+        # -- INSTALLANDBASICUSAGE JOBSAPP STOP
+
+        # -- INSTALLANDBASICUSAGE EXECFILE START
+        open('submit.py', 'w').write("""
+        j = Job()
+        j.submit()
+        """)
+        execfile('submit.py')
+        # -- INSTALLANDBASICUSAGE EXECFILE STOP
--- doc/UserGuide/index.rst
+++ doc/UserGuide/index.rst
@@ -1,5 +1,23 @@
 User Guide
 ==========
 
-This tutorial is in several stages that cover all of the basic and more of the advanced functionality that Ganga provides.
-To follow it properly, you should go through each stage in turn.
+This tutorial is in several stages that cover all of the basic and more of the advanced functionality that
+Ganga provides. To follow it properly, you should go through each stage in turn. Please post an issue if
+you encounter any problems!
+
+.. toctree::
+    InstallAndBasicUsage
+
+Configuration
+Job Manipulation
+4. Running Executables
+5. Using different backends
+6. Input and Output Data
+7. Splitters
+8. Post Processors
+9. Miscellaneous functionality
+10. Queues
+11. Tasks
+12. Pure Python Usage
+13. Ganga as a Service
+14. Developing a New Application
