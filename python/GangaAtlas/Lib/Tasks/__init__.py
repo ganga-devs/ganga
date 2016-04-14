344021d0e0bf2f4e2dc04e684e6bb2a8511dada7
Mark Slater <mslater@cern.ch>
2016-04-14 13:55:12 +0100

    Fixed some more dodgy logger imports

--- python/GangaAtlas/Lib/Tasks/__init__.py
+++ python/GangaAtlas/Lib/Tasks/__init__.py
@@ -5,7 +5,3 @@ from AtlasTask import AtlasTask
 from AtlasTransform import AtlasTransform
 from AtlasUnit import AtlasUnit
 
-# Start Logger
-#import Ganga.Utility.logging
-#logger = Ganga.Utility.logging.getLogger()
-from Ganga.GPIDev.Lib.Tasks.common import logger
