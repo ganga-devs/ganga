d1f3cd822542c0450c7c492a0ebb74851a039508
rcurrie <rcurrie@cern.ch>
2016-05-11 16:28:47 +0200

    Removing Dataset changes from PR

--- python/Ganga/GPIDev/Lib/Dataset/GangaDataset.py
+++ python/Ganga/GPIDev/Lib/Dataset/GangaDataset.py
@@ -19,11 +19,12 @@ class GangaDataset(Dataset):
     schema = {}
     docstr = 'List of File objects'
     schema['files'] = GangaFileItem(defvalue=[], sequence=1, doc="list of file objects that will be the inputdata for the job")
-    schema['treat_as_inputfiles'] = SimpleItem(defvalue=False, doc="Treat the inputdata as inputfiles, i.e. copy the inputdata to the WN")
+    schema['treat_as_inputfiles'] = SimpleItem(
+        defvalue=False, doc="Treat the inputdata as inputfiles, i.e. copy the inputdata to the WN")
     _schema = Schema(Version(3, 0), schema)
     _category = 'datasets'
     _name = "GangaDataset"
-    _exportmethods = ['append', 'extend', '__len__', 'isEmtpy', 'getFileNames', 'getFilenameList', '__getitem__']
+    _exportmethods = []
 
     def __init__(self, files=None):
         if files is None:
