cc0fd631bd2795ae4de079632009bf1e56ac0e12
Matt Williams <Matt.Williams@cern.ch>
2016-05-18 10:14:21 +0100

    Move the pytest marks to their own module

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-import pytest
 def pytest_addoption(parser):
     parser.addoption("--runexternals", action="store_true",
         help="run tests that depend on external services")
+
--- python/Ganga/testlib/__init__.py
+++ python/Ganga/testlib/__init__.py
@@ -1,6 +1,1 @@
-import pytest
 
-external = pytest.mark.skipif(
-    not pytest.config.getoption("--runexternals"),
-    reason="need --runexternals option to run external tests"
-)
