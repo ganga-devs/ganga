f7938ae7e286c444ced2148319d7e6c51b2f3570
rcurrie <rcurrie@cern.ch>
2016-05-23 12:10:28 +0200

    Trying to fis some shutdown issues

--- python/Ganga/Core/GangaThread/GangaThread.py
+++ python/Ganga/Core/GangaThread/GangaThread.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import
-from threading import Thread
+from threading import Thread, RLock
 from .GangaThreadPool import GangaThreadPool
 from Ganga.Utility.logging import getLogger
+import functools
 logger = getLogger()
+
+def synchronised(f):
+    @functools.wraps(f)
+    def decorated(self, *args, **kwargs):
+        with self._lock:
+            return f(self, *args, **kwargs)
+    return decorated
 
 
 class GangaThread(Thread):
@@ -20,7 +28,9 @@ class GangaThread(Thread):
         if auto_register:
             tpool = GangaThreadPool.getInstance()
             tpool.addServiceThread(self)
+        self._lock = RLock()
 
+    @synchronised
     def isCritical(self):
         """Return critical flag.
 
@@ -28,6 +38,7 @@ class GangaThread(Thread):
         """
         return self.__critical
 
+    @synchronised
     def setCritical(self, critical):
         """Set critical flag, which can be used for example in shutdown
         algorithms. See Ganga/Core/__init__.py for example.
@@ -36,16 +47,20 @@ class GangaThread(Thread):
         """
         self.__critical = critical
 
+    @synchronised
     def should_stop(self):
         return self.__should_stop_flag
 
+    @synchronised
     def stop(self):
         if not self.__should_stop_flag:
             logger.debug("Stopping: %s", self.gangaName)
             self.__should_stop_flag = True
 
+    @synchronised
     def unregister(self):
         GangaThreadPool.getInstance().delServiceThread(self)
 
+    @synchronised
     def register(self):
         GangaThreadPool.getInstance().addServiceThread(self)
