8d73ac740b94468322ad0d4746497acb7a513803
Matt Williams <Matt.Williams@cern.ch>
2016-06-07 16:27:53 +0100

    Mark tests as external

--- python/Ganga/test/GPI/LCG/TestLCG.py
+++ python/Ganga/test/GPI/LCG/TestLCG.py
@@ -2,7 +2,7 @@ from Ganga.testlib.mark import external
 from Ganga.testlib.monitoring import run_until_completed
 
 
-#@external
+@external
 def test_job_complete(gpi):
     from Ganga.GPI import Job, LCG
 
@@ -12,7 +12,7 @@ def test_job_complete(gpi):
     assert run_until_completed(j, timeout=1200, sleep_period=10), 'Timeout on job submission: job is still not finished'
 
 
-#@external
+@external
 def test_job_kill(gpi):
     from Ganga.GPI import Job, LCG
 
