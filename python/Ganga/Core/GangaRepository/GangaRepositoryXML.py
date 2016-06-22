4c2cff072981b17c71888e6f78b90c929109ba29
Robert Currie <rcurrie@cern.ch>
2016-06-22 11:00:05 +0100

    Update GangaRepositoryXML.py
    Making use of edit button

--- python/Ganga/Core/GangaRepository/GangaRepositoryXML.py
+++ python/Ganga/Core/GangaRepository/GangaRepositoryXML.py
@@ -131,7 +131,7 @@ def rmrf(name, count=0):
         try:
             remove_name = name
             if not remove_name.endswith('__to_be_deleted'):
-                remove_name += '_%s__to_be_deleted_' % str(time.time())
+                remove_name += '_%s__to_be_deleted_' % time.time()
                 os.rename(name, remove_name)
                 #logger.debug("Move completed")
         except OSError as err:
