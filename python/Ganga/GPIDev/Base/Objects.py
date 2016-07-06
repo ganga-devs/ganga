3c664b4429fd541125b7dc9d2580b50da66094ca
Robert Currie <rcurrie@cern.ch>
2016-07-06 13:55:22 +0100

    Update Objects.py

--- python/Ganga/GPIDev/Base/Objects.py
+++ python/Ganga/GPIDev/Base/Objects.py
@@ -273,8 +273,8 @@ def synchronised_set_descriptor(set_function):
         if obj is None:
             return set_function(self, obj, type_or_value)
 
-        with obj._internal_lock:
-            with obj.const_lock:
+        with obj.const_lock:
+            with obj._internal_lock:
                 return set_function(self, obj, type_or_value)
     return decorated
 
