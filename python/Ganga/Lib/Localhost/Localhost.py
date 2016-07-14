82cfa1712488f0d35b09b62c492d6d141333808a
rcurrie <rcurrie@cern.ch>
2016-07-14 15:27:04 +0200

    Reverting change

--- python/Ganga/Lib/Localhost/Localhost.py
+++ python/Ganga/Lib/Localhost/Localhost.py
@@ -21,7 +21,6 @@ import Ganga.Utility.logging
 import Ganga.Utility.Config
 
 from Ganga.GPIDev.Base.Proxy import getName, stripProxy
-from Ganga.GPIDev.Base.Objects import Descriptor
 
 logger = Ganga.Utility.logging.getLogger()
 config = Ganga.Utility.Config.getConfig('Local')
