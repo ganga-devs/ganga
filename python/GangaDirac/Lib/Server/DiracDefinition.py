5abfcff8f255f1723c6fc31d786665a43bc36435
Robert Currie <rcurrie@cern.ch>
2016-08-04 11:06:39 +0100

    Update DiracDefinition.py

--- python/GangaDirac/Lib/Server/DiracDefinition.py
+++ python/GangaDirac/Lib/Server/DiracDefinition.py
@@ -1,4 +1,10 @@
 ## NB parseCommandLine first then import Dirac!!
+import os
+import sys
+import time
+import datetime
+import glob
+import pickle
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
 from DIRAC.Interfaces.API.Dirac import Dirac
