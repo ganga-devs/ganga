63b739dbe3a0ad13d6ad029e2f0f66b0b787926a
rcurrie <rcurrie@cern.ch>
2016-09-26 15:14:49 +0200

    Adding dummy classes to make things sane

--- python/Ganga/GPIDev/Credentials/ICredential.py
+++ python/Ganga/GPIDev/Credentials/ICredential.py
@@ -82,13 +82,13 @@ logRepeatDuration = 120.0
 
 
 class VomsProxyInfo(GangaObject):
-
     """ dummy class to allow for loading of 6.3.x jobs inside 6.2.x. PLEASE NEVER USE THIS! """
+    _schema = Schema(Version(1, 0), {})
     _category = 'CredentialRequirement'
 
 class DiracProxyInfo(GangaObject):
-    
     """ dummy class to allow for loading of 6.3.x jobs inside 6.2.x. PLEASE NEVER USE THIS! """
+    _schema = Schema(Version(1, 0), {})
     _category = 'CredentialRequirement'
 
 def registerCommandSet(commandClass=None):
