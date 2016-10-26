c6d0d1f2c478208e5bdde950939200f28335d072
Robert Currie <rcurrie@cern.ch>
2016-10-26 14:15:15 +0100

    Fixing bug in opts parser for GaudiExec (#843)
    * Fixing bug in opts parser for GaudiExec
    * Adding new external tests for GaudiExec
    * Using ./run and adding check for initial build having been run for a lb-dev app on disk

---
+++ python/GangaLHCb/test/GPI/TestExternalGaudiExec.py
@@ -0,0 +1,131 @@
+from __future__ import absolute_import
+
+from os import path
+import shutil
+from tempfile import mkdtemp
+
+from Ganga.GPIDev.Lib.File.FileBuffer import FileBuffer
+from Ganga.testlib.GangaUnitTest import GangaUnitTest
+from Ganga.testlib.mark import external
+from Ganga.testlib.monitoring import run_until_completed
+
+def latestLbDevVersion(app):
+    import subprocess
+    pipe = subprocess.Popen('lb-dev %s -l' % app, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = pipe.communicate()
+    return stdout.split()[0]
+
+def latestDaVinci():
+    return latestLbDevVersion('DaVinci')
+
+def LFNstr():
+    return "/not/a/LFN"
+
+def inputOptsFile():
+    return """
+from Gaudi.Configuration import *
+
+EventSelector().Input   = [
+"DATAFILE='LFN:%s' TYP='POOL_ROOTTREE' OPT='READ'",
+]
+""" % LFNstr
+
+
+@external
+class TestExternalGaudiExec(GangaUnitTest):
+
+    tmpdir_release = mkdtemp()
+
+    def setUp(self):
+        """Make sure that the Job object isn't destroyed between tests"""
+        extra_opts = [('TestingFramework', 'AutoCleanup', 'False')]
+        super(TestExternalGaudiExec, self).setUp(extra_opts=extra_opts)
+
+    def testConstruction(self):
+        """
+        This tests that we can construct a GaudiExec object in a simple way
+        """
+        from Ganga.GPI import Job, prepareGaudiExec
+
+        j = Job(application=prepareGaudiExec('DaVinci', latestDaVinci(), TestExternalGaudiExec.tmpdir_release))
+
+        assert j.application.directory == path.join(TestExternalGaudiExec.tmpdir_release, 'DaVinciDev_%s' % latestDaVinci())
+
+        assert path.isfile(path.join(TestExternalGaudiExec.tmpdir_release, 'DaVinciDev_%s' % latestDaVinci(), 'run'))
+
+        assert path.isfile(path.join(TestExternalGaudiExec.tmpdir_release, 'DaVinciDev_%s' % latestDaVinci(), 'Makefile'))
+
+    def testParseInputFile(self):
+        """
+        Test that we can parse a fake opts file and get the inputdata from it
+        """
+        from Ganga.GPI import jobs
+
+        j = jobs[-1]
+
+        myOptsFile = path.join(TestExternalGaudiExec.tmpdir_release, 'myOpts.py')
+
+        FileBuffer('myOpts.py', inputOptsFile()).create(myOptsFile)
+
+        assert path.isfile(myOptsFile)
+
+        j.application.readInputData(myOptsFile)
+
+        assert len(j.inputdata) == 1
+
+    def testPrepareJob(self):
+
+        from Ganga.GPI import Job, LocalFile, prepareGaudiExec
+
+        j = Job(application=prepareGaudiExec('DaVinci', latestDaVinci(), TestExternalGaudiExec.tmpdir_release))
+
+        myHelloOpts = path.join(TestExternalGaudiExec.tmpdir_release, 'hello.py')
+
+        FileBuffer('hello.py', 'print("Hello")').create(myHelloOpts)
+
+        assert path.isfile(myHelloOpts)
+
+        j.application.options=[LocalFile(myHelloOpts)]
+
+        j.prepare()
+
+    def testSubmitJob(self):
+
+        from Ganga.GPI import jobs
+        
+        j = jobs[-1]
+        
+        j.submit()
+
+    def testSubmitJobComplete(self):
+        """
+        Test that the job completes successfully
+        """
+
+        from Ganga.GPI import jobs
+
+        j = jobs[-1]
+
+        run_until_completed(j)
+
+        assert j.status == 'completed'
+
+        outputfile = path.join(j.outputdir, 'stdout')
+
+        assert path.isfile(outputfile)
+
+        assert 'hello.py' in open(outputfile).read()
+
+        assert 'data.py' in open(outputfile).read()
+
+        assert 'Hello' in open(outputfile).read()
+
+        assert j.application.platform in open(outputfile).read()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Remove the 'release area'
+        """
+        shutil.rmtree(cls.tmpdir_release, ignore_errors=True)
+
