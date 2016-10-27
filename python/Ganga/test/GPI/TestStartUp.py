acc7f534e82f23a832900e0df4c2ad2172449613
rcurrie <rcurrie@cern.ch>
2016-10-27 23:42:07 +0200

    Fixing the Startup test to have the correct configuration

--- python/Ganga/test/GPI/TestStartUp.py
+++ python/Ganga/test/GPI/TestStartUp.py
@@ -4,7 +4,7 @@ import inspect
 import sys
 import shutil
 import glob
-
+from tempfile import mkdtemp
 
 # First clear away any configurations and temp files which may not be present on first launch
 homeDir = os.path.expanduser("~")
@@ -33,16 +33,40 @@ def standardSetup():
 standardSetup()
 del standardSetup
 
+this_dir = mkdtemp()
+
 def testStartUp():
     """ Lets test the startup of Ganga mimicking first launch """
     # Process options given at command line and in configuration file(s)
     # Perform environment setup and bootstrap
 
     from Ganga.Runtime import setupGanga
-    setupGanga(argv=['ganga', '--no-mon'], interactive=False)
+    argv = ['ganga', '--no-mon', '-o[Configuration]gangadir=%s' % this_dir, '-o[Configuration]RUNTIME_PATH=GangaTest']
+    setupGanga(argv=argv, interactive=False)
+
+    for this_file in ['.gangarc', '.ganga.log']:
+        assert os.path.isfile(os.path.join(homeDir, this_file))
+
+    # No way known to mimic IPython starting up in a simple way
+    #assert os.path.isdir(os.path.join(homeDir, '.ipython-ganga'))
+
+    for this_folder in ['repository',]:
+        assert os.path.isdir(os.path.join(this_dir, this_folder))
+
+    from Ganga.GPI import Job
+
+    j=Job()
+    j.submit()
+
+    for this_folder in ['shared', 'workspace']:
+        assert os.path.isdir(os.path.join(this_dir, this_folder))
 
 def testShutdown():
     """ Lets just call the shutdown here for safety """
-    from Ganga.Core.InternalServices import ShutdownManager
-    ShutdownManager._ganga_run_exitfuncs()
+
+    from Ganga.testlib.GangaUnitTest import stop_ganga
+
+    stop_ganga()
+
+    shutil.rmtree(this_dir, ignore_errors=True)
 
