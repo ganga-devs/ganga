b00982c0fdb76b78c7106e679310edf4e6c64432
Ulrik Egede <u.egede@imperial.ac.uk>
2017-01-20 18:08:09 +0000

    Updated test to only look at changes to local object.

--- python/Ganga/test/GPI/Bugs/TestSavannah14436.py
+++ python/Ganga/test/GPI/Bugs/TestSavannah14436.py
@@ -4,16 +4,16 @@ from Ganga.testlib.GangaUnitTest import GangaUnitTest
 class TestSavannah14436(GangaUnitTest):
 
     def test_ConfigDefaults(self):
-        from Ganga.GPI import Job
+        from Ganga.GPI import Executable
         from Ganga.Utility.Config import setConfigOption
 
         setConfigOption('defaults_Executable', 'exe', 'echo2')
         setConfigOption('defaults_Executable', 'args', ['Hello World2'])
-        j = Job()
-        assert j.application.exe == 'echo2'
-        assert j.application.args == ['Hello World2']
+        a = Executable()
+        assert a.exe == 'echo2'
+        assert a.args == ['Hello World2']
 
         setConfigOption('defaults_Executable', 'exe', '/bin/echo')
-        k = Job()
-        assert k.application.exe == '/bin/echo'
-        assert k.application.args == ['Hello World2']
+        k = Executable()
+        assert k.exe == '/bin/echo'
+        assert k.args == ['Hello World2']
