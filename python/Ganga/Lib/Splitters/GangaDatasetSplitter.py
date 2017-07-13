349539f7cd2075fc40d8c27ffe089f5c1bed9b9f
Mark Smith <mark.smith@cern.ch>
2017-07-13 11:41:51 +0200

    change of variable name

--- python/Ganga/Lib/Splitters/GangaDatasetSplitter.py
+++ python/Ganga/Lib/Splitters/GangaDatasetSplitter.py
@@ -43,14 +43,14 @@ class GangaDatasetSplitter(ISplitter):
             raise ApplicationConfigurationError(
                 "GangaDatasetSplitter couldn't find any files to split over")
 
-        oldType = type(job.inputdata)
+        masterType = type(job.inputdata)
 
         # split based on all the sub files
         fid = 0
         subjobs = []
         while fid < len(full_list):
             j = self.createSubjob(job)
-            j.inputdata = oldType()
+            j.inputdata = masterType()
             j.inputdata.treat_as_inputfiles = job.inputdata.treat_as_inputfiles
             for sf in full_list[fid:fid + self.files_per_subjob]:
                 j.inputdata.files.append(sf)
