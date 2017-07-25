e1761ebc5275fb7b2e3c101c30ade4dcfae533a2
Mark Smith <mark.smith@cern.ch>
2017-07-25 16:28:13 +0200

    revert import

--- python/GangaLHCb/test/Unit/DiracAPI/TestDiracCommands.py
+++ python/GangaLHCb/test/Unit/DiracAPI/TestDiracCommands.py
@@ -15,7 +15,6 @@ import pytest
 from Ganga.GPIDev.Credentials import credential_store
 from Ganga.Utility.logging import getLogger
 from GangaDirac.Lib.Utilities.DiracUtilities import execute
-from GangaDirac.Lib.Credentials.DiracProxy import DiracProxy
 
 from Ganga.testlib.mark import external
 from Ganga.testlib.GangaUnitTest import load_config_files, clear_config
@@ -57,7 +56,7 @@ def load_config():
 
 @pytest.yield_fixture(scope='class')
 def dirac_job(load_config):
-
+    from GangaDirac.Lib.Credentials.DiracProxy import DiracProxy
     sandbox_str = uuid.uuid4()
     get_file_str = uuid.uuid4()
     remove_file_str = uuid.uuid4()
