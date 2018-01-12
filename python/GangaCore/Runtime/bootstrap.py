7cc7c557f2dd085f7f042a023233ed6eec20e380
Mark Smith <mark.smith@cern.ch>
2018-01-12 11:52:34 +0100

    remove diagnostic

--- python/GangaCore/Runtime/bootstrap.py
+++ python/GangaCore/Runtime/bootstrap.py
@@ -463,8 +463,6 @@ under certain conditions; type license() for details.
         logger = getLogger('ReleaseNotes')
         if getConfig('Configuration')['ReleaseNotes'] == True:
             packages = itertools.imap(lambda x: 'ganga/python/' + x, itertools.ifilter(lambda x: x != '', ['Ganga'] + getConfig('Configuration')['RUNTIME_PATH'].split(':')))
-            for p in packages:
-                print p
             pathname = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'release', 'ReleaseNotes-%s' % _gangaVersion)
 
             if not os.path.exists(pathname):
