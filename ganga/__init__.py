b9c6ca21193d54ae4f5769c4b987a1d210ef1f00
Mark Smith <mark.smith@cern.ch>
2018-02-01 19:20:29 +0100

    hack for import ganga

--- ganga/__init__.py
+++ ganga/__init__.py
@@ -1,5 +1,8 @@
 import os
 import sys
-
+#This is a little cheat so that ganga can be imported with import ganga in a python session, rather than import ganga.ganga
 def pathSetup():
     sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+pathSetup()
+import ganga
