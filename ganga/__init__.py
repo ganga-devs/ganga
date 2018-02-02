b9265e7f5a3fc0f8a5ebc2f1570f0ecad12ecdf5
Mark Smith <mark.smith@cern.ch>
2018-02-02 18:28:35 +0100

    test

--- ganga/__init__.py
+++ ganga/__init__.py
@@ -6,8 +6,8 @@ print pth
 sys.path.insert(0, pth)
 print __name__
 
-del sys.modules[__name__] 
-import ganga
+#del sys.modules[__name__] 
+#import ganga
 
 
 #This is a little cheat so that ganga can be imported with import ganga in a python session, rather than import ganga.ganga
