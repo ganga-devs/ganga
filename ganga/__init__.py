66bfca7cd5b004e10d79ab4ae302ff75ec5e31d0
Mark Smith <mark.smith@cern.ch>
2018-02-05 23:01:29 +0100

    tidy

--- ganga/__init__.py
+++ ganga/__init__.py
@@ -3,14 +3,3 @@ import sys
 
 pth = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, pth)
-
-#del sys.modules[__name__] 
-#import ganga
-
-
-#This is a little cheat so that ganga can be imported with import ganga in a python session, rather than import ganga.ganga
-#def pathSetup():
-#    sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-
-#pathSetup()
-#import ganga
