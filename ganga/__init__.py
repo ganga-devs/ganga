4ab8ef2d6bf035e4e2b86d9b6c46338fd664ed22
Mark Smith <mark.smith@cern.ch>
2018-02-05 17:14:28 +0100

    resort the import

--- ganga/__init__.py
+++ ganga/__init__.py
@@ -2,12 +2,10 @@ import os
 import sys
 
 pth = os.path.join(os.path.dirname(os.path.realpath(__file__)))
-print pth
 sys.path.insert(0, pth)
-print __name__
 
-#del sys.modules[__name__] 
-#import ganga
+del sys.modules[__name__] 
+import ganga
 
 
 #This is a little cheat so that ganga can be imported with import ganga in a python session, rather than import ganga.ganga
