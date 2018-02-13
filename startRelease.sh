6fe4278d9e3afbdd1727bd2042ea4ac5f1d2f2fb
Mark Smith <mark.smith@cern.ch>
2018-02-13 16:04:03 +0100

    reorder

--- startRelease.sh
+++ startRelease.sh
@@ -63,13 +63,12 @@ git add ganga/GangaCore/__init__.py ./setup.py
 
 #Committing changes
 git commit -m "Setting release number"
-
+git push -u origin
 echo "Creating tag $VERSION"
 #Now create a tag and fire it at github
 git tag -a $VERSION -m "Release ${VERSION}"
 
 echo "Pushing to origin"
-git push -u origin
 git push --tags
 
 #Now send the release notes to github - need some python magic
