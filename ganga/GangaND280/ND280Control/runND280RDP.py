071866cb7a66a4b70374a726e07ba0ddd4a0f81d
Nick Hastings <hastings@triumf.ca>
2018-02-24 08:15:39 +0900

    Revert "Add prepare method."
    This reverts commit 190e3613053274625d422c161db718155ab1244b.

--- ganga/GangaND280/ND280Control/runND280RDP.py
+++ ganga/GangaND280/ND280Control/runND280RDP.py
@@ -48,7 +48,7 @@ class runND280RDP(IApplication):
         } )
     _category = 'applications'
     _name = 'runND280RDP'
-    _exportmethods = ['prepare']
+    _exportmethods = []
     _GUIPrefs = [ { 'attribute' : 'args', 'widget' : 'String_List' },
                   { 'attribute' : 'cmtsetup', 'widget' : 'String' },
                   { 'attribute' : 'confopts', 'widget' : 'String' },
@@ -62,9 +62,6 @@ class runND280RDP(IApplication):
 
     def __init__(self):
         super(runND280RDP,self).__init__()
-
-    def prepare(self, force=False):
-        pass
 
     def configure(self,masterappconfig):
 
