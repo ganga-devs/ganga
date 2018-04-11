ee5e34fc8265bb1881990d7c8b485d5b46f223f8
Mark Smith <mark.smith@cern.ch>
2018-04-11 13:13:11 +0200

    move import to top

--- ganga/GangaCore/GPIDev/Lib/GangaList/GangaList.py
+++ ganga/GangaCore/GPIDev/Lib/GangaList/GangaList.py
@@ -5,7 +5,7 @@ from GangaCore.GPIDev.Base.Proxy import isProxy, addProxy, isType, getProxyAttr,
 from GangaCore.GPIDev.Base.VPrinter import full_print, summary_print
 from GangaCore.GPIDev.Schema.Schema import ComponentItem, Schema, SimpleItem, Version
 from GangaCore.GPIDev.Base.Objects import synchronised
-from GangaCore.Utility.util import containsGangaObjects
+from GangaCore.Utility.util import containsGangaObjects, unique
 import copy
 import sys
 from functools import partial
@@ -241,7 +241,6 @@ class GangaList(GangaObject):
 
     def getCategory(self):
         """Returns a list of categories for the objects in the list. Returns [] for an empty list."""
-        from GangaCore.Utility.util import unique
         def return_cat(elem):
             if hasattr(elem, '_category'):
                 return elem._category
