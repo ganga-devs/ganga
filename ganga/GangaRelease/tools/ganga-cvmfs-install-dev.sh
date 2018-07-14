5d448a3f4ccee9833d3d2fbd0da3321f356fe965
Mark Smith <mark.smith@cern.ch>
2018-07-14 11:40:39 +0000

    Set LD_LIBRARY_PATH in DEV install script

--- ganga/GangaRelease/tools/ganga-cvmfs-install-dev.sh
+++ ganga/GangaRelease/tools/ganga-cvmfs-install-dev.sh
@@ -4,6 +4,8 @@ cvmfs_server transaction ganga.cern.ch
 
 cd /cvmfs/ganga.cern.ch/Ganga/install/DEV
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/sft.cern.ch/lcg/releases/LCG_88/Python/2.7.13/x86_64-slc6-gcc62-opt/lib
+
 . bin/activate
 
 pip install --upgrade git+https://github.com/ganga-devs/ganga.git@develop 
