33f4a485e99282429650662154d017a3c784e0ad
Mark Smith <mark.smith@cern.ch>
2019-03-19 10:24:12 +0000

    Update fill_gpi.py

--- doc/fill_gpi.py
+++ doc/fill_gpi.py
@@ -208,7 +208,7 @@ from GangaCore.Core.GangaThread import GangaThreadPool
 GangaThreadPool.shutdown_policy = 'batch'
 
 # This should now be safe
-ShutdownManager._ganga_run_exitfuncs()
+ShutdownManager._proteceted_ganga_exitfuncs()
 
 shutil.rmtree(gangadir, ignore_errors=True)
 ## FINISHED EXITING GANGA ##
