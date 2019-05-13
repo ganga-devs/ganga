cb40ec1f793efdaea8cbe1fad49d35b927e178e4
Mark Smith <mark.smith@cern.ch>
2019-05-13 12:47:14 +0200

    add check for LbDevTools in LHCb plugin

--- ganga/GangaLHCb/__init__.py
+++ ganga/GangaLHCb/__init__.py
@@ -15,12 +15,20 @@ from GangaCore.Utility.logging import getLogger
 
 from GangaCore.Runtime.GPIexport import exportToGPI
 
+from GangaCore.Core.exceptions import PluginError
+
 from GangaCore.GPIDev.Credentials.CredentialStore import credential_store
 from GangaDirac.Lib.Credentials.DiracProxy import DiracProxy
 from GangaLHCb.Utility.LHCbDIRACenv import store_dirac_environment
 
 logger = getLogger()
 
+#First check that LbDevTools has been installed and raise an exeception if not
+try:
+    import LbDevTools
+except ImportError:
+    raise PluginError("Cannot import package LbDevTools. This is required for the LHCb plugin. Install with pip: 'pip install LbDevTools'.")
+    
 if not _after_bootstrap:
     configLHCb = GangaCore.Utility.Config.makeConfig('LHCb', 'Parameters for LHCb')
 
