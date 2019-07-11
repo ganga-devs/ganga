3efd7557b3d90f810f8bce4043f85f4665196415
Inzamam <inzamam.15@cse.mrt.ac.lk>
2019-07-11 19:08:27 +0530

    fixed bugs in Virtualization Utility
    Signed-off-by: Inzamam <inzamam.15@cse.mrt.ac.lk>

--- ganga/GangaCore/Utility/Virtualization.py
+++ ganga/GangaCore/Utility/Virtualization.py
@@ -1,40 +1,49 @@
 import subprocess
 import os
 
+
 def checkSingularity():
+    nullOutput = open(os.devnull, 'wb')
     returnCode = 1
     try:
-        returnCode = subprocess.call(["singularity", "--version"])
+        returnCode = subprocess.call(["singularity", "--version"], stdout=nullOutput, stderr=nullOutput)
     except:
         pass 
     if returnCode == 0 : return True
     return False
-    
+
+
 def checkDocker():
+    nullOutput = open(os.devnull, 'wb')
     returnCode = 1
     try:
-        returnCode = subprocess.call(["docker", "ps"])
-        subprocess.call(["export", "PROOT_NO_SECCOMP=1"])
+        returnCode = subprocess.call(["docker", "ps"], stdout=nullOutput, stderr=nullOutput)
     except:
         pass 
     if returnCode == 0 : return True
     return False
-    
+
+
 def checkUDocker():
+    nullOutput = open(os.devnull, 'wb')
     if (os.path.isfile(os.path.expanduser("~") + "/udocker")):
-        returnCode = subprocess.call([os.path.expanduser("~") + "/udocker", "ps"])
+        returnCode = subprocess.call([os.path.expanduser("~") + "/udocker", "ps"], stdout=nullOutput, stderr=nullOutput)
         if (returnCode == 0):
             return True
     return False
 
+
 def installUdocker():
     udocker_address = "https://raw.githubusercontent.com/indigo-dc/udocker/master/udocker.py"
     returnCode = subprocess.check_call(['curl', udocker_address], stdout=open(os.path.expanduser("~")+"/udocker", 'w'))
-    if (returnCode < 0):
+    if (returnCode != 0):
         print ("Error downloading UDocker")
-        return
+        return False
     subprocess.call(["chmod", "u+rx", os.path.expanduser("~")+"/udocker"])
     returnCode = subprocess.call([os.path.expanduser("~")+"/udocker", "install"])
-    if (returnCode < 0):
+    if (returnCode != 0):
         print ("Error installing uDocker")
+        return False
+    subprocess.call(["export", "PROOT_NO_SECCOMP=1"], stdout=open(os.devnull, 'wb'))
     print ("UDocker Successfully installed")
+    return True
