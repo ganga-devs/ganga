dea04452c8b97d88f6055919787c5b756108be86
ishanrai05 <ishanrai05@gmail.com>
2019-07-21 20:39:40 +0530

    [CoW] Adds Test for set

--- ganga/GangaCore/test/Unit/Utility/TestCoW.py
+++ ganga/GangaCore/test/Unit/Utility/TestCoW.py
@@ -125,3 +125,197 @@ class TestFloat(unittest.TestCase):
         self.assertEqual(t.i, 5.2)
         self.assertEqual(t2.i, 12.5)
         t._flyweight_cache.clear()
+
+
+class TestSet(unittest.TestCase):
+
+    def test_set_complicated_nested(self):
+        t = SampleClass()
+
+        l = [1, 2, 3, {4: set([5])}]
+        t.l = l
+        t2 = copy(t)
+        t.l[-1][4].add(6)
+
+        self.assertEqual(t2.l, [1, 2, 3, {4: set([5])}])
+        self.assertEqual(t.l, [1, 2, 3, {4: set([5, 6])}])
+        t._flyweight_cache.clear()
+
+    def test_set_add_hash(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        old_hash = hash(t.l)
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t.l.add(5)
+        new_hash = hash(t.l)
+
+        self.assertNotEqual(old_hash, new_hash)
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        self.assertIn(new_hash, t._flyweight_cache[ProxySet])
+        t._flyweight_cache.clear()
+
+    def test_set_symmetric_update(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.update(set([5, 6, 7]))
+        self.assertEqual(t.l, set([1, 2, 3, 4, 5, 6, 7]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_symmetric_difference_update(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.symmetric_difference_update(set([1, 3, 4]))
+        self.assertEqual(t.l, set([2]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_remove(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.remove(3)
+        self.assertEqual(t.l, set([1, 2, 4]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_pop(self):
+        t = SampleClass()
+
+        t.l = set([1])
+        self.assertEqual(t.l, set([1]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        self.assertEqual(t.l.pop(), 1)
+        self.assertEqual(t.l, set())
+        self.assertEqual(t2.l, set([1]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_intersection_update(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.intersection_update(set([1, 4, 5]))
+        self.assertEqual(t.l, set([1, 4]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_discard(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.discard(2)
+
+        self.assertEqual(t.l, set([1, 3, 4]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+
+        t._flyweight_cache.clear()
+
+    def test_set_difference_update(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.difference_update(set([1, 2]))
+        self.assertEqual(t.l, set([3, 4]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_clear(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.clear()
+        self.assertEqual(t.l, set())
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_add(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l.add(5)
+        self.assertEqual(t.l, set([1, 2, 3, 4, 5]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
+
+    def test_set_basic(self):
+        t = SampleClass()
+
+        t.l = set([1, 2, 3, 4])
+        self.assertEqual(t.l, set([1, 2, 3, 4]))
+
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 1)
+
+        t2 = copy(t)
+        self.assertEqual(t2.l, t.l)
+
+        t.l = set([5])
+        self.assertEqual(t.l, set([5]))
+        self.assertEqual(t2.l, set([1, 2, 3, 4]))
+        self.assertEqual(len(t._flyweight_cache[ProxySet]), 2)
+        t._flyweight_cache.clear()
