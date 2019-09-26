5a3143bec2351f70d8e482a8d3f488a4b116389d
Ulrik Egede <ulrik.egede@monash.edu>
2019-09-26 16:09:08 +1000

    Further changes to pytest config

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ def pytest_addoption(parser):
     parser.addoption("--runexternals", action="store_true",
         help="run tests that depend on external services")
     parser.addoption("--keepRepo", action="store_true",
-        help="should tests preseve the repo used after completing")
+        help="should tests preserve the repo used after completing")
     parser.addoption("--testLHCb", action="store_true",
         help="should we try to use an LHCb proxy for testing")
 
