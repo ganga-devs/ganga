2fb5c43b00604dc368149a85e0b64e4cda85efda
Mark Smith <mark.smith@cern.ch>
2019-10-21 18:25:29 +0200

    add back runner

--- ganga/GangaTest/__init__.py
+++ ganga/GangaTest/__init__.py
@@ -5,12 +5,14 @@ def standardSetup():
 
 
 def loadPlugins( config = None ):
+    print('loading plugins')
+    import GangaTest.Framework.runner
     import GangaTest.Lib.GListApp
     import GangaTest.Lib.TFile
     import GangaTest.Lib.TestApplication
     import GangaTest.Lib.TestSubmitter # TestSubmitter, TestSplitter here
     import GangaTest.Lib.TestRobot
     import GangaTest.Lib.TestObjects
-    import GangaTest.Framework.runner
+#    import GangaTest.Framework.runner
 
 
