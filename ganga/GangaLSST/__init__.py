e3d1c00b17c53a8c7f6f869cc62ac449b4190923
Mark Smith <mark.smith@cern.ch>
2019-10-30 10:27:52 +0000

    Update __init__.py

--- ganga/GangaLSST/__init__.py
+++ ganga/GangaLSST/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from GangaCore.Core.exceptions import PluginError
-raise PluginError("The GangaLHCb module has not been upgraded for python 3. The last python 2 Ganga version is 7.1.15 . Please contact the ganga devs to discuss updating this module.")
+raise PluginError("The GangaLSST module has not been upgraded for python 3. The last python 2 Ganga version is 7.1.15 . Please contact the ganga devs to discuss updating this module.")
 
 from . import Lib
 
