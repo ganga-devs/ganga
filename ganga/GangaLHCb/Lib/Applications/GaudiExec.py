f07dcc140ffbc2fe93b62fe992d9fa332627e074
Mark Smith <mark.smith@cern.ch>
2019-11-21 16:56:24 +0100

    alternative strategy

--- ganga/GangaLHCb/Lib/Applications/GaudiExec.py
+++ ganga/GangaLHCb/Lib/Applications/GaudiExec.py
@@ -410,14 +410,14 @@ class GaudiExec(IPrepareApp):
         """
         Return the script which wraps the running command in a correct environment
         """
-        return 'source /cvmfs/lhcb.cern.ch/lib/LbEnv || lb-set-platform %s && ' % (self.platform)
+        return '. /cvmfs/lhcb.cern.ch/lib/LbEnv -c %s && ' % (self.platform)
 
 
     def getWNEnvScript(self):
         """
         Return the script to setup the correct env on a WN
         """
-        return 'source /cvmfs/lhcb.cern.ch/lib/LbEnv || lb-set-platform %s && ' % (self.platform)
+        return '. /cvmfs/lhcb.cern.ch/lib/LbEnv -c %s && ' % (self.platform)
 
     def execCmd(self, cmd):
         """
@@ -456,7 +456,7 @@ class GaudiExec(IPrepareApp):
         # but this requires a build to have been run before we can use this command reliably... so we're just going to be explicit
 
         if not path.isfile(path.join(self.directory, 'build.%s' %self.platform, 'run')):
-            initialCommand = 'source /cvmfs/lhcb.cern.ch/lib/LbEnv || lb-set-platform %s && make' % (self.platform)
+            initialCommand = '. /cvmfs/lhcb.cern.ch/lib/LbEnv -c %s && make' % (self.platform)
             rc, stdout, stderr = _exec_cmd(initialCommand, self.directory)
             if rc != 0:
                 logger.error("Failed to perform initial make on a Cmake based project")
