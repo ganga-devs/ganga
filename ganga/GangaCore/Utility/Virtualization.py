061d92ea63bc406ab5c82f772167040b26ca866c
Ulrik Egede <ulrik.egede@monash.edu>
2019-12-05 21:46:10 +1100

    Update of Virtualization (#1539)
    Updated virtualization to work with multiple backends

--- ganga/GangaCore/Utility/Virtualization.py
+++ ganga/GangaCore/Utility/Virtualization.py
@@ -60,14 +60,25 @@ def installUdocker(location='~'):
     fname = os.path.join(os.path.expanduser(location),"udocker")
     url = "https://raw.githubusercontent.com/indigo-dc/udocker/master/udocker.py"
 
-    with urlopen(url) as response, open(fname, 'wb') as out_file:
-        data = response.read() # a `bytes` object
-        out_file.write(data)
+    import ssl
+    context = ssl._create_unverified_context()
+    try:
+        with urlopen(url, context=context) as response, open(fname, 'wb') as out_file:
+            data = response.read()
+            out_file.write(data)
+    except:
+        returnCode = subprocess.check_call(['curl', '-k', url], stdout=open(fname, 'wb'))
+        if (returnCode != 0):
+            raise OSError('Error downloading uDocker')
 
     subprocess.call(["chmod", "u+rx", fname])
+    udockerdir = os.path.join(location,'.udocker')
+    os.environ['UDOCKER_DIR']=udockerdir
+    os.makedirs(udockerdir)
+    with open(os.path.join(udockerdir,'udocker.conf'), 'w') as fconfig:
+        fconfig.write('http_insecure = True')
     returnCode = subprocess.call([fname, "install"])
     if (returnCode != 0):
         raise OSError('Error installing uDocker')
-    os.environ['UDOCKER_DIR']=os.path.join(location,'.udocker')
     print('UDocker Successfully installed')
 
