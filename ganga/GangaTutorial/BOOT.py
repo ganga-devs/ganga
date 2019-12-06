79ba2d4ef0de1f839cdbfdcbfa209e40ef596601
ishanrai05 <ishanrai05@gmail.com>
2019-12-06 19:31:04 +0530

    PEP8 GangaTutorial

--- ganga/GangaTutorial/BOOT.py
+++ ganga/GangaTutorial/BOOT.py
@@ -1,25 +1,27 @@
+import os
+
+import GangaTutorial
+from GangaTutorial.Lib.primes.primes import check_prime_job, split_prime_job
+from GangaCore.Runtime.GPIexport import exportToGPI
+from GangaCore.Utility.logging import getLogger
+logger = getLogger(modulename=True)
+
+
 def getEnvironment(c):
     import PACKAGE
     PACKAGE.standardSetup()
     return {}
-    
+
 
 def loadPlugins(c):
     pass
 
-from GangaTutorial.Lib.primes.primes import check_prime_job, split_prime_job
 
-from GangaCore.Runtime.GPIexport import exportToGPI
+exportToGPI("check_prime_job", check_prime_job, "Functions")
+exportToGPI("split_prime_job", split_prime_job, "Functions")
 
-from GangaCore.Utility.logging import getLogger
-logger = getLogger(modulename=True)
-
-exportToGPI("check_prime_job",check_prime_job,"Functions")
-exportToGPI("split_prime_job",split_prime_job,"Functions")
-
-import os, GangaTutorial
 TUTDIR = os.path.dirname(GangaTutorial.__file__)
-exportToGPI("TUTDIR",TUTDIR,"Objects")
-del os,GangaTutorial
+exportToGPI("TUTDIR", TUTDIR, "Objects")
+del os, GangaTutorial
 
 logger.debug("*** Ganga Tutorial Loaded OK ***")
