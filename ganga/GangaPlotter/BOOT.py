9edf838d8676028d9d72178aa4a33efbd92c3967
ishanrai05 <ishanrai05@gmail.com>
2019-12-09 12:35:06 +0530

    PEP8 GangaPlotter

--- ganga/GangaPlotter/BOOT.py
+++ ganga/GangaPlotter/BOOT.py
@@ -2,13 +2,15 @@ def getEnvironment(c):
     import PACKAGE
     PACKAGE.standardSetup()
     return {}
-    
+
+
 def loadPlugins(c):
 
-    import os,sys
+    import os
+    import sys
     from GangaCore.Utility.logging import getLogger
     logger = getLogger()
-    logger.info('You are now using Python %s',sys.version.split()[0])
+    logger.info('You are now using Python %s', sys.version.split()[0])
 
     import GangaPlotter.Plotter
     from Plotter.GangaPlotter import GangaPlotter
@@ -18,4 +20,4 @@ def loadPlugins(c):
     from GangaCore.Runtime.GPIexport import exportToGPI
 
     #exportToGPI('plotter',GangaPlotter.Plotter.plotter,'Objects','graphics plotter')
-    exportToGPI('plotter',plotter,'Objects','graphics plotter')
+    exportToGPI('plotter', plotter, 'Objects', 'graphics plotter')
