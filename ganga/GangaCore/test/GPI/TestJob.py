006d23ec5dfb29a7b89f1a5a0f094851c3452bc8
Varun Bankar <mail.varunbankar@gmail.com>
2020-02-29 19:33:07 +0530

    migrated CopyJob1 test to TestJob.py
    migrated ganga/GangaCore/old_test/GPI/CopyJob.gpi to ganga/GangaCore/test/GPI/TestJob.py

--- ganga/GangaCore/test/GPI/TestJob.py
+++ ganga/GangaCore/test/GPI/TestJob.py
@@ -107,6 +107,15 @@ def test_job_copy(gpi):
     assert j2.splitter.attribute == "application.args"
     assert j2.splitter.values == ['arg 1', 'arg 2', 'arg 3']
 
+    # make sure properties are not shared between the copies
+    j2.name == "testname2"
+    j2.application.exe == "echo"
+
+    # test the properties are not shared between copies
+    assert j.name == "testname"
+    assert j2.name == "testname2"
+    assert j.application.exe == "sleep"
+    assert j2.application.exe == "echo"
 
 def test_job_equality(gpi):
     """Check that copies of Jobs are equal to each other"""
