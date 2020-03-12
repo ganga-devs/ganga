9414bc39764b43ad547c0189cb7de4ec3d35fa4a
Varun Bankar <mail.varunbankar@gmail.com>
2020-03-13 02:03:19 +0530

    migrate "SplitterException.gpi" "SubjobAppconfigFail.gpi" old test to "TestSplitter.py" new test

---
+++ ganga/GangaCore/test/TestSplitter.py
@@ -0,0 +1,26 @@
+def test_splitter_exception(gpi):
+
+    from GangaTest.Framework.utils import assert_cannot_submit
+
+    j = gpi.Job()
+    j.splitter = gpi.TestSplitter()
+    j.splitter.fail = "exception"
+
+    assert_cannot_submit(j)
+
+
+def test_subjob_app_config_fail(gpi):
+
+    # This is a test in which the second subjob fails to submit because of the application config exception
+    from GangaTest.Framework.utils import assert_cannot_submit
+
+    j = gpi.Job()
+    j.backend = gpi.TestSubmitter()
+    j.application = gpi.TestApplication()
+    j.splitter = gpi.GenericSplitter()
+    j.splitter.attribute = 'application.fail'
+    j.splitter.values = ['','config','']
+
+    assert_cannot_submit(j)
+
+    assert(len(j.subjobs)==0)
