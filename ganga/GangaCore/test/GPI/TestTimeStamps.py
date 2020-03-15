053a07765f93fdf6ff74131fc04acb31052c161b
Varun Bankar <mail.varunbankar@gmail.com>
2020-03-16 02:16:08 +0530

    migrate "test3_copy_local.gpi" old test to new test
    migrate "GangaCore/old_test/GPI/TimeStamps/test3_copy_local.gpi" old test to "TestTimeStamps.py" new test

--- ganga/GangaCore/test/GPI/TestTimeStamps.py
+++ ganga/GangaCore/test/GPI/TestTimeStamps.py
@@ -94,4 +94,59 @@ def test_duration_local(gpi):
         assert isinstance(sjs.time.waittime(), datetime.timedelta)
         assert isinstance(sjs.time.runtime(), datetime.timedelta)
 
+# Test for checking timestamps for Job Copy in Local Backend
+def test_basic_timestamps_copy_local(gpi):
+    
+    # Without Subjobs -------
+
+    j1 = gpi.Job()
+
+    assert 'new' in j1.time.timestamps.keys()
+
+    assert isinstance(j1.time.new(), datetime.datetime)
+
+    j2 = j1.copy()
+
+    assert 'new' in j2.time.timestamps.keys()
+
+    assert isinstance(j2.time.new(), datetime.datetime)
+
+    assert j2.time.new() - j1.time.new() > datetime.timedelta(0,0,0), "j2 'new' is not more recent than j1 'new'"
+
+    # With Subjobs -------
+
+    j3 = gpi.Job()
+
+    j3.splitter='ArgSplitter'
+    j3.splitter.args=[[],[],[]]
+
+    assert 'new' in j3.time.timestamps.keys()
+    assert isinstance(j3.time.new(), datetime.datetime)
+
+    j3.submit()
+
+    assert sleep_until_completed(j3, 300)
+
+    for sjs in j3.subjobs:
+        assert 'new' in sjs.time.timestamps.keys()
+
+    j4 = j3.copy()
+
+    assert 'new' in j4.time.timestamps.keys()
+    assert isinstance(j4.time.new(), datetime.datetime)
+
+    assert j4.time.new() - j3.time.new() > datetime.timedelta(0,0,0), "j4 'new' is not more recent than j3 'new'"
+
+    for sjs in j4.subjobs:
+        assert 'new' not in sjs.time.timestamps.keys()
+
+    j4.submit()
+
+    assert sleep_until_completed(j4, 300)
+
+    for i in range(0,len(j4.subjobs)):
+        for j in range(0,len(j3.subjobs)):
+            assert j4.subjobs(i).time.new() > j3.subjobs(j).time.new()
+
+
 
