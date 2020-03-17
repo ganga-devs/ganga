a836d42aaea7adf995d431dffabb4f0cfbaed83f
Varun Bankar <mail.varunbankar@gmail.com>
2020-03-17 15:18:10 +0530

    Update TestGangaTutorial.py

--- ganga/GangaCore/test/GPI/TestGangaTutorial.py
+++ ganga/GangaCore/test/GPI/TestGangaTutorial.py
@@ -1,18 +1,23 @@
 from GangaCore.testlib.decorators import add_config
-from GangaCore.Utility.Config import getConfig, expandvars
+import os
+import pytest
 
-c = getConfig('Configuration')
-c.setSessionValue('RUNTIME_PATH', "GangaTutorial")
-c.setSessionValue('RUNTIME_PATH', "GangaTest")
+@pytest.fixture
+def mock_env_ganga_config_path(monkeypatch):
+    monkeypatch.setenv("GANGA_CONFIG_PATH", "GangaTutorial/Tutorial.ini")
 
-def test_ganga_tutorial(gpi):
+# @pytest.fixture
+# def mock_env_missing(monkeypatch):
+#     monkeypatch.delenv("GANGA_CONFIG_PATH", raising=False)
+
+def test_ganga_tutorial(mock_env_ganga_config_path, gpi):
     j = gpi.Job()
     j.application = gpi.PrimeFactorizer(number=123456)
     j.inputdata = gpi.PrimeTableDataset(table_id_lower=1, table_id_upper=1)
     j.submit()
     assert j.status in ['submitted','running','completed']
 
-def test_ganga_tutorial_subjobs(gpi):
+def test_ganga_tutorial_subjobs(mock_env_ganga_config_path, gpi):
     j = gpi.Job()
     j.splitter = gpi.PrimeFactorizerSplitter(numsubjobs=2)
     j.application = gpi.PrimeFactorizer(number=123456)
