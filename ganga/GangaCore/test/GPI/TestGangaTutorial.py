546ffcda2e2c4672e196d8cf99278a848645dbc3
Varun Bankar <mail.varunbankar@gmail.com>
2020-03-17 18:24:35 +0530

    Update TestGangaTutorial.py

--- ganga/GangaCore/test/GPI/TestGangaTutorial.py
+++ ganga/GangaCore/test/GPI/TestGangaTutorial.py
@@ -1,5 +1,12 @@
 # A trivial test to see if Jenkins gives positive result
 from GangaCore.testlib.GangaUnitTest import GangaUnitTest
+from GangaTutorial.Lib.PrimeFactorizer import PrimeFactorizer
+from GangaTutorial.Lib.PrimeTableDataset import PrimeTableDataset
+from GangaCore.Runtime.GPIexport import exportToGPI
+import os, GangaTutorial
+
+TUTDIR = os.path.dirname(GangaTutorial.__file__)
+exportToGPI("TUTDIR",TUTDIR,"Objects")
 
 class TestGangaTutorial(GangaUnitTest):
 
@@ -8,11 +15,11 @@ class TestGangaTutorial(GangaUnitTest):
         from GangaCore.GPI import Job
 
         j = Job()
+        j.application = PrimeFactorizer()
+        j.application.number = 123456
+        j.inputdata = PrimeTableDataset()
+        j.inputdata.table_id_lower = 1
+        j.inputdata.table_id_upper = 1
         j.submit()
-
-        # j = Job()
-        # j.application = PrimeFactorizer(number=123456)
-        # j.inputdata = PrimeTableDataset(table_id_lower=1, table_id_upper=1)
-        # j.submit()
 
         self.assertIn(j.status, ['submitted','running','completed'])
