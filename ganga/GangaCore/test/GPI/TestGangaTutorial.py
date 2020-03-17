7beda6f932e81a9690a7a1506e88ed76d0f0f2e7
Varun Bankar <mail.varunbankar@gmail.com>
2020-03-17 16:27:05 +0530

    Update TestGangaTutorial.py

--- ganga/GangaCore/test/GPI/TestGangaTutorial.py
+++ ganga/GangaCore/test/GPI/TestGangaTutorial.py
@@ -1,29 +1,22 @@
-from GangaCore.testlib.decorators import add_config
+from GangaCore.testlib.GangaUnitTest import GangaUnitTest
 import os
-import pytest
 
-@pytest.fixture
-def mock_env_ganga_config_path(monkeypatch):
-    monkeypatch.setenv("GANGA_CONFIG_PATH", "GangaTutorial/Tutorial.ini")
+class TestGangaTutorial(GangaUnitTest):
 
-# @pytest.fixture
-# def mock_env_missing(monkeypatch):
-#     monkeypatch.delenv("GANGA_CONFIG_PATH", raising=False)
+    def setUp(self, extra_opts=[]):
+        os.environ["GANGA_CONFIG_PATH"] = "GangaTutorial/Tutorial.ini"
+        super().setUp(extra_opts=[])
 
-def test_ganga_tutorial(mock_env_ganga_config_path, gpi):
-    j = gpi.Job()
-    j.application = gpi.PrimeFactorizer(number=123456)
-    j.inputdata = gpi.PrimeTableDataset(table_id_lower=1, table_id_upper=1)
-    j.submit()
-    assert j.status in ['submitted','running','completed']
+    def test_ganga_tutorial(self):
+        
+        from GangaCore.GPI import Job, PrimeFactorizer, PrimeTableDataset
 
-def test_ganga_tutorial_subjobs(mock_env_ganga_config_path, gpi):
-    j = gpi.Job()
-    j.splitter = gpi.PrimeFactorizerSplitter(numsubjobs=2)
-    j.application = gpi.PrimeFactorizer(number=123456)
-    j.inputdata = gpi.PrimeTableDataset(table_id_lower=1, table_id_upper=2)
-    j.submit()
-    assert len(j.subjobs) == 2
-    assert j.status in ['submitted', 'running', 'completed']
-    for sj in j.subjobs:
-        assert sj.status in ['submitted', 'running', 'completed']
+        j = Job()
+        j.application = PrimeFactorizer(number=123456)
+        j.inputdata = PrimeTableDataset(table_id_lower=1, table_id_upper=1)
+        j.submit()
+        self.assertIn(j.status, ['submitted','running','completed'])
+
+    def tearDown(self):
+        super().tearDown()
+        del os.environ["GANGA_CONFIG_PATH"]
