910170d2217f5a4c361f5b6a7b4821e48c751d5a
Varun Bankar <mail.varunbankar@gmail.com>
2020-03-17 17:49:16 +0530

    A trivial test

--- ganga/GangaCore/test/GPI/TestGangaTutorial.py
+++ ganga/GangaCore/test/GPI/TestGangaTutorial.py
@@ -1,22 +1,18 @@
+# A trivial test to see if Jenkins gives positive result
 from GangaCore.testlib.GangaUnitTest import GangaUnitTest
-import os
 
 class TestGangaTutorial(GangaUnitTest):
 
-    def setUp(self, extra_opts=[]):
-        os.environ["GANGA_CONFIG_PATH"] = "GangaTutorial/Tutorial.ini"
-        super().setUp(extra_opts=[])
-
     def test_ganga_tutorial(self):
         
-        from GangaCore.GPI import Job, PrimeFactorizer, PrimeTableDataset
+        from GangaCore.GPI import Job
 
         j = Job()
-        j.application = PrimeFactorizer(number=123456)
-        j.inputdata = PrimeTableDataset(table_id_lower=1, table_id_upper=1)
         j.submit()
-        self.assertIn(j.status, ['submitted','running','completed'])
 
-    def tearDown(self):
-        super().tearDown()
-        del os.environ["GANGA_CONFIG_PATH"]
+        # j = Job()
+        # j.application = PrimeFactorizer(number=123456)
+        # j.inputdata = PrimeTableDataset(table_id_lower=1, table_id_upper=1)
+        # j.submit()
+
+        self.assertIn(j.status, ['submitted','running','completed'])
