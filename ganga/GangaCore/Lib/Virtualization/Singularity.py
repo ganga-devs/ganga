78e19d6ea6cb83ac1c4aa1222963b6cb867a6feb
Mark Smith <mark.smith@cern.ch>
2020-05-11 01:04:32 +0100

    Virtualization docstrings (#1665)

--- ganga/GangaCore/Lib/Virtualization/Singularity.py
+++ ganga/GangaCore/Lib/Virtualization/Singularity.py
@@ -55,10 +55,10 @@ class Singularity(IVirtualization):
       j.virtualization.tokenpassword = 'gftrh84dgel-245^ghHH'
 
     Directories can be mounted from the host to the container using key-value 
-    pairs to the mount option. If the directory is not vailable on the host, a 
+    pairs to the mounts option. If the directory is not vailable on the host, a 
     warning will be written to stderr of the job and no mount will be attempted.
 
-      j.virtualization.mount = {'/cvmfs':'/cvmfs'}
+      j.virtualization.mounts = {'/cvmfs':'/cvmfs'}
 
     By default the container is started in singularity with the `--nohome` 
     option. Extra options can be provided through the `options` attribute. See 
