43008c5cf397c041e049c8c1e73e2465b5f412e3
DumbMachine <ratin.kumar.2k@gmail.com>
2020-06-01 03:07:42 +0530

    added tests for JStreamer.py

--- ganga/GangaCore/test/GPI/JsonTest/TestJStreamer.py
+++ ganga/GangaCore/test/GPI/JsonTest/TestJStreamer.py
@@ -1,6 +1,5 @@
 from GangaCore.testlib.GangaUnitTest import GangaUnitTest
 
-# Test Report() Functionality
 class TestJobToFile(GangaUnitTest):
 
 
@@ -9,12 +8,12 @@ class TestJobToFile(GangaUnitTest):
         import json
         from GangaCore.GPI import Job
         from GangaCore.GPIDev.Base import GangaObject
-        from GangaCore.Core.GangaRepository.JStreamer import to_file, from_file
-        from GangaCore.GPIDev.Base.Proxy import addProxy, stripProxy, isType, getName
+        from GangaCore.Core.GangaRepository.JStreamer import to_file
+        from GangaCore.GPIDev.Base.Proxy import stripProxy
 
         j = Job()
 
-        # Assert post-submission job information
+        # Assert pre-submission job information
         self.assertIn(j.status, ['new'])  
         self.assertEqual(len(j.subjobs), 0)
         
@@ -30,6 +29,7 @@ class TestJobToFile(GangaUnitTest):
         # Loading the json
         fobj = open(filename, "r")
         json_content = json.load(fobj)
+
         # ForReview: 
         # ["inputdir", "outputdir"] should be adapted to correspond to the gangadir testing repository
         # ["fqid"] was added as that was causing errors and I do not know the use of this attribute
@@ -40,10 +40,36 @@ class TestJobToFile(GangaUnitTest):
                 attr_value = stripped_j._schema.getDefaultValue(attr)
                 # checking for simple attributes
                 if isinstance(attr_value, (bool, list, str)) or attr_value is None:
+                    self.assertEqual(attr_value, json_content[attr])
+                    # assert attr_value == json_content[attr], attr
                     # self.assertEqual(attr_value, json_content[attr])
-                    # assert ((attr_value == json_content[attr], attr)
-                    assert attr_value == json_content[attr], attr
-                    # self.assertEqual(attr_value, json_content[attr])
+                
+                # ForReview: Do we require the checking of attributes of components attributes like ["JobTime", "JobInfo", ...]
                 # checking for component attributes
                 else:
                     print("Skipping the component object")
+
+
+    # This test assumes the file created by the above test still exists
+    # FIXME: Add a cleaner function to remove created files like: "test_job.job"
+    def test_job_from_file(self):
+        import os
+        import json
+        from GangaCore.GPI import Job
+        from GangaCore.GPIDev.Base import GangaObject
+        from GangaCore.Core.GangaRepository.JStreamer import from_file
+        from GangaCore.GPIDev.Base.Proxy import addProxy, stripProxy
+
+
+        # Check if the created file by to_file exists
+        filename = "test_job.json"
+        self.assertTrue(os.path.exists(filename))
+
+        fobj = open(filename, "r")
+        j, error = from_file(f=fobj)
+
+        proxy_j = addProxy(j)
+
+        # Assert post-submission job information
+        self.assertIn(j.status, ['new'])  
+        self.assertEqual(len(j.subjobs), 0)
