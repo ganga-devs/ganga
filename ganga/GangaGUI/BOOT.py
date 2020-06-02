e546192fbec2254d3f223642b67a6565a3c505c0
Varun Bankar <47035057+varunbankar@users.noreply.github.com>
2020-06-03 04:36:06 +0530

    GangaGUI - Token Based Authentication (#1683)
    * Add database support in Flask
    
    * Create database User model with token generation method
    
    * Add config for SQLAlchemy
    
    * Create /token route & @token_required decorator
    
    * Update start.py to generate and return server and authentication details
    
    * Update start.py development server startup to support authentication details generation
    
    * Update setup.py with latest dependencies
    
    * Update GangaGUI/BOOT.py docstring
    
    * Update GangaGUI/test/test_dashboard.py
    
    * Create test for testing GUI database
    
    * Create test for testing GUI token authentication
    
    * Update GangaGUI/test/test_gui_database.py to remove db session in tearDown
    
    * Update GangaGUI/test/test_token_authentication.py to remove db session in tearDown
    
    * Update new lines
    
    * Update logger to display the URL and additional port forwarding info
    
    Co-authored-by: Ulrik Egede <ulrik.egede@monash.edu>

--- ganga/GangaGUI/BOOT.py
+++ ganga/GangaGUI/BOOT.py
@@ -1,3 +1,5 @@
+"""Set RUNTIME_PATH = GangaGUI into order to export the following function the GPI"""
+
 from GangaCore.Runtime.GPIexport import exportToGPI
 from GangaGUI.start import start_gui, stop_gui
 
--- ganga/GangaGUI/gui/__init__.py
+++ ganga/GangaGUI/gui/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from GangaGUI.config import Config
+from flask_sqlalchemy import SQLAlchemy
 import sys
 
 # Disable development server warning (as it is not applicable to our GUI Flask App)
@@ -9,5 +10,9 @@ cli.show_server_banner = lambda *x: None
 # Flask App and get configuration from config.py file
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Database object which is used to interact with the "gui_db.sqlite"
+# NOTE: IT HAS NO RELATION WITH THE GANGA PERSISTENT DATABASE
+db = SQLAlchemy(app)
 
 from GangaGUI.gui import routes
