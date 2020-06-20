179db617951d13669df507480bb99d1bc390a12a
Varun Bankar <mail.varunbankar@gmail.com>
2020-06-20 20:37:20 +0530

    Create Templates API GET, DELETE endpoints

--- ganga/GangaGUI/gui/routes.py
+++ ganga/GangaGUI/gui/routes.py
@@ -1,3 +1,5 @@
+# ******************** Imports ******************** #
+
 import jwt
 from GangaGUI.gui import app
 from flask import request, jsonify
@@ -5,18 +7,28 @@ from functools import wraps
 from GangaGUI.gui.models import User
 
 
+# ******************** View Routes ******************** #
+
+# Dashboard route
 @app.route("/")
 def dashboard():
-    return "Hello GangaGUI"
+    return "GangaGUI"
 
+
+# ******************** Token Based Authentication ******************** #
 
 # Generate token for API authentication - token validity 5 days
 @app.route("/token", methods=["POST"])
 def generate_token():
-    # Store request data
+    """
+    Using the 'user' and 'password' data from the form body, validates the user and returns a JSON Web Token (JWT).
+    """
+
+    # Request form data
     request_user = request.form.get("user")
     request_password = request.form.get("password")
 
+    # Handle no user or no password case
     if not request_user or not request_password:
         response_data = {"success": False, "message": "Could not verify user."}
         return jsonify(response_data), 401
@@ -28,12 +40,19 @@ def generate_token():
         response_data = {"token": token}
         return jsonify(response_data)
 
+    # If authentication fails, return 401 HTTP code
     response_data = {"success": False, "message": "Could not verify user."}
     return jsonify(response_data), 401
 
 
+# ******************** Token Authentication Decorator ******************** #
+
 # Decorator for token protected routes
 def token_required(f):
+    """
+    Decorator which validates the request header token in 'X-Acess-Token' field, and returns the user.
+    """
+
     @wraps(f)
     def decorated(*args, **kwargs):
 
@@ -64,13 +83,91 @@ def token_required(f):
     return decorated
 
 
-# TODO resource route for development purpose - REMOVE LATER
-# Protected resource
-@app.route("/resource", methods=["POST"])
-@token_required
-def resource(current_user):
-    return jsonify({"success": True, "message": "Token is valid"})
+# ******************** Templates API ******************** #
 
+# Templates API - GET Method
+@app.route("/templates", methods=["GET"])
+@token_required
+def templates_endpoint(current_user):
+    """
+    Returns a list of objects where each object is template data in JSON format.
+    """
+
+    # Imports
+    from GangaCore.GPI import templates
+
+    # Store templates information in a list
+    templates_data_list = []
+    for t in templates:
+        templates_data_list.append(get_template_data(t.id))
+
+    return jsonify(templates_data_list)
+
+
+# Template Delete API - DELETE Method
+@app.route("/template/<int:template_id>/delete", methods=["DELETE"])
+@token_required
+def template_delete_endpoint(current_user, template_id: int):
+    """
+
+    Given the templates id, delete it from the template repository.
+
+    :param template_id: int
+    """
+
+    # Imports
+    from GangaCore.GPI import templates
+
+    # Template existence check
+    if template_id not in list(templates.ids()):
+        return jsonify({"success": False, "message": "Job with ID {} does not exist".format(template_id)}), 400
+
+    # Remove template
+    try:
+        t = templates[template_id]
+        t.remove()
+    except Exception as err:
+        return jsonify({"success": False, "message": str(err)})
+
+    return jsonify({"success": True, "message": "Template with ID {} successfully removed".format(template_id)})
+
+
+# ******************** Helper Functions ******************** #
+
+def get_template_data(template_id: int) -> dict:
+    """
+    Given the template_id, return a dict containing general info of the template.
+
+    :param template_id: int
+    :return: dict
+    """
+
+    # Imports
+    from GangaCore.GPI import templates
+
+    # Get template from the templates list
+    try:
+        t = templates[int(template_id)]
+    except Exception as err:
+        return jsonify({"success": False, "message": str(err)})
+
+    # Store template info in a dict
+    template_data = {}
+    template_data["id"] = str(t.id)
+    template_data["fqid"] = str(t.fqid)
+    template_data["status"] = str(t.status)
+    template_data["name"] = str(t.name)
+    template_data["subjobs"] = len(t.subjobs)
+    template_data["application"] = str(type(t.application))
+    template_data["backend"] = str(type(t.backend))
+    template_data["backend.actualCE"] = str(t.backend.actualCE)
+    template_data["comment"] = str(t.comment)
+    template_data["subjob_statuses"] = str(t.returnSubjobStatuses())
+
+    return template_data
+
+
+# ******************** Shutdown Function ******************** #
 
 # Route used to shutdown the flask server
 @app.route("/shutdown", methods=["POST"])
@@ -79,3 +176,5 @@ def shutdown():
     func()
     response_data = {"success": True, "message": "Shutting down the server..."}
     return jsonify(response_data)
+
+# ******************** EOF ******************** #
