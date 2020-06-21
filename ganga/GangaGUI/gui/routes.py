4e4d7bfb8be6092e4a931062b77693656e402acb
Varun Bankar <mail.varunbankar@gmail.com>
2020-06-21 23:08:58 +0530

    Create Credential Store API GET, PUT endpoints

--- ganga/GangaGUI/gui/routes.py
+++ ganga/GangaGUI/gui/routes.py
@@ -1,3 +1,5 @@
+# ******************** Imports ******************** #
+
 import jwt
 from GangaGUI.gui import app
 from flask import request, jsonify
@@ -5,18 +7,28 @@ from functools import wraps
 from GangaGUI.gui.models import User
 
 
+# ******************** View Routes ******************** #
+
+# Dashboard route
 @app.route("/")
 def dashboard():
-    return "Hello GangaGUI"
+    return "GangaGUI"
 
+
+# ******************** Token Based Authentication ******************** #
 
 # Generate token for API authentication - token validity 5 days
 @app.route("/token", methods=["POST"])
 def generate_token():
-    # Store request data
+    """
+    Using the 'user' and 'password' data from the form body, validates the user and returns a JSON Web Token (JWT).
+    """
+
+    # Request form data
     request_user = request.form.get("user")
     request_password = request.form.get("password")
 
+    # Handle no user or no password case
     if not request_user or not request_password:
         response_data = {"success": False, "message": "Could not verify user."}
         return jsonify(response_data), 401
@@ -28,12 +40,19 @@ def generate_token():
         response_data = {"token": token}
         return jsonify(response_data)
 
+    # If authentication fails, return 401 HTTP code
     response_data = {"success": False, "message": "Could not verify user."}
     return jsonify(response_data), 401
 
 
+# ******************** Token Authentication Decorator ******************** #
+
 # Decorator for token protected routes
 def token_required(f):
+    """
+    Decorator which validates the request header token in 'X-Acess-Token' field, and returns the user.
+    """
+
     @wraps(f)
     def decorated(*args, **kwargs):
 
@@ -64,13 +83,56 @@ def token_required(f):
     return decorated
 
 
-# TODO resource route for development purpose - REMOVE LATER
-# Protected resource
-@app.route("/resource", methods=["POST"])
-@token_required
-def resource(current_user):
-    return jsonify({"success": True, "message": "Token is valid"})
+# ******************** Credential Store API ******************** #
 
+
+# Credential Store API - GET Method - Get list of all credentials
+@app.route("/credential_store", methods=["GET"])
+@token_required
+def get_credential_store_data(current_user):
+    """
+    Return a list of credentials and their information in JSON format.
+    """
+
+    # Imports
+    from GangaCore.GPI import credential_store
+
+    credential_data_list = []
+    try:
+        for c in credential_store:
+            credential_info = {}
+            credential_info["location"] = str(c.location)
+            credential_info["time_left"] = str(c.time_left())
+            credential_info["expiry_time"] = str(c.expiry_time())
+            credential_info["is_valid"] = str(c.is_valid())
+            credential_info["exists"] = str(c.exists())
+            credential_data_list.append(credential_info)
+    except Exception as err:
+        return jsonify({"success": False, "message": str(err)}), 400
+
+    return jsonify(credential_data_list)
+
+
+# Credential Store API - PUT Method - Renew all credentials
+@app.route("/credential_store/renew", methods=["PUT"])
+@token_required
+def renew_credential_store_credentials(current_user):
+    """
+    Renew all the credentials in the credential store.
+    """
+
+    # Imports
+    from GangaCore.GPI import credential_store
+
+    try:
+        credential_store.renew()
+    except Exception as err:
+        return jsonify({"success": False, "message": str(err)}), 400
+
+    return jsonify({"success": True, "message": "Credentials store credentials renewed"})
+
+
+# ******************** Shutdown Function ******************** #
 
 # Route used to shutdown the flask server
 @app.route("/shutdown", methods=["POST"])
@@ -79,3 +141,5 @@ def shutdown():
     func()
     response_data = {"success": True, "message": "Shutting down the server..."}
     return jsonify(response_data)
+
+# ******************** EOF ******************** #
