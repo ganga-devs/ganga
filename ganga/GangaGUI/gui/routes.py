6acba7d91eab88e96a338c0fb443d426a1bd3e05
Varun Bankar <mail.varunbankar@gmail.com>
2020-07-06 22:35:35 +0530

    Update dashboard view

--- ganga/GangaGUI/gui/routes.py
+++ ganga/GangaGUI/gui/routes.py
@@ -1,9 +1,13 @@
+# ******************** Imports ******************** #
+
 import jwt
 from GangaGUI.gui import app
 from flask import request, jsonify, render_template
 from functools import wraps
 from GangaGUI.gui.models import User
 
+
+# ******************** View Routes ******************** #
 
 # Dashboard route
 @app.route("/")
@@ -20,14 +24,20 @@ def dashboard():
     return render_template("home.html", title="Dashboard", status_color=status_color, recent_jobs=recent_jobs, jobs=jobs)
 
 
+# ******************** Token Based Authentication ******************** #
 
 # Generate token for API authentication - token validity 5 days
 @app.route("/token", methods=["POST"])
 def generate_token():
-    # Store request data
+    """
+    Using the 'user' and 'password' data from the form body, validates the user and returns a JSON Web Token (JWT).
+    """
+
+    # Request form data
     request_user = request.form.get("user")
     request_password = request.form.get("password")
 
+    # Handle no user or no password case
     if not request_user or not request_password:
         response_data = {"success": False, "message": "Could not verify user."}
         return jsonify(response_data), 401
@@ -39,12 +49,19 @@ def generate_token():
         response_data = {"token": token}
         return jsonify(response_data)
 
+    # If authentication fails, return 401 HTTP code
     response_data = {"success": False, "message": "Could not verify user."}
     return jsonify(response_data), 401
 
 
+# ******************** Token Authentication Decorator ******************** #
+
 # Decorator for token protected routes
 def token_required(f):
+    """
+    Decorator which validates the request header token in 'X-Acess-Token' field, and returns the user.
+    """
+
     @wraps(f)
     def decorated(*args, **kwargs):
 
@@ -75,13 +92,7 @@ def token_required(f):
     return decorated
 
 
-# TODO resource route for development purpose - REMOVE LATER
-# Protected resource
-@app.route("/resource", methods=["POST"])
-@token_required
-def resource(current_user):
-    return jsonify({"success": True, "message": "Token is valid"})
-
+# ******************** Shutdown Function ******************** #
 
 # Route used to shutdown the flask server
 @app.route("/shutdown", methods=["POST"])
@@ -90,3 +101,5 @@ def shutdown():
     func()
     response_data = {"success": True, "message": "Shutting down the server..."}
     return jsonify(response_data)
+
+# ******************** EOF ******************** #
