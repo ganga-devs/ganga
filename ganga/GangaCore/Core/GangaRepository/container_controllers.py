ef6ee9b144a5ab3868f394518f8f6e999c4fe18c
DumbMachine <ratin.kumar.2k@gmail.com>
2020-08-16 02:45:25 +0530

    Added support for singularity containers using subprocess calls

--- ganga/GangaCore/Core/GangaRepository/container_controllers.py
+++ ganga/GangaCore/Core/GangaRepository/container_controllers.py
@@ -2,13 +2,41 @@
 
 import os
 import docker
+import subprocess
 
 from GangaCore.Utility import logging
 from GangaCore.Utility.Config import getConfig
+from GangaCore.Utility.Virtualization import checkDocker, checkUDocker, checkSingularity, installUdocker
 
 logger = logging.getLogger()
 
+GANGADIR = os.path.expandvars('$HOME/gangadir')
+DATABASE_CONFIG = getConfig("DatabaseConfigurations")
+COMMANDS = {
+    "udocker": None,
+    "singularity": {
+        "start": {
+            "register": "singularity instance start --bind {bind_loc}:/data docker://{image_name} {instance_name}",
+            "exec": "singularity exec instance://{instance_name} mongod --fork --logpath /data/daemon-mongod.log"
+        },
+        # "start": "singularity instance start --bind data:/data {image_name} {instance_name}; singularity exec instance://ganga_mongo mongod &",
+        "kill": {
+            "shutdown": "singularity instance stop {instance_name}"
+        }
+    }
+}
 
+
+def create_mongodir():
+    """
+    Will create a the required data folder for mongo db
+    """
+    dirs_to_make = [os.path.join(GANGADIR, "data"),
+                    os.path.join(GANGADIR, "data/db"),
+                    os.path.join(GANGADIR, "data/configdb")]
+    _ = map(lambda x: os.makedirs(x, exist_ok=True), dirs_to_make)
+
+    return os.path.join(GANGADIR, "data")
 
 def native_handler(database_config, action="start"):
     """
@@ -58,7 +86,23 @@ def singularity_handler(database_config, action="start"):
     database_config: The config from ganga config
     action: The action to be performed using the handler
     """
-    raise NotImplementedError
+    installed = checkSingularity()
+    if not installed:
+        raise Exception(
+            "uDocker was not installed in the system. Make sure that")
+    bind_loc = create_mongodir()
+    for key, cmd in COMMANDS["singularity"][action].items():
+        stdout = None if key == "register" else subprocess.PIPE
+        command = cmd.format(
+            bind_loc=bind_loc,
+            instance_name=DATABASE_CONFIG["containerName"],
+            image_name=DATABASE_CONFIG["baseImage"].replace(
+                ":latest", "")
+        )
+        process = subprocess.Popen(
+            command, stdout=stdout, stderr=subprocess.PIPE, shell=True)
+        stdout, stderr = process.communicate()
+    return stdout, stderr # we only need the last one
 
 
 def docker_handler(database_config, action="start"):
@@ -68,6 +112,8 @@ def docker_handler(database_config, action="start"):
     database_config: The config from ganga config
     action: The action to be performed using the handler
     """
+    if not checkDocker():
+        raise Exception("Docker seems to not be installed on the system.")
     if action not in ["start", "quit"]:
         raise NotImplementedError(f"Illegal Opertion on container")
 
@@ -85,7 +131,7 @@ def docker_handler(database_config, action="start"):
 
         except docker.errors.NotFound:
             # call the function to get the gangadir here
-            host_path = os.path.expanduser("~/gangadir/gangaDB")
+            bind_loc = create_mongodir()
             logger.info(
                 f"Pulling a copy of baseImage: {database_config['baseImage']}")
 
@@ -101,7 +147,7 @@ def docker_handler(database_config, action="start"):
                 #         target="/data/db", source=host_path,  type="bind")
                 # ]
                 # volumes={
-                #     os.path.expanduser("~/gangadir/gangaDB"): {"bind": "/data/db", "mode": "rw"}}
+                #     os.path.expanduser("bind_loc"): {"bind": "/data/db", "mode": "rw"}}
             )
         except Exception as e:
             # TODO: Handle gracefull quiting of ganga
