e1e6daae9fcd35575a31c232be6af587eed80d34
DumbMachine <ratin.kumar.2k@gmail.com>
2020-08-23 01:34:21 +0530

    please work this time 4

--- ganga/GangaCore/test/GPI/db_testing/blank.py
+++ ganga/GangaCore/test/GPI/db_testing/blank.py
@@ -2,8 +2,31 @@ import os
 import pytest
 import pymongo
 
+def test_keys():
+    """
+    Check whether the keys are found in the in the os
+    """
+    if "MONGODB_HOST" in os.environ.keys() and "MONGODB_PORT" in os.environ.keys():
+        assert True
+    else:
+        assert False
 
-def test_mongo_running_host():
+# def test_one():
+#     from pymongo import MongoClient
+#     from pymongo.errors import ServerSelectionTimeoutError
+#     PORT = os.environ["MONGODB_PORT"] if "MONGODB_PORT" in os.environ else 27017
+#     HOST = os.environ["MONGODB_HOST "] if "MONGODB_HOST " in os.environ else "localhost"
+#     connection_string = f"mongodb://{HOST}:{PORT}/"
+#     client = MongoClient(connection_string, serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
+#     try:
+#         _ = client.server_info() # Forces a call.
+#         assert True
+#     except ServerSelectionTimeoutError:
+#         assert False
+
+
+
+def test_two():
     from pymongo import MongoClient
     from pymongo.errors import ServerSelectionTimeoutError
     PORT = os.environ["MONGODB_PORT"] if "MONGODB_PORT" in os.environ else 27017
@@ -16,12 +39,3 @@ def test_mongo_running_host():
     except ServerSelectionTimeoutError:
         assert False
 
-def test_mongo_running():
-    from pymongo import MongoClient
-    from pymongo.errors import ServerSelectionTimeoutError
-    client = MongoClient(serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
-    try:
-        _ = client.server_info() # Forces a call.
-        assert True
-    except ServerSelectionTimeoutError:
-        assert False
