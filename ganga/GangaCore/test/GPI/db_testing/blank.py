e2ac87e67e04c47777233a424c2058979fe0f1db
DumbMachine <ratin.kumar.2k@gmail.com>
2020-08-23 01:28:42 +0530

    please work this time 4

--- ganga/GangaCore/test/GPI/db_testing/blank.py
+++ ganga/GangaCore/test/GPI/db_testing/blank.py
@@ -8,7 +8,7 @@ def test_mongo_running_host():
     from pymongo.errors import ServerSelectionTimeoutError
     PORT = os.environ["MONGODB_PORT"] if "MONGODB_PORT" in os.environ else 27017
     HOST = os.environ["MONGODB_HOST "] if "MONGODB_HOST" in os.environ else "mongodb"
-    connection_string = f"mongodb://{HOST}:{PORT}/dbName"
+    connection_string = f"mongodb://{HOST}:{PORT}"
     client = MongoClient(connection_string, serverSelectionTimeoutMS=10, connectTimeoutMS=20000)
     try:
         _ = client.server_info() # Forces a call.
