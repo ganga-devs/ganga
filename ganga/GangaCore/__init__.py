33baa5bd7f38efe84f75c0c927cd4ea6432ed356
Alexander Richards <a.richards@imperial.ac.uk>
2021-09-01 10:34:42 +0000

    Setting version number constants

--- ganga/GangaCore/__init__.py
+++ ganga/GangaCore/__init__.py
@@ -29,7 +29,7 @@ def getLCGRootPath():
 
 # ------------------------------------------------
 # store Ganga version based on new git tag for this file
-_gangaVersion = '8.5.1'
+_gangaVersion = '8.5.3'
 _development = False
 
 # store a path to Ganga libraries
