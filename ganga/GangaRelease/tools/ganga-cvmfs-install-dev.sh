4b590cdca7cd41ac40cbdf6ff578445ec6105aa3
Mark Smith <mark.smith@cern.ch>
2021-11-02 11:11:24 +0000

    Update ganga-cvmfs-install-dev.sh

--- ganga/GangaRelease/tools/ganga-cvmfs-install-dev.sh
+++ ganga/GangaRelease/tools/ganga-cvmfs-install-dev.sh
@@ -10,6 +10,17 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/sft.cern.ch/lcg/releases/LCG_100/
 
 pip install --upgrade git+https://github.com/ganga-devs/ganga.git@develop#egg=ganga[LHCb] 
 
+
+sed -i "23i\
+lib_string = '/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc9-opt/lib64:/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc9-opt/lib:/cvmfs/sft.cern.ch/lcg/releases/gcc/9.2.0-afc57/x86_64-centos7/lib:/cvmfs/sft.cern.ch/lcg/releases/gcc/9.2.0-afc57/x86_64-centos7/lib64'\n\
+sys.path.append('/cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc9-opt/lib/python3.8/site-packages')\n\
+if not 'LD_LIBRARY_PATH' in os.environ.keys():\n\
+    os.environ['LD_LIBRARY_PATH'] = lib_string\n\
+    os.execv(sys.argv[0], sys.argv)\n\
+elif not lib_string in os.environ['LD_LIBRARY_PATH']:\n\
+    os.environ['LD_LIBRARY_PATH'] += ':'+lib_string\n\
+    os.execv(sys.argv[0], sys.argv)" bin/ganga
+
 deactivate
 
 cd ~
