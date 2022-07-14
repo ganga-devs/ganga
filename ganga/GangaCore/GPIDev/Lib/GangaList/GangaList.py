e6cc19aa78095fdea78e5c86e71674a5e8590700
Ulrik Egede <ulrik.egede@monash.edu>
2022-07-15 08:56:23 +1000

    Add __class_getitem method required for python 3.9 and above (#2038)

--- ganga/GangaCore/GPIDev/Lib/GangaList/GangaList.py
+++ ganga/GangaCore/GPIDev/Lib/GangaList/GangaList.py
@@ -109,7 +109,7 @@ class GangaList(GangaObject):
 
     _category = 'internal'
     _exportmethods = ['__add__', '__contains__', '__delitem__', '__delslice__', '__eq__', '__ge__',
-                      '__getitem__', '__getslice__', '__gt__', '__iadd__', '__imul__',
+                      '__getitem__','__class_getitem__', '__getslice__', '__gt__', '__iadd__', '__imul__',
                       '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__reversed__', '__radd__', '__rmul__',
                       '__setitem__', '__setslice__', 'append', 'count', 'extend', 'index',
                       'insert', 'pop', 'remove', 'reverse', 'sort', '__hash__', 'get', 'clear']
@@ -354,6 +354,12 @@ class GangaList(GangaObject):
 
     def _export___getitem__(self, index):
         return addProxy(self.__getitem__(index))
+
+    def __class_getitem__(cls, *args, **kwargs):
+        return cls
+
+    def _export___class_getitem__(cls, *args, **kwargs):
+        return addProxy(self.__class_getitem__(cls, *args, **kwargs))
 
 #    def __getslice__(self, start, end):
 #        return makeGangaList(_list=self._list.__getslice__(start, end), preparable=self._is_preparable)
