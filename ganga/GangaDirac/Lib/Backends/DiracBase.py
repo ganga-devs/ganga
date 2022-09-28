65cc2802a5f38fc8b1c02473711080e55fb65241
Giorgos Apostolopoulos <s3apostol@gmail.com>
2022-09-28 13:13:01 +0300

    Remove cprofile wrapper from DiracBase

--- ganga/GangaDirac/Lib/Backends/DiracBase.py
+++ ganga/GangaDirac/Lib/Backends/DiracBase.py
@@ -46,19 +46,6 @@ default_unpackOutputSandbox = configDirac['default_unpackOutputSandbox']
 logger = getLogger()
 regex = re.compile(r'[*?\[\]]')
 
-import cProfile
-
-
-def profileit(func):
-    def wrapper(*args, **kwargs):
-        datafn = str(func) + ".profile"  # Name the data file sensibly
-        prof = cProfile.Profile()
-        retval = prof.runcall(func, *args, **kwargs)
-        prof.dump_stats(datafn)
-        return retval
-
-    return wrapper
-
 
 class DiracBase(IBackend):
 
