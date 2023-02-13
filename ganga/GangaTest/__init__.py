94096257e108be1e1802cf8934d055cf3c323288
abhijeetsharma200 <abhijeetsharma2002@gmail.com>
2023-02-13 09:22:34 +0100

    Made requested changes

--- ganga/GangaTest/__init__.py
+++ ganga/GangaTest/__init__.py
@@ -10,6 +10,5 @@ def loadPlugins(config=None):
     import GangaTest.Lib.TFile
     import GangaTest.Lib.TestApplication
     import GangaTest.Lib.TestSubmitter  # TestSubmitter, TestSplitter here
-    import GangaTest.Lib.TestRobot
     import GangaTest.Lib.TestObjects
     import GangaTest.Framework.runner
