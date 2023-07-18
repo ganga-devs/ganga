4abed91d2f86cde095648d95e64df5a4620c0a2c
Giorgos Apostolopoulos <s3apostol@gmail.com>
2023-07-18 12:32:26 +0300

    Upgrade aiohttp version for 3.11 compatibility

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ install_requires = [
     'jedi==0.17.2',
     'httplib2>=0.8',
     'aiofile==3.7.4',
-    'aiohttp==3.8.1',
+    'aiohttp==3.8.5',
     'absl-py>=0.1.2',
     'google-api-python-client',
     'google-auth-httplib2',
