4d9aaee20ebc84bf08e4b339c36bc2dde015adbd
Mark Smith <mark.smith@cern.ch>
2024-01-10 12:34:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
             'pytest-mock'],
         'profiler': ['memory_profiler'],
         'LHCb': ['LbDevTools'],
-        'Dirac': ['UltraDict']},
+        'Dirac': ['UltraDict','psutil']},
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 3.8',
