143cbf0fb6891cb08aa113af89d28b22287b098c
samadpls <abdulsamadsid1@gmail.com>
2024-02-14 12:47:05 +0500

    Fixed formatting and syntax issues
    Signed-off-by: samadpls <abdulsamadsid1@gmail.com>

--- ganga/GangaCore/test/GPI/TestInteractive.py
+++ ganga/GangaCore/test/GPI/TestInteractive.py
@@ -1,5 +1,6 @@
 from GangaCore.testlib.GangaUnitTest import GangaUnitTest
 
+
 class TestInteractiveJobOutput(GangaUnitTest):
     def testInteractiveJobOutput(self):
         from GangaCore.GPI import Job, Executable, Interactive, LocalFile
@@ -8,11 +9,13 @@ class TestInteractiveJobOutput(GangaUnitTest):
 
         app = Executable()
         app.exe = 'echo'
-        app.args = ["Hello World",""]
-        j = Job(backend=Interactive(), application=app, outputfiles=[LocalFile('stdout'), LocalFile('stderr')])
+        app.args = ["Hello World", ""]
+        j = Job(backend=Interactive(), application=app,
+                outputfiles=[LocalFile('stdout'), LocalFile('stderr')])
         j.submit()
-    
-        self.assertTrue(sleep_until_completed(j, 60), 'Timeout on registering Interactive job as completed')
+
+        self.assertTrue(sleep_until_completed(j, 60),
+                        'Timeout on registering Interactive job as completed')
 
         stdout_path = os.path.join(j.outputdir, 'stdout')
         stderr_path = os.path.join(j.outputdir, 'stderr')
@@ -24,8 +27,10 @@ class TestInteractiveJobOutput(GangaUnitTest):
 
         with open(stdout_path, 'rb') as stdout_file:
             actual_stdout_content = stdout_file.read()
-            self.assertIn(expected_stdout_content, actual_stdout_content, 'Incorrect content in stdout file')
+            self.assertIn(expected_stdout_content, actual_stdout_content,
+                          'Incorrect content in stdout file')
 
         with open(stderr_path, 'rb') as stderr_file:
             actual_stderr_content = stderr_file.read()
-            self.assertIn(expected_stderr_content, actual_stderr_content, 'Incorrect content in stderr file')
+            self.assertIn(expected_stderr_content, actual_stderr_content,
+                          'Incorrect content in stderr file')
