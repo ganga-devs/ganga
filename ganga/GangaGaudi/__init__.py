c57a3b13cd7ed08f5c4b25281589413890934652
egede <egede@users.noreply.github.com>
2024-03-18 04:22:11 +0000

    autopep8 action fixes

--- ganga/GangaGaudi/__init__.py
+++ ganga/GangaGaudi/__init__.py
@@ -9,7 +9,7 @@ logger = getLogger()
 if not _after_bootstrap:
 
     configGaudi = GangaCore.Utility.Config.makeConfig(
-    'GAUDI', 'Generic GAUDI based parameters')
+        'GAUDI', 'Generic GAUDI based parameters')
 
     dscrpt = 'The command used to make a CMT application.'
     configGaudi.addOption('make_cmd', 'make', dscrpt)
@@ -25,11 +25,11 @@ def standardSetup():
 
 
 def loadPlugins(config=None):
-    #import Lib.Backends
-    #import Lib.Checkers
+    # import Lib.Backends
+    # import Lib.Checkers
     pass
-    #import Lib.Applications
-    #import Lib.RTHandlers
-    #import Lib.Datasets
-    #import Lib.Datafiles
-    #import Lib.Splitters
+    # import Lib.Applications
+    # import Lib.RTHandlers
+    # import Lib.Datasets
+    # import Lib.Datafiles
+    # import Lib.Splitters
