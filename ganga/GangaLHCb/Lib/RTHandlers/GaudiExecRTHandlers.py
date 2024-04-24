829aaa62526cd2728f21604cb29624a2b47a2320
Ulrik Egede <ulrik.egede@monash.edu>
2024-04-24 17:18:43 +1000

    Proposed solution for running slc6 jobs

--- ganga/GangaLHCb/Lib/RTHandlers/GaudiExecRTHandlers.py
+++ ganga/GangaLHCb/Lib/RTHandlers/GaudiExecRTHandlers.py
@@ -191,7 +191,9 @@ def prepareCommand(app):
     for _arg in app.run_args:
         run_args += ' %s ' % _arg
 
-    run_cmd = ' export ganga_jobid=%s && ./run %s ' % (app.getJobObject().fqid, run_args)
+    run_cmd = f' export ganga_jobid={app.getJobObject().fqid} && ./run {run_args} '
+    if 'slc6' in app.platform:
+        run_cmd = f'sed -i "s/lb-run  /lb-run --container apptainer /g" ./run && {run_cmd}'
 
     if not app.useGaudiRun:
         full_cmd = sourceEnv + run_cmd + 'python %s' % app.getWrapperScriptName()
@@ -792,13 +794,26 @@ if __name__ == '__main__':
     # Extract any/_all_ (b/g)zip files on the WN
     extractAllTarFiles('.')
 
+    runenv = dict()
+    result = subprocess.run(["bash", "-lc", 
+                             "source /cvmfs/lhcb.cern.ch/lib/LbEnv && printenv"],
+                            capture_output=True, encoding='utf-8')
+    for line in result.stdout.split('\\n'):
+        varval = line.strip().split('=')
+        if len(varval) < 2:
+            pass
+        else:
+            content = ''.join(varval[1:])
+            if not str(content).startswith('() {'):
+                runenv[varval[0]] = content
+
     print("Executing: %s" % '###COMMAND###'+' '+' '.join(sys.argv[1:]))
 
     # Execute the actual command on the WN
     # NB os.system caused the entire stream to be captured before being streamed in some cases
     pipe = subprocess.Popen('###COMMAND###'+' '+' '.join(sys.argv[1:]), shell=True,
                             stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                            universal_newlines=True, errors="replace")
+                            universal_newlines=True, errors="replace", env=runenv)
 
     # Flush the stdout/stderr as the process is running correctly
     flush_streams(pipe)
