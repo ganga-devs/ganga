fe5bbc2c101a0c8f09fa2fb598035f44b32eda65
glk0 <glk0@users.noreply.github.com>
2025-09-22 07:27:28 +0000

    autopep8 action fixes

--- scripts/deprecation_report.py
+++ scripts/deprecation_report.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 from ganga.GangaCore.Utility.Deprecation import generate_deprecation_json_report
 
-root = str ((Path (__file__) / '../../' / ("./ganga")).resolve ())
+root = str((Path(__file__) / '../../' / ("./ganga")).resolve())
 r = generate_deprecation_json_report(root)
 if len(sys.argv) > 1:
     f = open(sys.argv[1], 'w')
