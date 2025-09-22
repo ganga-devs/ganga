416f794d8be753c851e64f1dc1411e1a7e2493ee
glk0 <contact@glk0.me>
2025-09-22 08:26:18 +0100

    add dynamic definition of root in deprecation_report.py

--- scripts/deprecation_report.py
+++ scripts/deprecation_report.py
@@ -1,7 +1,8 @@
 import sys
+from pathlib import Path
 from ganga.GangaCore.Utility.Deprecation import generate_deprecation_json_report
 
-root = "./ganga"
+root = str ((Path (__file__) / '../../' / ("./ganga")).resolve ())
 r = generate_deprecation_json_report(root)
 if len(sys.argv) > 1:
     f = open(sys.argv[1], 'w')
