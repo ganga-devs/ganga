ed5cd36334db426a108cf0a06833bc234e125723
mesmith75 <mesmith75@users.noreply.github.com>
2026-04-22 07:44:20 +0000

    autopep8 action fixes

--- ganga/GangaDirac/Lib/Splitters/OfflineGangaDiracSplitter.py
+++ ganga/GangaDirac/Lib/Splitters/OfflineGangaDiracSplitter.py
@@ -165,7 +165,8 @@ def getLFNReplicas(allLFNs, index, allLFNData):
         this_max = int((index + 1) * LFN_parallel_limit)
 
     try:
-        output = wrapped_execute('getReplicasForJobs(%s, diskOnly=True)' % str(allLFNs[this_min:this_max]), dict, new_subprocess=True)
+        output = wrapped_execute('getReplicasForJobs(%s, diskOnly=True)' %
+                                 str(allLFNs[this_min:this_max]), dict, new_subprocess=True)
     except SplitterError:
         logger.error("Failed to Get Replica Info: [%s:%s] of %s" % (str(this_min), str(this_max), len(allLFNs)))
         raise
